ba301421c3fe45197a18a75d57de296d0ccf5ee6
Adam Salma <adam3salma@gmail.com>
2016-07-25 21:29:53 +0100

    Remove compiled files in build

--- client/build/index.js
+++
@@ -1,9 +0,0 @@
-/// <reference path="../../typings/index.d.ts" />
-"use strict";
-require('core-js');
-require('zone.js/dist/zone');
-require('reflect-metadata');
-var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
-var http_1 = require('@angular/http');
-var app_component_1 = require('./app.component');
-platform_browser_dynamic_1.bootstrap(app_component_1.AppComponent, [http_1.HTTP_PROVIDERS]);
