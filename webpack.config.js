7ea7709b4d1b1488a39cdd7b99abf2ca26ba563a
Adam Salma <adam3salma@gmail.com>
2016-08-08 01:43:08 +0100

    Refactor env + add middleware to webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -4,18 +4,20 @@ var path = require('path');
 var loaders = require('./webpack.loaders');
 var CopyWebpackPlugin = require('copy-webpack-plugin');
 
-const HOST = process.env.HOST || "0.0.0.0";
-const PORT = process.env.PORT || "9999";
+if (!process.env.HOST) require('./env');
+const HOST = process.env.HOST
+const PORT = process.env.PORT
 
 module.exports = {
 	entry: [
-		`webpack-dev-server/client?http://${HOST}:${PORT}`, // WebpackDevServer host and port
-		`webpack/hot/only-dev-server`,
+        'webpack-hot-middleware/client',
+        'webpack/hot/dev-server',
 		`./src/index.jsx`
 	],
 	devtool: process.env.WEBPACK_DEVTOOL || 'source-map',
 	output: {
-		path: path.join(__dirname, 'dist'),
+        path: "/",
+        publicPath: "http://" +HOST+ ":" +PORT+ "/",
 		filename: 'app.bundle.js'
 	},
 	resolve: {
