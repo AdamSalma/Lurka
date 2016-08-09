3506ee326066a519f7a01d2dfbf508495b230dbd
Adam Salma <adam3salma@gmail.com>
2016-08-09 22:58:38 +0100

    Add http to viewer page

--- src/pages/MemeViewer.jsx
+++ src/pages/MemeViewer.jsx
@@ -1,20 +1,53 @@
 import React from "react";
-import { Link } from "react-router";
 
 import Board from "../components/Board";
 import Thread from "../components/Thread";
 
 export default class MemeViewer extends React.Component {
-    constructor() {
-        super()
+    constructor(props) {
+        super(props);
+
+        this.requestBoard.bind(this);
+        this.onThreadRequest.bind(this);
+
+        this.state = {
+            provider: "4chan",
+            threads: {},
+            thread: {},
+            board: "g"
+        }
     }
 
     render() {
+        const { provider, thread, threads } = this.state;
         return (
-            <div className="MemeViewer">
-                <Board/>
-                <Thread/>
+            <div className={provider}>
+                <button onClick={this.requestBoard.bind(null, "g")}>
+
+                </button>
+                <Board threads={ threads } onThreadRequest={this.onThreadRequest}/>
+                <Thread thread={ thread }/>
             </div>
         )
+    }
+
+    requestBoard( boardID ) {
+        const { provider } = this.props;
+        axios.get(`/${provider}/${boardID}`).then( board => {
+            console.log("Board success!");
+            console.log(board);
+            this.setState({
+                boardID: boardID,
+                threads: board.data
+            });
+        }).catch( err => console.error(err) );
+    }
+
+    onThreadRequest( threadID ) {
+        const { provider, boardID } = this.state;
+        axios.get(`/${provider}/${boardID}/${threadID}`).then( thread => {
+            console.log("Thread success!");
+            console.log(thread);
+        }).catch( err => console.error(err) );
     }
 }
