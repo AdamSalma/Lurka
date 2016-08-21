6054cce0922cd1cc0a7a9a1460917f2464adf222
Adam Salma <adam3salma@gmail.com>
2016-08-21 01:46:07 +0100

    Update constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,18 +2,11 @@
 export const USER_SAVE = "USER_SAVE"
 export const USER_LOAD = "USER_LOAD"
 
-// HTTP actions
-export const HTTP_GET = "HTTP_GET"
-export const HTTP_POST = "HTTP_POST"
-
 // content viewer actions
-export const BOARD_CREATE = "BOARD_CREATE"
+export const BOARD_REQUEST = "BOARD_REQUEST"
 export const BOARD_LOADED = "BOARD_LOADED"
 export const BOARD_DESTROY = "BOARD_DESTROY"
 
-export const THREAD_CREATE = "THREAD_CREATE"
+export const THREAD_REQUEST = "THREAD_REQUEST"
 export const THREAD_LOADED = "THREAD_LOADED"
 export const THREAD_DESTROY = "THREAD_DESTROY"
-
-// TODO - Delete this
-export const TEST = "TEST"
