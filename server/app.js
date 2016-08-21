44b237bece14083f8d8e3a71ca837ea15bf55820
Adam Salma <adam3salma@gmail.com>
2016-08-21 01:55:26 +0100

    Add root path constant to server

--- server/app.js
+++ server/app.js
@@ -1,26 +1,26 @@
 var express = require('express');
-var path = require('path');
-
 var app = express();
 var router = express.Router();
 
-// If env is development, use webpack hot reloading
-var env = process.env.NODE_ENV
-if (!env || env === 'dev' || env === 'development') {
+var ROOT = require('path').join(__dirname, '../dist');
+var ENV = process.env.NODE_ENV
+
+
+// If ENV is development, use webpack hot reloading
+if (!ENV || ENV === 'dev' || ENV === 'development') {
     console.log('DEVOLOPMENT ENVIRONMENT: Turning on WebPack Middleware...');
     require('./index.dev.js').useWebpackMiddleware(app);
 } else {
     console.log('PRODUCTION ENVIRONMENT');
-    app.use(express.static(path.join(__dirname, '../dist')));
-    console.log('PRODUCTION ENVIRONMENT 2');
-    // TODO - Uncomment this when ready
+    app.use(express.static(ROOT));
+    // TODO - Uncomment this when ready:
     // app.use(favicon(path.join(__dirname, '../dist/imgs', 'favicon.ico')));
 }
 
 app.use('/4chan', require('./routes/4chan'));
 
 app.use('/', function(req, res, next) {
-  res.render('index');
+  res.sendFile('index.html', { root: ROOT });
   res.end();
 });
 
