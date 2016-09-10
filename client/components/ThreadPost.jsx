f6de2054842641192eeb1f06341329bf6d14f185
Adam Salma <adam3salma@gmail.com>
2016-09-10 19:00:53 +0100

    Add image open/close to ThreadPost

--- client/components/ThreadPost.jsx
+++ client/components/ThreadPost.jsx
@@ -1,25 +1,65 @@
 import React from 'react';
+import $ from 'jquery';
 
 export default ({post}) => {
-    const { id, title, date, imgsrc, comment } = post
+    const { id, title, date, imgsrc, comment, ext } = post
+
+    const SRC = imgsrc
+    const ID = 'thread-media-' + id
+
     return (
-        <div className="thread-post">
-            <div className="post-info">
-                <span className={title ? "subtitle-active" : ""}>{title}</span>
-                <span className="dateTime">{date}</span>
-                <span className="postNum">No.{id}</span>
-                <span className="backlink"></span>
+        <div className='thread-post'>
+            <div className='post-info'>
+                <span className={title ? 'subtitle-active' : ''}>{title}</span>
+                <span className='dateTime'>{date}</span>
+                <span className='postNum'>No.{id}</span>
+                <span className='backlink'></span>
             </div>
-            {createImageIfExists(imgsrc)}
+            {createMediaIfExists()}
             <blockquote>
-                <div className="postMessage" 
+                <div className='postMessage' 
                      dangerouslySetInnerHTML={{__html: comment}}></div>
             </blockquote>
         </div>
     )
+
+    function createMediaIfExists() {
+        if (SRC) {
+            if (ext === '.webm') return createWebm();
+            else return createImage(ID, SRC);
+        }
+    }
 }
 
 
-function createImageIfExists(imgsrc) {
-    if (imgsrc) return <div className="post-img"><img src={imgsrc.sm} /></div>
+function createImage(ID, SRC) {
+    return (
+        <div>
+            <img 
+                className={'post-img'}
+                id={ID}
+                src={SRC.sm}
+                onClick={toggleImage.bind(null, ID, SRC)}/>
+        </div>
+    )
+}
+
+function createWebm() {
+    console.log('Webm creation attempted');
+}
+
+function toggleImage(ID, SRC) {
+    const img = $(document.getElementById(ID))
+    if (img.attr('src') === SRC.sm) {
+        // blur image while large image is loading
+        img
+            .addClass('image-blur')
+            .on({ 
+                'load': () => img.removeClass('image-blur')
+            })
+
+        img.attr('src', SRC.lg);
+    } else {
+        img.attr('src', SRC.sm);
+    }
 }
