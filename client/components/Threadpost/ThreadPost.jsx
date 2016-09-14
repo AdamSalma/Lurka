c4403c23ef0aac2312d92ce5a2ba98270ed13910
Adam Salma <adam3salma@gmail.com>
2016-09-14 23:19:54 +0100

    Move threadpost functions to new helper file

--- client/components/Threadpost/ThreadPost.jsx
+++ client/components/Threadpost/ThreadPost.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
-import $ from 'jquery';
+import {
+    createImage, 
+    createWebm
+} from './helpers'
 
 export default ({post, children}) => {
     const { id, title, date, imgsrc, comment, ext } = post
@@ -29,38 +32,3 @@ export default ({post, children}) => {
 }
 
 
-function createImage(ID, SRC) {
-    return (
-        <div className='img-container' onClick={toggleImage.bind(null, ID, SRC)}>
-            <span className="fullscreen hidden fa-stack fa-sm">
-                <i className="fa fa-circle fa-stack-2x"></i>
-                <i className="fa fa-expand fa-stack-1x"></i>
-            </span>
-            <img id={ID} src={SRC.sm}/>
-        </div>
-    )
-}
-
-function createWebm() {
-    console.log('Webm creation attempted');
-}
-
-function toggleImage(ID, SRC) {
-    const img = $('#'+ID)
-    const fullscreen = img.prev()
-
-    if (img.attr('src') === SRC.sm) {
-        // blur image while large image is loading
-        img.addClass('image-loading')
-           .on('load', function(){
-               img.removeClass('image-loading')
-                  .off('load');
-               fullscreen.removeClass('hidden');
-           })
-
-        img.attr('src', SRC.lg);
-    } else {
-        img.attr('src', SRC.sm)
-        fullscreen.addClass('hidden')
-    }
-}
