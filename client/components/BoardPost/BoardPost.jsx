220a54061abbb8f4979fe787eed8cd386a3a64b4
Adam Salma <adam3salma@gmail.com>
2016-09-18 20:31:00 +0100

    Remove redux from BoadPost + Add tooltip

--- client/components/BoardPost/BoardPost.jsx
+++ client/components/BoardPost/BoardPost.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
+import Tooltip from '../Tooltip';
 
-export default function({post, fetchThread}) {
+export default ({ post, fetchThread }) => {
     const {id, title, comment, date, imgsrc, replies} = post;
 
     return (
-        <div className="board-post"
-             onClick={() => fetchThread(id)}>
+        <div 
+            className="board-post"
+            onClick={() => fetchThread(id)}
+        >
             <img src={imgsrc.sm} />
-            <div className="count">
-                R: <b>{replies.textCount}</b> |
-                I: <b>{replies.imgCount}</b>
-            </div>
+            <Tooltip>
+                <div className="count">
+                    R: <b>{replies.textCount}</b> |
+                    I: <b>{replies.imgCount}</b>
+                </div>
+            </Tooltip>
             <div className="op">
                 <b dangerouslySetInnerHTML={{__html: title}} className="title"></b>
                 <div dangerouslySetInnerHTML={{__html: comment}}></div>
