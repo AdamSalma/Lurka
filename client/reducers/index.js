34ab0dfe1e592aaa22e9d62e3e7730f0a5928f17
Adam Salma <adam3salma@gmail.com>
2016-09-19 22:40:56 +0100

    New content status reducer

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import BoardReducer from "./board.reducer";
 import ThreadReducer from "./thread.reducer";
+import StateReducer from "./status.reducer";
 
 const rootReducer = combineReducers({
     board: BoardReducer,
-    thread: ThreadReducer
+    thread: ThreadReducer,
+    status: StateReducer
 });
 
 export default rootReducer
