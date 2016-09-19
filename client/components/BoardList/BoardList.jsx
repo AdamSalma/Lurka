adbc21e8a30619f30a6a89c056f2ffd853cd7964
Adam Salma <adam3salma@gmail.com>
2016-09-19 22:38:59 +0100

    Connect BoardList dropdown to fetchBoard action

--- client/components/BoardList/BoardList.jsx
+++ client/components/BoardList/BoardList.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 
-export default function({boardList, fetchBoard}) { 
+export default function({boardList, fetchBoard, provider}) { 
     return (
-        <select className="board-list">
-            {boardList.map( ({board, title}) => {
-                return <option onClick={fetchBoard(board)}> /{board}/ - {title} </option>
-            })}
+        <select 
+        	className="board-list"
+        	onChange={({target}) => fetchBoard({boardID: target.value, provider})}>
+	            {boardList.map( ({board, title}) => (
+                	<option key={board} value={board}>
+                		/{board}/ - {title} 
+              		</option>
+	            ))}
         </select>
     )
 }
