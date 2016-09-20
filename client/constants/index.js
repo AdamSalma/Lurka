d0732938fff828d010a5866bcc48e7debd27504f
Adam Salma <adam3salma@gmail.com>
2016-09-20 22:59:37 +0100

    Add actiontypes: BOARD_CHANGE, PROVIDER_CHANGE

--- client/constants/index.js
+++ client/constants/index.js
@@ -2,6 +2,7 @@
 export const USER_SAVE = "USER_SAVE"
 export const USER_LOAD = "USER_LOAD"
 
+export const BOARD_CHANGE = "BOARD_CHANGE"
 export const BOARD_REQUEST = "BOARD_REQUEST"
 export const BOARD_LOADED = "BOARD_LOADED"
 export const BOARD_DESTROY = "BOARD_DESTROY"
@@ -12,4 +13,7 @@ export const THREAD_REQUEST = "THREAD_REQUEST"
 export const THREAD_LOADED = "THREAD_LOADED"
 export const THREAD_DESTROY = "THREAD_DESTROY"
 export const THREAD_POST_LOAD = "THREAD_POST_LOAD"
+
+export const PROVIDER_CHANGE = "PROVIDER_CHANGE"
+
 
