7d7b2c2ce1584ddf6909dfef4def795d1d983e07
Adam Salma <adam3salma@gmail.com>
2016-10-08 21:11:53 +0100

    Change server startup logging

--- src/server/routes/dashboard/getDashboard.js
+++ src/server/routes/dashboard/getDashboard.js
@@ -3,7 +3,7 @@ import { join } from 'path';
 
 export default function (req, res, next) {
     if (req.xhr) return next();
-    console.log(`Sending dashboard from ${global.app_root}`);
+    console.log(`Streaming dashboard from ${global.app_root}`);
     let index = join(global.app_root, 'app.html');
     createReadStream(index).pipe(res);
 }
