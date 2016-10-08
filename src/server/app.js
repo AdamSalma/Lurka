d95f55b0642e7e78053724eca5cff3d92661af79
Adam Salma <adam3salma@gmail.com>
2016-10-08 15:44:53 +0100

    Compress app.js

--- src/server/app.js
+++ src/server/app.js
@@ -1,54 +1,44 @@
-import express from 'express';
-import config from '../config';
-import webpackHotMiddleware from './middleware/webpackMiddleware';
-
-var app = express();
-var router = express.Router();
-
-const ROOT = global.root = require('path').join(__dirname, '../dist');
-
-
+import Express from 'express'
+import config from '../../config'
+import webpackHotMiddleware from './middleware/webpackMiddleware'
+import { join } from 'path';
+ 
+const app = Express();
+const router = Express.Router();
+ 
 // Environment
 console.info(`Environment: "${config.env}"`);
-if (config.env !== 'production') 
+if (config.env === 'production') {
+    global.app_root = __dirname
+    app.use(Express.static(global.app_root));
+} else {
+    // Development
+    global.app_root = join(__dirname, '../..', 'app');
     webpackHotMiddleware(app);
-else 
-    app.use(express.static(ROOT));
-// app.use(favicon(path.join(ROOT, '/assets/favicon.ico')))
-
-
-// Print url for debugging:
-app.all('*', function(req, res, next){
-    console.info("User request:", req.url);
-    next()
-});
-
-
+    app.all('*', (req, res, next) => {
+        console.info("User request:", req.url);
+        next()
+    });
+}
+ 
+ 
 // Routes
-app.use('/', function (req, res, next) {
-    console.log("Sending dashboard")
-    console.log(__dirname)
-    console.log(process.env.PWD)
-    console.log(global.root)
-    console.log("")
-    res.sendFile('index.html', { root: ROOT });
-    res.end();
-})
+app.use('/', require('./routes/dashboard'))
 app.use('/4chan', require('./routes/4chan'));
-
-
+ 
+ 
 // 404 handler
 app.use((req, res, next) => {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
 });
-
+ 
 // Print stacktrace
 app.use( err => {
     console.error(err.message);
     res.send(err)
 });
-
-
+ 
+ 
 export default app;
