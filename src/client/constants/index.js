b5f147c986d0819918d1cd6472cb76f5e472e5c6
Adam Salma <adam3salma@gmail.com>
2016-10-12 00:52:30 +0100

    New header constants

--- src/client/constants/index.js
+++ src/client/constants/index.js
@@ -1,7 +1,10 @@
-// User actions
+/* User settings */
+// TODO - Add more
 export const USER_SAVE = "USER_SAVE"
 export const USER_LOAD = "USER_LOAD"
 
+
+/* Conetnt */
 export const BOARD_CHANGE = "BOARD_CHANGE"
 export const BOARD_REQUEST = "BOARD_REQUEST"
 export const BOARD_LOADED = "BOARD_LOADED"
@@ -14,6 +17,16 @@ export const THREAD_LOADED = "THREAD_LOADED"
 export const THREAD_DESTROY = "THREAD_DESTROY"
 export const THREAD_POST_LOAD = "THREAD_POST_LOAD"
 
+
+/* Header */
 export const PROVIDER_CHANGE = "PROVIDER_CHANGE"
+export const FILTER_BOARD = "FILTER_BOARD"
+export const FILTER_THREAD = "FILTER_THREAD"
+export const SEARCH_THREAD = "SEARCH_THREAD"
+export const SERACH_BOARD = "SERACH_BOARD"
 
 
+/* Animations */
+export const HEADER_ANIMATE = "HEADER_ANIMATE"
+export const LOGO_SPIN_START = "LOGO_SPIN_START"
+export const LOGO_SPIN_END = "LOGO_SPIN_END"
