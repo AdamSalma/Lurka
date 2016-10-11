1d51107ddbb8b326846b26f5e395aa53c82340cd
Adam Salma <adam3salma@gmail.com>
2016-10-12 00:46:28 +0100

    Move provider actions to header, add toggleHeaderAnimation

---
+++ src/client/actions/HeaderActions.js
@@ -0,0 +1,29 @@
+import Axios from 'axios';
+import {
+    PROVIDER_CHANGE, 
+    HEADER_ANIMATE,
+    LOGO_SPIN,
+    FILTER_BOARD,
+    FILTER_THREAD,
+    SERACH_BOARD,
+    SEARCH_THREAD
+} from '../constants';
+
+export function toggleHeaderAnimation(){
+    console.info('toggleHeaderAnimation');
+    return dispatch => {
+        dispatch(() => { return {type: HEADER_ANIMATE} })
+    }
+}
+
+export function changeProvider({ provider }) {
+    console.info("Action changeProvider() to " + provider);
+    return dispatch => {
+        dispatch(() => {
+            return {
+                type: PROVIDER_CHANGE,
+                payload: provider
+            }
+        })
+    }   
+}
