6c890846d3f576b4575e53321c1e90781a7131ed
Adam Salma <adam3salma@gmail.com>
2016-10-13 22:43:12 +0100

    Add trigger for into animations

--- src/client/components/Logo/Logo.jsx
+++ src/client/components/Logo/Logo.jsx
@@ -7,12 +7,16 @@ class Logo extends React.Component {
         super(props);
         this.displayName = 'Logo';
         this.toggleLogoSize = this.toggleLogoSize.bind(this);
+        this.triggerTransitionAnimation = this.triggerTransitionAnimation.bind(this);
     }
 
+    componentWillUpdate(nextProps, nextState) {
+        if (nextProps.isFullsize !== this.props.isFullsize) 
+            this.triggerTransitionAnimation();
+    }
+    
     componentDidMount() {
-    	setTimeout(() => {
-	        this.toggleLogoSize()
-    	}, 1000)
+        this.triggerLogoDropdownAnimation(this.refs.logoImg)
     }
 
     render() {
@@ -29,6 +33,14 @@ class Logo extends React.Component {
 		)
     }
 
+    triggerTransitionAnimation(){
+        console.log("isFullsize?", this.props.isFullsize);
+        console.log("Triggering logo");
+        this.toggleLogoSize()
+        console.log("Triggering header");
+        this.props.toggleHeader()
+    }
+
     toggleLogoSize() {
     	const {logo, logoImg} = this.refs;
     	if (this.props.isFullsize) {
@@ -36,7 +48,7 @@ class Logo extends React.Component {
 
 	    	Velocity(logo, {right: distance, top: "10px", width: "50px"}, {
                 duration: 1250,
-                easing: [0.25, 0.8, 0.25, 1]
+                easing: "ease-out"
             })
 
             Velocity(logoImg, {width: "50px"}, {
@@ -44,13 +56,23 @@ class Logo extends React.Component {
             })
 
     	} else {
-    		Velocity(
-	    		this.refs.logo,
-	    		{opacity: 1}
-	    	)
+    		Velocity(logo, {right: 0, top: "70px", width: "200px"}, {
+                duration: 1250,
+                easing: "[0.25, 0.8, 0.25, 1]"
+            })
+
+            Velocity(logoImg, {width: "200px"}, {
+                duration: 1250,
+            })
     	}
 
     	this.props.toggleHeader()
+    }
+
+    triggerLogoDropdownAnimation(logo) {
+        console.log("triggerLogoDropdownAnimation()", logo)
+        Velocity(logo, {opacity: 1, top: "+10px"}, {duration: 1000})
+
     }
 }
 
