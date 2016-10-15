16a380d577fa81232d76428c60e534b6e4aceab8
Adam Salma <adam3salma@gmail.com>
2016-10-15 03:04:52 +0100

    Relocate all animations to AnimationActions

---
+++ src/client/actions/AnimationActions.js
@@ -0,0 +1,90 @@
+import Velocity from 'velocity-animate'
+
+import {
+	SCROLL_START,
+	SCROLL_END,
+    HEADER_ANIMATION_END,
+    HEADER_SHRINKING,
+    HEADER_EXPANDING
+} from '../constants'
+
+export function scrollPage({ content=false, mainPage=false })  {
+	if (!content && !mainPage) {
+		throw new Error("No scroll destination supplied")
+	}
+	
+	const offset = content ? window.innerHeight : 0;
+
+	return dispatch => {
+        dispatch({type: SCROLL_START});
+        return Velocity(document.body, 'scroll', {
+        	offset: offset, 
+        	duration: 1000, 
+        	easing: "ease-in-out", 
+        	complete: () => {
+        		dispatch({type: SCROLL_END})
+        	}
+       	});
+    }
+}
+
+
+export function shrinkHeader() {
+    const header = document.querySelector('#header');
+    const logo   = document.querySelector('#logo');
+    const logoDistanceFromLeft = window.innerWidth - 90 + "px"
+    let animationCompleteCount = 0
+
+    return dispatch => {
+        dispatch({type: HEADER_SHRINKING})
+
+        Velocity(header, {height: "70px"}, {
+            duration: 1000,
+            complete: isComplete
+
+        })
+
+        Velocity(logo, {right: logoDistanceFromLeft, top: "10px", width: "50px"}, {
+            duration: 1000,
+            easing: "ease-out",
+            complete: isComplete
+        })
+
+        function isComplete() {
+            animationCompleteCount++
+            if (animationCompleteCount == 2) {
+                dispatch({type: HEADER_ANIMATION_END})
+            }
+        }
+        
+    }
+}
+
+export function expandHeader() {
+    const header = document.querySelector('#header');
+    const logo   = document.querySelector('#logo');
+    let animationCompleteCount = 0
+
+    return dispatch => {
+        dispatch({type: HEADER_EXPANDING})
+
+        Velocity( header, {height: "270px"}, {
+            duration: 1000, 
+            easing: "ease-out",
+            complete: isComplete
+        })
+
+        Velocity(logo, {right: 0, top: "70px", width: "200px"}, {
+            duration: 1000,
+            easing: "ease-out",
+            complete: isComplete
+        })
+
+        function isComplete() {
+            animationCompleteCount++
+            if (animationCompleteCount == 2) {
+                dispatch({type: HEADER_ANIMATION_END})
+            }
+        }
+    }
+}
