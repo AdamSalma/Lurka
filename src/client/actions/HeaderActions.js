878b84d89050f393536e95a37b985b6c17edcdbb
Adam Salma <adam3salma@gmail.com>
2016-10-15 03:04:04 +0100

    Remove toggleHeaderAnimation

--- src/client/actions/HeaderActions.js
+++ src/client/actions/HeaderActions.js
@@ -1,22 +1,12 @@
 import Axios from 'axios';
 import {
     PROVIDER_CHANGE, 
-    HEADER_ANIMATE,
     LOGO_SPIN,
     FILTER_BOARD,
     FILTER_THREAD,
     SERACH_BOARD,
     SEARCH_THREAD
 } from '../constants';
-
-export function toggleHeaderAnimation(){
-    console.info('toggleHeaderAnimation');
-    return dispatch => {
-        dispatch({
-            type: HEADER_ANIMATE
-        })
-    }
-}
 
 export function changeProvider({ provider }) {
     console.info("Action changeProvider() to " + provider);
