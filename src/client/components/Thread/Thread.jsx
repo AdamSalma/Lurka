dac11a6195eabf6f50be849b9b102d3a86df46ce
Adam Salma <adam3salma@gmail.com>
2016-10-16 23:08:51 +0100

    Add nanoScroller to Thread

--- src/client/components/Thread/Thread.jsx
+++ src/client/components/Thread/Thread.jsx
@@ -9,6 +9,8 @@ import ThreadPost from '../ThreadPost';
 import Background from '../Background';
 import Spinner from '../Spinner';
 
+import '../../vendor';
+
 import {
     fullscreenImageDelegation
 } from './helpers'
@@ -20,25 +22,26 @@ export default class Thread extends Component {
     }
 
     componentDidMount() {
-        fullscreenImageDelegation(this.refs.thread)
+        const {thread, threadWrap} = this.refs;
+        fullscreenImageDelegation(thread)
+        // $(threadWrap).nanoScroller({ sliderMinHeight: 40, alwaysVisible: true })
     }
 
-    componentWillUnmount() {
-        $(this.refs.thread).off('click')
-    }
+    // componentWillUnmount() {
+    //     $(this.refs.thread).off('click');
+    // }
 
     componentDidUpdate(prevProps) {
         const { thread } = this.props;
         if (prevProps.thread.length !== thread.length){
             // Thread created or destroyed
-            this.threadToggle();            
+            this.threadToggle();
         } 
     }
 
     render() {
         const { thread, isFetching } = this.props
-        const threadWrapClasses = classNames({
-            "thread-wrap": true,
+        const threadWrapClasses = classNames('thread-wrap', 'nano', {
             "thread-wrap-active": thread.length || isFetching
         });
 
@@ -46,40 +49,40 @@ export default class Thread extends Component {
         
 
         return (
-            <div className={threadWrapClasses}>
+            <div>
                 <Background 
                     isVisible={thread.length || isFetching} 
                     closeBackground={this.threadToggle}/>
-                <Spinner 
-                    isSpinning={isFetching}/>
-                <div 
-                    className="thread"
-                    ref="thread">
-                    {thread.map( 
-                        post => {
-                            console.log(post.time);
-                            return (
-                                <ThreadPost 
-                                    key={uuid.v4()} 
-                                    post={post}>
-                                    <TimeAgo date={post.time}/>
-                                </ThreadPost>
-                            )
-                        }
-                    )}
+                <Spinner isSpinning={isFetching}/>
+
+                <div ref='threadWrap' className={threadWrapClasses}>
+                    <div className="thread nano-content" ref="thread">
+                        {thread.map( 
+                            post => <ThreadPost 
+                                key={uuid.v4()} 
+                                post={post}>
+                                <TimeAgo date={post.time}/>
+                            </ThreadPost>
+                        )}
+                    </div>
                 </div>
             </div>
         )
     }
 
     threadToggle() {
-        const { thread } = this.refs;
-        console.log(thread.offsetTop)
+        const { thread, threadWrap } = this.refs;
         if (thread.offsetTop > 0) {
             // Slide up...
+
+            $(threadWrap).nanoScroller({ sliderMinHeight: 60, stop: true })
+            
             return Velocity(thread, {top: "0"}, {
                 duration: 850,
-                easing: [0.25, 0.8, 0.25, 1]
+                easing: [0.25, 0.8, 0.25, 1],
+                complete: () => {
+                    $(threadWrap).nanoScroller({ stop: false })
+                }
             })
         }
 
