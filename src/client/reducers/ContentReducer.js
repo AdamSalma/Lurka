523a816bf59df6348576157229d8635971b72d2f
Adam Salma <adam3salma@gmail.com>
2016-10-29 00:13:26 +0100

    Combine into ContentReducer

--- src/client/reducers/ContentReducer.js
+++ src/client/reducers/ContentReducer.js
@@ -1,28 +1,67 @@
 import initialState from './initialState';
 import { 
     BOARD_LOADED, 
+    BOARD_REQUEST, 
+
     BOARD_LIST_LOADED, 
-    BOARD_LIST_REQUEST 
-    
-    BOARD_REQUEST 
+    BOARD_LIST_REQUEST,
+
+    THREAD_LOADED, 
+    THREAD_REQUEST, 
+    THREAD_DESTROY,
+
+    PROVIDER_CHANGE,
+    BOARD_CHANGE,
+    THREAD_CHANGE
 } from '../constants'
 
-export default function (state = initialState.board, action) {
+export default function (state = initialState.content, action) {
     switch (action.type) {
+
         case BOARD_REQUEST:
+        case THREAD_REQUEST:
+        case BOARD_LIST_REQUEST:
             return Object.assign({}, state, {
-                isFetching: true  
+                isFetching: true,
+                requestType: action.type  // for logging error to user...?
             })
 
         case BOARD_LOADED:
             return Object.assign({}, state, {
-                items: action.payload,
+                board.posts: action.payload,
                 isFetching: false
             })
 
         case BOARD_LIST_LOADED:
             return Object.assign({}, state, {
-                boardList: action.payload
+                boardList[action.key]: action.payload
+            })
+
+        case THREAD_LOADED:
+            return Object.assign({}, state, {
+                thread.posts: action.payload,
+                isFetching: false
+            })
+
+        case THREAD_DESTROY:
+            return Object.assign({}, state, {
+                thread.history[state.threadID]: state.thread.posts,
+                thread.posts = []
+            })
+
+        case PROVIDER_CHANGE:
+            return Object.assign({}, state, {
+                provider: action.payload
+            })
+
+        case BOARD_CHANGE:
+            return Object.assign({}, state, {
+                boardID: action.payload
+            })
+
+        case THREAD_CHANGE:
+            return Object.assign({}, state, {
+                threadID: action.payload
             })
 
         default:
