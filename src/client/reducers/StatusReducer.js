2b6b24270dbedff128fd3804c980f954e93182aa
Adam Salma <adam3salma@gmail.com>
2016-10-29 00:51:53 +0100

    Add status, comments

--- src/client/reducers/StatusReducer.js
+++ src/client/reducers/StatusReducer.js
@@ -1,17 +1,34 @@
-import { BOARD_CHANGE, PROVIDER_CHANGE } from '../constants';
 import initialState from './initialState';
+import { 
+	HEADER_SHRINKING, 
+	HEADER_EXPANDING, 
+	HEADER_ANIMATION_END, 
+	LOGO_SPIN_START, 
+	LOGO_SPIN_END, 
+	SCROLL_START, 
+	SCROLL_END,
+	APP_INIT
+} from '../constants';
 
 export default function (state = initialState.status, action) {
     switch (action.type) {
-        case BOARD_CHANGE:
-            return Object.assign({}, state, {
-                boardID: action.payload
+
+   		case APP_INIT:
+   			return Object.assign({}, state, {
+                isMainPage: true
             })
 
-        case PROVIDER_CHANGE:
-            return Object.assign({}, state, {
-                provider: action.payload
+    	case HEADER_EXPANDING:
+    	case HEADER_SHRINKING:
+    		return Object.assign({}, state, {
+                isAnimating: true
             })
+
+        case HEADER_ANIMATION_END:
+			return Object.assign({}, state, {
+                isAnimating: false,
+                isMainPage: !state.isMainPage
+            })        	
 
         default:
             return state
--- src/client/reducers/initialState.js
+++ src/client/reducers/initialState.js
@@ -1,14 +1,17 @@
 export default {	
 	status: {
-		isMainPage: true,
-		isAnimating: false,
+		providers: ["4chan", "reddit", "imgur"],
 
-		loadingText: "test",
-		mode: "normal"  // archive
+		isMainPage: true,
+		isAnimating: false,  // for header toggle
+		isScrolling: false,  // not user scroll
+		isLogoSpinning: false,  // log to user animation
+
+		loadingMessage: "test",  // log to user text
+		mode: "normal"  // "archive"
 
 	},
 	content: {
-		providers: ["4chan", "reddit", "imgur"],
 		provider: "4chan",
     	boardID: "g"
     	threadID: null
@@ -17,8 +20,8 @@ export default {
 		didInvalidate: false,
 		requestType: null,
 
-		boardlist: {  // obj for each provider: 4chan: {}, reddit: {}
-			favourites: {}
+		boardlist: {  // obj for each provider: {4chan: [], reddit: []}
+			favourites: []  // [{id:'4chan', board: 'g'}, ...]
 		},  
 		board: {
 			history: {},
