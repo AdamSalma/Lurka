9189ebe741b596ffb68083efbfde890b43a86269
Adam Salma <adam3salma@gmail.com>
2016-11-02 18:47:00 +0000

    Add tooltip component

--- src/client/components/Tooltip/Tooltip.jsx
+++ src/client/components/Tooltip/Tooltip.jsx
@@ -1,11 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
+import classNames from 'classnames';
 
-export default ({children}) => {
-    return (
-        <div className='tooltip'>
-            {children}
-        </div>
-    )
+export default class Tooltip extends Component {
+	constructor(props) {
+		super(props);
+		this.showTooltip = this.showTooltip.bind(this)
+		this.hideTooltip = this.hideTooltip.bind(this)
+		this.state = {
+			isVisible: false
+		}
+	}
+
+	showTooltip() {
+		this.setState({
+			isVisible: true
+		})
+	}
+
+	hideTooltip() {
+		this.setState({
+			isVisible: false
+		})
+	}
+
+	render() {
+		const content = classNames("tooltip-content", {
+			'tooltip-active': this.state.isVisible
+		})
+
+		return (
+			<div className={this.props.className || "tooltip"}>
+				<div
+					className="tooltip-target"
+					onMouseEnter={this.showTooltip}
+					onMouseLeave={this.hideTooltip}
+				>
+					{this.props.children}
+				</div>
+				<div className={content}>
+					{this.props.content}
+				</div>
+			</div>
+		)
+	}
 }
 
 
