2a8ce140d9c0b622fec5d1bdcaec96745c61d8c7
Adam Salma <adam3salma@gmail.com>
2016-11-05 03:12:04 +0000

    Add redux devtools

--- src/client/store/index.js
+++ src/client/store/index.js
@@ -1,13 +1,16 @@
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import thunkMiddleware from 'redux-thunk'
 import createLogger from 'redux-logger'
 import rootReducer from '../reducers'
 
 export default function configureStore(preloadedState) {
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
   const store = createStore(
     rootReducer,
     preloadedState,
-    applyMiddleware(thunkMiddleware, createLogger())
+    composeEnhancers(
+      applyMiddleware(thunkMiddleware, createLogger())
+    )
   )
 
   if (module.hot) {
