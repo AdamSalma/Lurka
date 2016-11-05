b81e8919c500d8604b0297caccdad36e2c084a51
Adam Salma <adam3salma@gmail.com>
2016-11-05 03:21:32 +0000

    Add ContentOptions to Header

--- src/client/components/Header/Header.jsx
+++ src/client/components/Header/Header.jsx
@@ -4,19 +4,37 @@ import Velocity from 'velocity-animate';
 import classNames from "classnames";
 
 import Logo from "../Logo";
+import ContentOptions from "../ContentOptions";
 
 export default class Header extends Component {
     constructor(props) {
         super(props);
         this.onHeaderExpand = this.onHeaderExpand.bind(this)
+        this.createOptions = this.createOptions.bind(this)
     }
     render() {
-        const {isMainPage, loadingMessage, expandHeader, scrollPage} = this.props;
+        const {
+            isMainPage, loadingMessage, expandHeader, scrollPage
+        } = this.props;
+
+        console.info(`isMainPage in Header? ${isMainPage}`)
         return (
             <div id="header">
                 <Logo isFullsize={isMainPage} loadingMessage={loadingMessage} expandHeader={this.onHeaderExpand}/>
+                {this.createOptions( isMainPage )}
             </div>
         )
+    }
+
+    createOptions( isMainPage ) {
+        const {boardList, fetchBoardList, provider, fetchBoard} = this.props
+        if (!isMainPage)
+            return <ContentOptions 
+                        provider={provider} 
+                        boardList={boardList} 
+                        fetchBoard={fetchBoard}
+                        fetchBoardList={fetchBoardList}/>
+
     }
 
     onHeaderExpand() {
