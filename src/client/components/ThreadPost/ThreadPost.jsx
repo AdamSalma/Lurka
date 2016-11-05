0742f02e92ff2f29c0b956288a5baed103e7c980
Adam Salma <adam3salma@gmail.com>
2016-11-05 03:24:05 +0000

    Add backrefs to thread posts; check who refered

--- src/client/components/ThreadPost/ThreadPost.jsx
+++ src/client/components/ThreadPost/ThreadPost.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import uuid from 'uuid'
 import {
     createMediaIfExists
 } from './Media'
 
 export default function ({post, children}) {
-    const { id, title, date, imgsrc, comment, ext } = post
+    const { id, title, date, imgsrc, comment, ext, references } = post
 
     const SRC = imgsrc
     const ID = 'post-media-' + id
@@ -15,7 +16,7 @@ export default function ({post, children}) {
                 {() => {if (title) return <span className='thread-post-title'>{title}</span>}}
                 {children}
                 <span className='thread-post-number'>No.{id}</span>
-                <span className='thread-post-backlink'></span>
+                <span className='thread-post-backlink'>{renderRefs(references)}</span>
                 <span className='mdi mdi-dots-vertical thread-post-options'></span>
             </div>
             {createMediaIfExists(ID, SRC, ext)}
@@ -25,3 +26,13 @@ export default function ({post, children}) {
 }
 
 
+function renderRefs(refs) {
+    if (refs)
+        return refs.map( ref => 
+            <a 
+                href={`#p${ref}`} 
+                className="quotelink refered" 
+                key={uuid.v4()}
+            >{`>>${ref}`}</a>);
+
+}
