7358d0e153bef242b7de89912ec05ec286190af6
Adam Salma <adam3salma@gmail.com>
2016-11-05 03:11:26 +0000

    Fix production comparasion

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -8,7 +8,7 @@ import App from './containers/App';
 import configureStore from './store';
 import { loadState, saveState } from './store/localStorage';
 
-const isProd = process.env.NODE_ENV !== "production"
+const isProd = process.env.NODE_ENV === "production"
 const preloadedState = isProd ? loadState() : undefined;
 const store = configureStore(preloadedState);
 
