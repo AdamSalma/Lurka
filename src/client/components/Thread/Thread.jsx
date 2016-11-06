a2a3809bc5fe10c466ffac3880bd619be1550d3f
Adam Salma <adam3salma@gmail.com>
2016-11-06 23:22:32 +0000

    Add unidirectional thread operations

--- src/client/components/Thread/Thread.jsx
+++ src/client/components/Thread/Thread.jsx
@@ -16,88 +16,93 @@ import delegateMediaFullscreen from './makeFullscreen'
 import delegatePostScroll from './scrollToPost'
 
 export default class Thread extends Component {
-    constructor() {
-        super()
-        this.threadToggle = this.threadToggle.bind(this)
+    constructor(props) {
+        super(props)
+        this.openThread = this.openThread.bind(this)
+        this.closeThread = this.closeThread.bind(this)
     }
 
     componentDidMount() {
         const {thread, threadWrap} = this.refs;
         delegateMediaFullscreen(thread)
-        delegatePostScroll(this.refs.thread)
+        delegatePostScroll(thread)
         // $(threadWrap).nanoScroller({ sliderMinHeight: 40, alwaysVisible: true })
     }
 
-    // componentWillUnmount() {
-    //     $(this.refs.thread).off('click');
-    // }
-
-    componentDidUpdate( prevProps ) {
+    componentDidUpdate({ thread: oldthread } ) {
         const { thread } = this.props;
-        if (prevProps.thread.posts.length !== thread.posts.length){
-            // Thread created or destroyed
-            this.threadToggle();
-        } 
+        // if (oldthread.posts.length !== thread.posts.length) {
+        //     // Thread created or destroyed
+        //     this.threadToggle();
+        // }
+
+        if (!oldthread.posts.length && thread.posts.length) {
+            // New thread
+            this.openThread()
+            $(this.refs.thread).nanoScroller({ scroll: "top" })
+        }
+
     }
 
-    shouldComponentUpdate( nextProps ) {
-        return this.props.thread.posts.length != nextProps.thread.posts.length
+    shouldComponentUpdate({ thread: newthread }) {
+        return this.props.thread.posts.length != newthread.posts.length
     }
 
     render() {
-        const { thread, isFetching } = this.props
-        const threadWrapClasses = classNames('thread-wrap', 'nano', {
-            "thread-wrap-active": thread.posts.length || isFetching
-        });
-        const threadIsVisible = classNames({
-            "hidden": thread.posts.length || isFetching
-        })
+        const { thread, isFetching } = this.props, { posts } = thread;
 
-        if (thread.posts.length) {
-            console.info(`Rendering Thread with ${thread.posts.length} posts`);
+        const threadWrapClasses = classNames('thread-wrap', 'nano', {
+            "thread-wrap-active": posts.length || isFetching
+        });
+        // const threadIsVisible = classNames({
+        //     "hidden": posts.length || isFetching
+        // })
+
+        if (posts.length) {
+            console.info(`Rendering Thread with ${posts.length} posts`);
         }
 
         return (
             <div>
                 <Background 
-                    isVisible={thread.posts.length || isFetching} 
-                    closeBackground={this.threadToggle}/>
+                    isVisible={posts.length || isFetching} 
+                    closeBackground={this.closeThread}/>
                 <div ref='threadWrap' className={threadWrapClasses}>
                     <div className="thread nano-content" ref="thread">
-                        {thread.posts.map( 
-                            post => <ThreadPost 
-                                key={uuid.v4()} 
-                                post={post}>
-                                <TimeAgo date={post.time}/>
-                            </ThreadPost>
-                        )}
+                        {this.createPosts( posts )}
                     </div>
                 </div>
             </div>
         )
     }
 
-    threadToggle() {
-        const { thread, threadWrap } = this.refs;
-        if (thread.offsetTop > 0) {
-            // Slide up...
-            
-            return Velocity(thread, {top: "0"}, {
-                duration: 850,
-                easing: [0.25, 0.8, 0.25, 1],
-                complete: () => {
-                    $(threadWrap).nanoScroller()
-                }
-            })
-        }
-
-        // Slide down...
-        this.props.closeThread()
-        // $('#thread').css('top', window.innerHeight + "px");
-        Velocity(
-            thread, 
-            {top: window.innerHeight + "px"}, 
-            {duration: 10}
+    createPosts( posts ) {
+        return posts.map( 
+            post => <ThreadPost 
+                key={uuid.v4()} 
+                post={post}>
+                <TimeAgo date={post.time}/>
+            </ThreadPost>
         )
+    }
+
+    openThread() {
+        const { thread, threadWrap } = this.refs;
+        Velocity(thread, {top: "0"}, {
+            duration: 850,
+            easing: [0.25, 0.8, 0.25, 1],
+            complete: () => {
+                $(threadWrap).nanoScroller()
+            }
+        });
+    }
+
+    closeThread() {
+        const { thread } = this.refs, { closeThread } = this.props;
+        
+        Velocity( thread, {top: window.innerHeight + "px"}, {
+            duration: 100,
+            complete: closeThread
+        })
     }
 }
