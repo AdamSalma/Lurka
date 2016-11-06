ac075922b4ad7065d8e7f99d36ba21d0fae421b8
Adam Salma <adam3salma@gmail.com>
2016-11-06 22:26:24 +0000

    Tidy app.js

--- src/server/app.js
+++ src/server/app.js
@@ -1,34 +1,29 @@
 import Express from 'express'
 import config from '../../config'
 import webpackHotMiddleware from './middleware/webpackMiddleware'
+import logRequests from './middleware/logRequests'
 import { join } from 'path';
  
 const app = Express();
-const router = Express.Router();
+const isProd = config.env === 'production'
+
+
+// Used to send index.html
+global.app_root = isProd ? __dirname : join(__dirname, '../..', 'app')
 
  
-// Environment config
 console.info(`Environment: "${config.env}"`);
-if (config.env === 'production') {
-    // Bundled production code is placed in the app directory
-    global.app_root = __dirname  
+if (isProd) {
     app.use(Express.static(__dirname));
     app.use(Express.static(join(__dirname, 'assets')));
 } else {
-    // Used to send index.html
-    global.app_root = join(__dirname, '../..', 'app');
-    // Static assets - mainly for favicon because its cool
     app.use(Express.static(join(__dirname, '..', 'assets')));
     webpackHotMiddleware(app);
-    // Log requested url
-    app.all('*', (req, res, next) => {
-        console.info("User request:", req.url);
-        next()
-    });
 }
  
  
 // Routes
+app.all('*', logRequests);
 app.use('/', require('./routes/dashboard'));  // index.html, (validation?)
 app.use('/provider', require('./routes/provider'));  // Request content from external API
 
