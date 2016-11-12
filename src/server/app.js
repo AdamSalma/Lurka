f95696626127055586b39fd9c848fe0c5f881e71
Adam Salma <adam3salma@gmail.com>
2016-11-12 17:30:26 +0000

    Add 404 handler in app.js

--- src/server/app.js
+++ src/server/app.js
@@ -3,16 +3,15 @@ import config from '../../config'
 import webpackHotMiddleware from './middleware/webpackMiddleware'
 import logRequests from './middleware/logRequests'
 import { join } from 'path';
- 
+
 const app = Express();
 const isProd = config.env === 'production'
-
 
 // Used to send index.html
 global.app_root = isProd ? __dirname : join(__dirname, '../..', 'app')
 
  
-console.info(`Environment: "${config.env}"`);
+log.app(`Environment: "${config.env}"`);
 if (isProd) {
     app.use(Express.static(__dirname));
     app.use(Express.static(join(__dirname, 'assets')));
@@ -30,6 +29,18 @@ app.use('/provider', require('./routes/provider'));  // Request content from ext
 // Eventually...
 // app.use('/user', require('./routes/user'));  // Save/Load user archives
 // app.use('/settings', require('./routes/settings'));  // Save/Load setting related files (config, stylesheets?)
+
+// 404 handler
+app.use((req, res, next) => {
+    if (isProd) {
+        var err = new Error('Not Found');
+        log.error(err.message);
+        res.send(err)
+    } else {
+        log.error("Not found")
+        res.status(404);
+    }
+});
 
 
 export default app;
