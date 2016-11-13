eff87476ccf0f0485932e11b07d51878e9400f62
Adam Salma <adam3salma@gmail.com>
2016-11-13 01:26:18 +0000

    Add proptypes to tooltip

--- src/client/components/Tooltip/Tooltip.jsx
+++ src/client/components/Tooltip/Tooltip.jsx
@@ -1,48 +1,58 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import classNames from 'classnames';
 
 export default class Tooltip extends Component {
-	constructor(props) {
-		super(props);
-		this.showTooltip = this.showTooltip.bind(this)
-		this.hideTooltip = this.hideTooltip.bind(this)
-		this.state = {
-			isVisible: false
-		}
-	}
+    constructor(props) {
+        super(props);
+        this.showTooltip = this.showTooltip.bind(this)
+        this.hideTooltip = this.hideTooltip.bind(this)
+        this.state = {
+            isVisible: false
+        }
+    }
 
-	showTooltip() {
-		this.setState({
-			isVisible: true
-		})
-	}
+    showTooltip() {
+        this.setState({
+            isVisible: true
+        })
+    }
 
-	hideTooltip() {
-		this.setState({
-			isVisible: false
-		})
-	}
+    hideTooltip() {
+        this.setState({
+            isVisible: false
+        })
+    }
 
-	render() {
-		const content = classNames("tooltip-content", {
-			'tooltip-active': this.state.isVisible
-		})
+    render() {
+        const { className, content } = this.props
+        const contentClasses = classNames("tooltip-content", {
+            'tooltip-active': this.state.isVisible
+        })
 
-		return (
-			<div className={this.props.className || "tooltip"}>
-				<div
-					className="tooltip-target"
-					onMouseEnter={this.showTooltip}
-					onMouseLeave={this.hideTooltip}
-				>
-					{this.props.children}
-				</div>
-				<div className={content}>
-					{this.props.content}
-				</div>
-			</div>
-		)
-	}
+        return <div className={`tooltip ${className}`}>
+            <div
+                className="tooltip-target"
+                onMouseEnter={this.showTooltip}
+                onMouseLeave={this.hideTooltip}
+            >
+                {this.props.children}
+            </div>
+            <div className={contentClasses}>
+                {this.props.content}
+            </div>
+        </div>
+    }
 }
 
 
+Tooltip.defaultProps = {
+    className: ''
+}
+
+Tooltip.propTypes = {
+    className: PropTypes.string.isRequired,
+    content: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.element
+    ])
+}
