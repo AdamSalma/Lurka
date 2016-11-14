a909b5fd193881ae324dcc84c53438ee13595db6
Adam Salma <adam3salma@gmail.com>
2016-11-14 23:36:00 +0000

    Add filename and linenumber to server logger

--- src/server/helpers/logger.js
+++ src/server/helpers/logger.js
@@ -1,27 +1,46 @@
+'use strict'
+
 var clc = require('cli-color');
 var moment = require('moment');
 
+var loggerWidth = 109
+
 module.exports = {
     info: function(text) {
-        console.log(`${getTime()} ${clc.greenBright("Info")} - ${text}`)
+        format(`${clc.greenBright("Info")} - ${text}`)
     },
-    warn: function(text) {
-        console.log(`${getTime()} ${clc.yellow("Warning")} - ${text}`)
+    warning: function(text) {
+        format(`${clc.yellow("Warning")} - ${text}`)
     },
     error: function(text) {
-        console.log(`${getTime()} ${clc.redBright("Error")} - ${text}`)
+        format(`${clc.redBright("Error")} - ${text}`)
     },
     http: function(text) {
-        console.log(`${getTime()} ${clc.cyanBright("Http")} - ${text}`)
+        format(`${clc.cyanBright("Http")} - ${text}`)
     },
     app: function(text) {
-        console.log(`${getTime()} ${clc.magentaBright("App")}  - ${text}`)
+        format(`${clc.magentaBright("App ")} - ${text}`)
     },
     custom: function(type, color, text) {
-        console.log(`${getTime()} ${clc[color](type)}  - ${text}`)
+        format(`${clc[color](type)} - ${text}`)
     }
 }
 
 function getTime(){
     return moment().format('H:mm:ss')
+}
+
+function getPath(){
+    let callee = (new Error).stack.split("\n")[4]
+    return callee.split('\\').pop().split('/').pop()  // handles \ or /
+}
+
+function format(string) {
+    let callee = getPath(), time = getTime()
+    let path = callee.split(':')
+    let spaces = loggerWidth - (string.length + callee.length + time.length)
+    let newString = time +' '+ string +
+                    Array(spaces > 0 ? spaces : 2).join(" ") +    // add spaces if possible
+                    clc.underline(path[0])+':'+clc.bold(path[1])  // color path and join
+    console.info(newString)
 }
