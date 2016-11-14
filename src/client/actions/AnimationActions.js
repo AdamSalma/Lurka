985bf6ab604f515549bfce7e18698699f89bfcf3
Adam Salma <adam3salma@gmail.com>
2016-11-14 22:46:09 +0000

    Remove header grow/shrink animations

--- src/client/actions/AnimationActions.js
+++ src/client/actions/AnimationActions.js
@@ -1,11 +1,8 @@
 import Velocity from 'velocity-animate'
 
 import {
-	SCROLL_START,
-	SCROLL_END,
-    HEADER_ANIMATION_END,
-    HEADER_SHRINKING,
-    HEADER_EXPANDING
+	SCROLL_STARTED,
+	SCROLL_ENDED
 } from '../constants'
 
 export function scrollPage({ content=false, mainPage=false })  {
@@ -16,80 +13,14 @@ export function scrollPage({ content=false, mainPage=false })  {
 	const offset = content ? window.innerHeight : 0;
 
 	return dispatch => {
-        dispatch({type: SCROLL_START});
+        dispatch({type: SCROLL_STARTED});
         return Velocity(document.body, 'scroll', {
         	offset: offset, 
         	duration: 1000, 
         	easing: "ease-in-out", 
         	complete: () => {
-        		dispatch({type: SCROLL_END})
+        		dispatch({type: SCROLL_ENDED})
         	}
        	});
-    }
-}
-
-
-export function shrinkHeader() {
-    const header = document.querySelector('#header');
-    const logo   = document.querySelector('#logo');
-    const $board  = $('#board');
-
-    const logoDistanceFromLeft = window.innerWidth - 90 + "px"
-    let animationCompleteCount = 0
-
-    $board.nanoScroller({ sliderMaxHeight: 120, sliderMinHeight: 60, stop: true })
-
-    return dispatch => {
-        dispatch({type: HEADER_SHRINKING})
-
-        Velocity(header, {height: "70px"}, {
-            duration: 1000,
-            complete: isComplete
-
-        })
-
-        Velocity(logo, {right: logoDistanceFromLeft, top: "10px", width: "50px"}, {
-            duration: 1000,
-            easing: "ease-out",
-            complete: isComplete
-        })
-
-        function isComplete() {
-            animationCompleteCount++
-            if (animationCompleteCount == 2) {
-                dispatch({type: HEADER_ANIMATION_END})
-                $board.nanoScroller({ stop: false })
-            }
-        }
-        
-    }
-}
-
-export function expandHeader() {
-    const header = document.querySelector('#header');
-    const logo   = document.querySelector('#logo');
-    let animationCompleteCount = 0
-
-    return dispatch => {
-        dispatch({type: HEADER_EXPANDING})
-
-        Velocity( header, {height: "270px"}, {
-            duration: 1000, 
-            easing: "ease-out",
-            complete: isComplete
-        })
-
-        Velocity(logo, {right: 0, top: "70px", width: "200px"}, {
-            duration: 1000,
-            easing: "ease-out",
-            complete: isComplete
-        })
-
-        function isComplete() {
-            animationCompleteCount++
-            if (animationCompleteCount == 2) {
-                dispatch({type: HEADER_ANIMATION_END})
-            }
-        }
     }
 }
