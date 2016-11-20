400b536263273f33e36de391cdc02c8067787470
Adam Salma <adam3salma@gmail.com>
2016-11-20 15:03:40 +0000

    Combine refactored reducers

--- src/client/reducers/index.js
+++ src/client/reducers/index.js
@@ -1,11 +1,15 @@
 import { combineReducers } from 'redux';
-import ContentReducer from "./ContentReducer";
+import ThreadReducer from "./ThreadReducer";
+import BoardReducer from "./BoardReducer";
+import BoardlistReducer from "./BoardlistReducer";
 import StatusReducer from "./StatusReducer";
 import SettingsReducer from "./SettingsReducer";
 
-
+// TODO: Add PostReducer
 const rootReducer = combineReducers({
-    content: ContentReducer,
+    boardlist: BoardlistReducer,
+    board: BoardReducer,
+    thread: ThreadReducer,
     status: StatusReducer,
     settings: SettingsReducer
 });
