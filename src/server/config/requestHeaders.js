7d81cb52da451bb7c0930f063c9e493bf5f5d9b5
Adam Salma <adam3salma@gmail.com>
2016-11-20 21:53:54 +0000

    Add 4chan headers

--- src/server/config/requestHeaders.js
+++ src/server/config/requestHeaders.js
@@ -1,12 +1,17 @@
-export const chan = {
-    headers: {
-        'Accept': '*/*',
-        'Accept-Encoding': 'gzip, deflate, sdch',
-        'Host': 'a.4cdn.org',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:49.0)' /
-                      'Gecko/20100101 Firefox/49.0',
-        'Origin': 'http://boards.4chan.org',
-        'Referer': 'http://boards.4chan.org/',
-        'If-Modified-Since': '0'
+export default {
+    chan: {
+        headers: {
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+            "Accept-Encoding": "gzip, deflate, sdch",
+            "Accept-Language": "en-US,en;q=0.8",
+            "Host": "a.4cdn.org",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36",
+            "Origin": "a.4cdn.org",
+            "Referer": "http://boards.4chan.org/",
+            "If-Modified-Since": "0",
+            "DNT": 1,
+            "Upgrade-Insecure-Requests": 1
+        }
     }
 }
+
