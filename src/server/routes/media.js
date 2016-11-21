ee37d9e6511860cdc5275fc5c1701fcdb379338c
Adam Salma <adam3salma@gmail.com>
2016-11-21 00:49:09 +0000

    Add media proxy route

--- src/server/routes/media.js
+++ src/server/routes/media.js
@@ -1,8 +1,20 @@
 import Express from "express";
-const router = Express.Router();
+import request from "request";
+import headers from '../config/requestHeaders'
 
-router.get('/', function(res, req, next){
-    log.warning("Media requested")
+const router = Express.Router();
+var options;
+
+router.get('/', function(req, res, next){
+    const {url, provider} = req.query
+    
+    if (provider === "4chan") {
+        options = headers.chan
+        options.headers["host"] = "i.4cdn.org"
+    }
+
+    options.url = unescape(url)
+    request.get(options).pipe(res);
 })
 
 export default router
