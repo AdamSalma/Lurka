b3582d8361b8c7bb562968718a73e8b23d4a9db4
Adam Salma <adam3salma@gmail.com>
2016-11-21 00:53:00 +0000

    Refactor proxy url generator

--- src/server/services/proxyUrls.js
+++ src/server/services/proxyUrls.js
@@ -1,23 +1,5 @@
-import query from 'query-string'
-const route = '/media'
+import query from 'querystring';
 
-/**
- * Takes a url and returns a new href that routes to /media with the url as 
- * a parameter
- * @param  {[type]} options.url      - external media href
- * @param  {[type]} options.provider - content provider; 4cha, reddit etc
- * @return {[type]}                  - internal href with url as a param
- */
-export default function proxify({ url, provider }) {
-    const parsed = query.parse(route)
-    log.info(`parsed: ${parsed}`)
-    throw new Error
-    parsed.url = url
-    parsed.provider = provider
-    return query.stringify(parsed)
-
-    return `${route}?` query.stringify({
-        url: url,
-        provider: provider
-    })
+export default function proxify(baseUrl, params) {
+    return baseUrl + "?" + query.stringify(params)
 }
