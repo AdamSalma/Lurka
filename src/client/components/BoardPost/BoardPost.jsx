9bd19e0a58734ebc5d6be80f3db0d419b38599aa
Adam Salma <adam3salma@gmail.com>
2016-11-22 23:52:16 +0000

    Add board post onhover shows more of comment

--- src/client/components/BoardPost/BoardPost.jsx
+++ src/client/components/BoardPost/BoardPost.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Tooltip from '../Tooltip';
+import Velocity from "velocity-animate"
 
 export default ({ post, fetchThread, reshuffle }) => {
     const {id, title, comment, date, imgsrc, replies} = post;
@@ -8,13 +9,17 @@ export default ({ post, fetchThread, reshuffle }) => {
             id={"t"+id}
             className="board-post"
             onClick={() => fetchThread(id)}
+            onMouseEnter={moveCommentUp.bind(null, id)}
         >
-            
-            <img src={imgsrc.sm} onLoad={reshuffle}/>
-            {renderStatusBar(replies)}
-            <div className="op">
-                <b dangerouslySetInnerHTML={{__html: title}} className="title" />
-                <div dangerouslySetInnerHTML={{__html: comment}} />
+            <div className="image-wrap">   
+                <img src={imgsrc.sm} onLoad={reshuffle}/>
+            </div>   
+            <div className="comment-wrap">
+                {renderStatusBar(replies)}
+                <div className="op">
+                    <b dangerouslySetInnerHTML={{__html: title}} className="title" />
+                    <div dangerouslySetInnerHTML={{__html: comment}} />
+                </div>
             </div>
         </div>
     )
@@ -42,4 +47,32 @@ function renderStatusBar(replies) {
             </span>
         </span>
     </div>
+}
+
+const maxHeight = 400;
+const maxMoveDistance = 100;
+function moveCommentUp(id) {
+    const 
+        $target = $("#t"+id),
+        kids = $target.children(),
+        commentHeight = kids[1].clientHeight,
+        imgHeight = kids[0].clientHeight;
+
+    console.info($target)
+    if (imgHeight + commentHeight > maxHeight ) {
+        // comment is long and offscreen; make it more viewable by moving it up
+        
+        // move by difference, else if that would move comment outside thread, use image height (scroll to top) ;
+        const offScreenAmount = commentHeight - (maxHeight - imgHeight) + 5 // 5px for padding at bottom of comment
+        const height = imgHeight/1.5 // 3/4 of image height
+        const distance = offScreenAmount < height ? offScreenAmount : height
+        Velocity(kids[1], {top: `-${distance}px`}, 200, "ease-out")
+        $target.one('mouseleave', ()=>Velocity(kids[1], {top: 0}, 200))
+
+    }
+
+}
+
+function revertCommentDown() {
+
 }
