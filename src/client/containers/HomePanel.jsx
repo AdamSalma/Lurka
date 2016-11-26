cc569c443309c6b9c62ddc7d2db3522a3a0bb725
Adam Salma <adam3salma@gmail.com>
2016-11-26 17:33:09 +0000

    Refactor HomePanel

--- src/client/containers/HomePanel.jsx
+++ src/client/containers/HomePanel.jsx
@@ -2,18 +2,17 @@ import React, { Component } from "react";
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 
-import { Link } from "react-router";
-
 import Velocity from 'velocity-animate';
 
 import Header from "../components/Header";
+import BoardLists from "../components/BoardLists";
 
 import {
-	changeProvider
+    changeProvider
 } from '../actions/StatusActions';
 
 import {
-	scrollPage
+    scrollPage
 } from '../actions/AnimationActions';
 
 import {
@@ -23,62 +22,38 @@ import {
 // import scroll action here
 
 class HomePanel extends Component {
-	constructor() {
-		super();
-		this.onProviderClick = this.onProviderClick.bind(this);
-	}
+    constructor() {
+        super();
+    }
 
     render() {
-		const {
-			scrollPage, fetchBoard, fetchBoardList,
-			status, boardlist, threadIsActive
-		} = this.props;
+        const {
+            scrollPage, fetchBoard, fetchBoardList, changeProvider,
+            status, boardList, threadIsActive
+        } = this.props;
         return (
-        	<div id="pages">
-	            <div className="page page-home">
-	            	<Header 
+            <div id="pages">
+                <div className="page page-home">
+                    <Header 
                         scrollPage={scrollPage} fetchBoardList={fetchBoardList} fetchBoard={fetchBoard}
-	                    statusMessage={status.statusMessage} boardList={boardlist} provider={status.provider}
+                        statusMessage={status.statusMessage} boardList={boardList} provider={status.provider}
                         boardID={status.boardID} threadID={status.threadID} threadIsActive={threadIsActive}
-	           		/>  
-	            	<div>
-	            		<h3>Providers:</h3>
-	            		<div className="providers">
-	            		{this.renderProviders(status.providers)}
-	            		</div>
-	            	</div>
-	            </div>
-	            {this.props.children}
-	        </div>
+                    />  
+                    <BoardLists 
+                        scrollPage={scrollPage} fetchBoardList={fetchBoardList} fetchBoard={fetchBoard} changeProvider={changeProvider}
+                        boardList={boardList} provider={status.provider} status={status}
+                    />
+                </div>
+                {this.props.children}
+            </div>
         )
-    }
-
-    renderProviders( providers ) {
-        console.warn("Rendering providers out... " + providers)
-        return providers.map( 
-            provider => <Link key={provider} to="/content">
-                <input 
-                    type="button" 
-                    value={provider} 
-                    onClick={()=>this.onProviderClick(provider)}/>
-            </Link>
-        )
-
-    }
-
-    onProviderClick(provider) {
-    	const {scrollPage, changeProvider} = this.props;
-        // TODO: be able to change provider and board
-        changeProvider(provider)
-        scrollPage({content: true})
-    	
     }
 }
 
-function mapStateToProps({status, boardlist, thread}) {
+function mapStateToProps({status, boardList, thread}) {
     return {
         status,
-        boardlist,
+        boardList,
         threadIsActive: thread.isActive
     }
 }
