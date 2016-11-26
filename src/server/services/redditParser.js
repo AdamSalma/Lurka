206eea821cf0fc3dcfeaa049d0c7d7876e2e1e8a
Adam Salma <adam3salma@gmail.com>
2016-11-26 17:51:28 +0000

    Initial Reddit parser

---
+++ src/server/services/redditParser.js
@@ -0,0 +1,20 @@
+import fs from 'fs'
+export function parseBoardList(boardList) {
+    // console.log(JSON.stringify(boardList))
+    try {
+        boardList.data.children
+    } catch (e) {
+        log.error(`Bad reddit data returned => ${e}`)
+    }
+
+    const boards = boardList.data.children.map(board => board.data) // thanks, reddit!
+
+    return boards.map(({url, title}) => {
+        let boardID = url.split('/').slice(-2)[0]
+        console.log(boardID)
+        return {
+            boardID,
+            description: `${url} - ${title}`
+        }
+    });
+}
