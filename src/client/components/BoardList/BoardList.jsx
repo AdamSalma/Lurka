d8eabea64b85ae0e06f3a1cc88a1f3ff318a8824
Adam Salma <adam3salma@gmail.com>
2016-11-27 23:49:54 +0000

    Add searching to boardlist

--- src/client/components/BoardList/BoardList.jsx
+++ src/client/components/BoardList/BoardList.jsx
@@ -4,11 +4,17 @@ import classNames from 'classnames';
 import uuid from 'uuid';
 
 import Dropdown from '../Dropdown';
+import SearchBox from '../SearchBox';
 
 export default class BoardList extends Component {
     constructor({boardList, provider, fetchBoardList}) {
         super()
-        this.renderBoardListIfExists = this.renderBoardListIfExists.bind(this)
+        this.handleKeyUp = this.handleKeyUp.bind(this)
+        this.renderBoardList = this.renderBoardList.bind(this)
+
+        this.state = {
+            keyword: ''
+        }
 
         if (!boardList) {
             fetchBoardList(provider);
@@ -16,32 +22,59 @@ export default class BoardList extends Component {
     }
 
     render() {
-        const classes = classNames("board-list")  // TODO: add to boardlist
+        const {boardList} = this.props
+        const hasBoards = boardList && boardList.length;
 
+        return hasBoards ? this.renderBoardList() : false
+    }
+
+
+    renderBoardList() {
+        const { boardList, provider, handleClick } = this.props;
         return (
-            <div className={classes}>
-                {this.renderBoardListIfExists()}
+            <div className={`board-list ${provider}`} 
+                 onClick={this.toggleBoardList}
+            >
+                <span className="placeholder">{provider}</span>
+                <div className="content">
+                    <SearchBox
+                        className="search"
+                        onKeyUp={this.handleKeyUp}
+                        placeholder={`Search ${provider}`}
+                    />
+                    <Dropdown 
+                        className={provider}
+                        items={this.renderDropdownChildren(boardList)}
+                        handleClick={handleClick}
+                    />
+                </div>
             </div>
         )
     }
+    
 
-    renderBoardListIfExists() {
-        const { boardList, provider, handleClick } = this.props;
+    handleKeyUp(event) {
+        this.setState({
+            keyword: event.target.value.toLowerCase()
+        })
+    }
 
-        if (boardList && boardList.length) {
-            return <Dropdown 
-                className={`boardList ${provider}`}
-                items={this.renderDropdownChildren(boardList)}
-                handleClick={handleClick}
-            />
-        } 
+    toggleBoardList(event) {
+        console.log("clicked on boardlist");
+        $(event.target).closest('.board-list').toggleClass('bl-active')
     }
 
     renderDropdownChildren(boardList) {
-        return boardList.map( ({boardID, description}) => (
+
+        const boards = boardList.filter( 
+            ({description}) => 
+                description.toLowerCase().includes(this.state.keyword)
+        )
+        return boards.map( ({boardID, description}) => (
             <div key={uuid.v4()} data-value={boardID}>
                 {description}
             </div>
         ));
     }
+
 }
