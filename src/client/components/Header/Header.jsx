9304b043a4c0546ca964fdddc6efbebe62aed176
Adam Salma <adam3salma@gmail.com>
2016-11-27 23:58:39 +0000

    Add header scroll

--- src/client/components/Header/Header.jsx
+++ src/client/components/Header/Header.jsx
@@ -4,15 +4,15 @@ import Velocity from 'velocity-animate';  // TODO: Is this needed? Logo?
 import classNames from "classnames";
 
 import Logo from "../Logo";
-import BoardList from "../BoardList";
+// import BoardList from "../BoardList";
 import SearchBox from "../SearchBox";
 
 export default class Header extends Component {
     constructor(props) {
         super(props);
-        this.createOptions = this.createOptions.bind(this)
-        this.createStatusText = this.createStatusText.bind(this)
-        this.createSearchbox = this.createSearchbox.bind(this)
+        // this.createBoardList = this.createBoardList.bind(this)
+        this.renderStatusText = this.renderStatusText.bind(this)
+        this.renderSearchbox = this.renderSearchbox.bind(this)
         this.state = {
             fadeOut: false
         }
@@ -25,37 +25,45 @@ export default class Header extends Component {
         }
     }
 
+    componentDidUpdate(prevProps, prevState) {
+        console.info("header updated")
+        const {currentPage, scrollHeader} = this.props;
+        if (prevProps.currentPage !== currentPage) {
+            scrollHeader((currentPage === "content"))
+        }
+    }
+
     render() {
-        const { scrollPage, statusMessage } = this.props;
+        const { scrollPage, statusMessage, currentPage } = this.props;
         return (
             <div id="header" className="header">
                 <div className="header-content">
                     <Logo scrollPage={scrollPage} statusMessage={statusMessage}/>
-                    {this.createStatusText()}
-                    {this.createSearchbox()}
-                    {this.createOptions()}
+                    {this.renderStatusText()}
+                    {this.renderSearchbox()}
+                    {/*this.createBoardList()*/}
                 </div>
             </div>
         )  // TODO: Add search/filter functionality
     }
 
-    createOptions() {
-        const {boardList, fetchBoardList, provider, fetchBoard} = this.props
-        return <BoardList 
-            provider={provider} 
-            boardList={boardList[provider]} 
-            fetchBoard={fetchBoard}
-            fetchBoardList={fetchBoardList}
-        />
-    }
+    // createBoardList() {
+    //     const {boardList, fetchBoardList, provider, fetchBoard} = this.props
+    //     return <BoardList 
+    //         provider={provider} 
+    //         boardList={boardList[provider]} 
+    //         fetchBoard={fetchBoard}
+    //         fetchBoardList={fetchBoardList}
+    //     />
+    // }
 
-    createSearchbox() {
+    renderSearchbox() {
         const {threadIsActive} = this.props
         const placeholder = `Search ${threadIsActive ? "thread" : "board"} ...`
         return <SearchBox placeholder={placeholder}/>
     }
 
-    createStatusText() {
+    renderStatusText() {
         const { statusMessage } = this.props;
         const statusClasses = classNames("status", {
             "status-active": !!statusMessage,
