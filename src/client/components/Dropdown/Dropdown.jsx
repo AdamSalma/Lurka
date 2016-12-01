3b5a515c6429af9c3bad4d658f4c4d7b0434ccd0
Adam Salma <adam3salma@gmail.com>
2016-12-01 23:58:22 +0000

    Tidy dropdown component

--- src/client/components/Dropdown/Dropdown.jsx
+++ src/client/components/Dropdown/Dropdown.jsx
@@ -6,7 +6,11 @@ export default function ({items, className, handleClick}) {
     const classes = classNames(className, "dropdown")
     return (
         <ul className={classes} onClick={handleClick}>
-            {items.map(el => <li key={uuid.v4()} className="item">{el}</li>)}
+            {items.map( item => 
+                <li key={uuid.v4()} className="item">
+                    {item}
+                </li>
+            )}
         </ul>
     )
 }
