c2fee3b5bdbf6676a8a1a1fcde63cb3fb2de4479
Adam Salma <adam3salma@gmail.com>
2016-12-03 23:44:35 +0000

    Change Dropdown into compoenent and add nanoscroller to contents

--- src/client/components/Dropdown/Dropdown.jsx
+++ src/client/components/Dropdown/Dropdown.jsx
@@ -1,16 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 import uuid from "uuid";
 import classNames from "classnames";
 
-export default function ({items, className, handleClick}) { 
-    const classes = classNames(className, "dropdown")
-    return (
-        <ul className={classes} onClick={handleClick}>
-            {items.map( item => 
-                <li key={uuid.v4()} className="item">
-                    {item}
-                </li>
-            )}
-        </ul>
-    )
+export default class Dropdown extends Component {
+    constructor(props) {
+        super(props);
+        this.updateScroller = this.updateScroller.bind(this);
+    }
+
+    componentDidMount() {
+        this.updateScroller(this.props.scrollOpts)
+    }
+
+    componentWillUnmount() {
+        this.updateScroller({ destroy: true })
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        this.updateScroller()
+    }
+
+    updateScroller( opts={} ) {
+        $(this.refs.dropdown).nanoScroller(opts);
+    }
+
+    render () {
+        const {items, className, handleClick} = this.props;
+        const classes = classNames(className, "nano");
+
+        return (
+            <div ref="dropdown" className={classes}>
+                <div className="dropdown nano-content" onClick={handleClick}>
+                    {items.map( item => 
+                        <span key={uuid.v4()} className="item">
+                            {item}
+                        </span>
+                    )}
+                </div>
+            </div>
+        )
+    }
+} 
+
+Dropdown.defaultProps = {
+    scrollOpts: {},
+    items: []
 }
