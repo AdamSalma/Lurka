a366c75ab85a07127214f951b66a843aa7ccc36a
Adam Salma <adam3salma@gmail.com>
2016-12-03 23:37:38 +0000

    Change BoardPost into component, was function

--- src/client/components/BoardPost/BoardPost.jsx
+++ src/client/components/BoardPost/BoardPost.jsx
@@ -1,78 +1,82 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Tooltip from '../Tooltip';
+import Counter from "../Counter"
+
 import Velocity from "velocity-animate"
 
-export default ({ post, fetchThread, reshuffle }) => {
-    const {id, title, comment, date, imgsrc, replies} = post;
-    return (
-        <div 
-            id={"t"+id}
-            className="board-post"
-            onClick={() => fetchThread(id)}
-            onMouseEnter={moveCommentUp.bind(null, id)}
-        >
-            <div className="image-wrap">   
-                <img src={imgsrc.sm} onLoad={reshuffle}/>
-            </div>   
-            <div className="comment-wrap">
-                {renderStatusBar(replies)}
-                <div className="op">
-                    <b dangerouslySetInnerHTML={{__html: title}} className="title" />
-                    <div dangerouslySetInnerHTML={{__html: comment}} />
-                </div>
-            </div>
-        </div>
-    )
-}
+export default class BoardPost extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            maxPostHeight: 400,
+            isMovable: null
+        }
 
-function renderStatusBar(replies) {
-    return <div className="count">
-        <span>
-            <span>
-                <i className="mdi mdi-comment-text"></i>
-                <b>{replies.textCount}</b>
-            </span>
-            <span className="updater updater-replycount">
-                +1
-            </span>
-        </span>
-        <span>
-            <span>
-                <i className="mdi mdi-message-image"></i>
-                <b>{replies.imgCount}</b>
-            </span>
-            <span className="updater updater-imgcount">
-                +1
-            </span>
-        </span>
-    </div>
-}
-
-const maxHeight = 400;
-const maxMoveDistance = 100;
-function moveCommentUp(id) {
-    const 
-        $target = $("#t"+id),
-        kids = $target.children(),
-        commentHeight = kids[1].clientHeight,
-        imgHeight = kids[0].clientHeight;
-
-    console.info($target)
-    if (imgHeight + commentHeight > maxHeight ) {
-        // comment is long and offscreen; make it more viewable by moving it up
-        
-        // move by difference, else if that would move comment outside thread, use image height (scroll to top) ;
-        const offScreenAmount = commentHeight - (maxHeight - imgHeight) + 5 // 5px for padding at bottom of comment
-        const height = imgHeight/1.5 // 3/4 of image height
-        const distance = offScreenAmount < height ? offScreenAmount : height
-        Velocity(kids[1], {top: `-${distance}px`}, 200, "ease-out")
-        $target.one('mouseleave', ()=>Velocity(kids[1], {top: 0}, 200))
-
+        this.scrollPostUp = this.scrollPostUp.bind(this)
+        this.scrollPostDown = this.scrollPostDown.bind(this)
     }
 
-}
+    render() {
+        const { fetchThread, reshuffle, post: {
+            id, title, comment, date, imgsrc, replies
+        }} = this.props;
 
-function revertCommentDown() {
+        return (
+            <div 
+                id={"t"+id}
+                className="board-post"
+                onClick={ fetchThread.bind(null, id) }
+                onMouseEnter={this.scrollPostUp}
+                onMouseLeave={this.scrollPostDown}
+            >
+                <div className="image-wrap" ref="image">   
+                    <img src={imgsrc.sm} onLoad={reshuffle}/>
+                </div>   
+                <div className="comment-wrap clearfix" ref="comment">
+                    <div className="counters">
+                        <div className="float-right">
+                            <i className="mdi mdi-comment-text"/>
+                            <Counter value={replies.textCount}/>
+                        </div>
+                        <div className="float-right">
+                            <i className="mdi mdi-image-filter-hdr"/>
+                            <Counter value={replies.imgCount} />
+                        </div>
+                    </div>
+                    <div className="op">
+                        <b dangerouslySetInnerHTML={{__html: title}} className="title" />
+                        <div dangerouslySetInnerHTML={{__html: comment}} />
+                    </div>
+                </div>
+            </div>
+        )
+    }
 
+    scrollPostUp(){
+        const { comment, image } = this.refs;
+        const imgHeight = image.clientHeight, 
+              commentHeight = comment.clientHeight,
+              maxHeight = this.state.maxPostHeight
+
+        if (imgHeight + commentHeight > maxHeight ) {
+            // comment is long and offscreen; make it more viewable by moving it up
+            
+            // move by difference, else if that would move comment outside 
+            // thread, use image height (scroll to top)
+            const offScreenAmount = commentHeight - (maxHeight - imgHeight) + 5 // 5px for padding at bottom of comment
+            const height = imgHeight/1.5 // 3/4 of image height
+            const distance = offScreenAmount < height ? offScreenAmount : height
+            Velocity(comment, {top: `-${distance}px`}, 200, "ease-out")
+            this.setState({
+                isMovable: true
+            })
+
+        }
+    }
+ 
+    scrollPostDown(){
+        if (this.state.isMovable) 
+            Velocity(this.refs.comment, {top: 0}, 200)
+    }
 }
