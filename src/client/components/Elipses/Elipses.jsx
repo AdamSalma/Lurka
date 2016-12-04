ff81b287df29b89c751acb61c30cbac1098b2d03
Adam Salma <adam3salma@gmail.com>
2016-12-04 23:22:51 +0000

    New component Elipses

---
+++ src/client/components/Elipses/Elipses.jsx
@@ -0,0 +1,50 @@
+import React, { Component, PropTypes } from "react";
+import classNames from "classnames"
+
+export default class Elipses extends Component {
+    constructor({maxDots, interval}) {
+        super();
+        this.tick = this.tick.bind(this)
+
+        this.state = {
+            dots: 0,
+            maxDots: maxDots,
+            interval: interval
+        }
+
+        this._interval = setInterval(this.tick, interval)
+    }
+
+    componentWillUnmount() {
+        clearInterval(this._interval)    
+    }
+
+    render() {
+        return (
+            <div className="elipses-wrap">
+                {this.props.text}
+                <span className="elipses">
+                    {'.'.repeat(this.state.dots)}
+                </span>
+            </div>
+        )
+    }
+
+    tick() {
+        this.setState(({dots, maxDots}) => {
+            return {
+                dots: dots < maxDots ? ++dots : 0
+            }
+        })
+    }
+}
+
+Elipses.defaultProps = {
+    maxDots: 3,
+    interval: 300
+}
+
+Elipses.propTypes = {
+    maxDots: PropTypes.number,
+    interval: PropTypes.number
+}
