17b3d7d57f527cfe95c16e24ae06925a0e257879
Adam Salma <adam3salma@gmail.com>
2016-12-04 23:23:15 +0000

    Remove clutter from Logo

--- src/client/components/Logo/Logo.jsx
+++ src/client/components/Logo/Logo.jsx
@@ -1,36 +1,12 @@
-import React, { Component } from "react"
+import React from "react"
 import classNames from 'classnames'
 
-class Logo extends Component {
-    constructor(props) {
-        super(props);
-        this.displayName = 'Logo';
-        this.scrollToHome = this.scrollToHome.bind(this)
-    }
+export default function ({ className }) {
+    const classes = classNames("logo", className);
 
-    render() {
-        const { isFullsize, statusMessage } = this.props;
-        const logoClasses = classNames("logo", {"logo-fullsize": isFullsize});
-
-        return ( 
-            <div 
-                id="logo"
-                className={statusMessage ? "logo-rotating" : ""} 
-                onClick={this.scrollToHome}
-            >
-                <img src='./logo.png' className={logoClasses}/>
-            </div>      
-        )
-    }
-
-    scrollToHome() {
-        // scrolls content page away to reveal home
-        console.log("logo click");
-        const {scrollPage, scrollHeader} = this.props;
-        scrollPage("content", false)
-        scrollHeader(false)
-
-    }
+    return (
+        <div className="logo-wrap">
+            <img src='./logo.png' className={classes}/>
+        </div>      
+    )
 }
-
-export default Logo;
