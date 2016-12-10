84e7ac46b2aba4a8e34bed8a3d0b863630d5628a
Adam Salma <adam3salma@gmail.com>
2016-12-10 23:54:55 +0000

    Refactor BoardList; abstract logic

--- src/client/components/BoardList/BoardList.jsx
+++ src/client/components/BoardList/BoardList.jsx
@@ -4,18 +4,10 @@ import classNames from 'classnames';
 import uuid from 'uuid';
 
 import Dropdown from '../Dropdown';
-import SearchBox from '../SearchBox';
 
 export default class BoardList extends Component {
     constructor({shouldPreload, boardList, provider, fetchBoardList}) {
         super()
-        this.handleKeyUp = this.handleKeyUp.bind(this)
-        this.renderBoardList = this.renderBoardList.bind(this)
-        this.toggleBoardList = this.toggleBoardList.bind(this)
-
-        this.state = {
-            keyword: ''
-        }
 
         if (shouldPreload && !boardList) {
             fetchBoardList(provider);
@@ -23,61 +15,40 @@ export default class BoardList extends Component {
     }
 
     render() {
-        const {boardList} = this.props
+        const {boardList, provider, onClick} = this.props
+        const classes = classNames('boardlist', `p-${provider}`)
         const hasBoards = boardList && boardList.length;
 
-        return hasBoards ? this.renderBoardList() : false
-    }
-
-
-    renderBoardList() {
-        const { boardList, provider, handleClick } = this.props;
         return (
-            <div className={`board-list ${provider}`} 
-                 onClick={this.toggleBoardList}
-            >
-                <span className="placeholder">{provider}</span>
-                <div className="content">
-                    <SearchBox
-                        className="search"
-                        onKeyUp={this.handleKeyUp}
-                        placeholder={`Search ${provider}`}
-                        onClick={ e => e.stopPropagation()}
-                    />
-                    <Dropdown 
-                        className={provider}
-                        items={this.renderDropdownChildren(boardList)}
-                        handleClick={handleClick}
-                    />
-                </div>
+            <div className={classes}>
+                {hasBoards && <Dropdown 
+                    onClick={onClick} 
+                    items={this.dropdownItems(boardList)} 
+                />}
             </div>
         )
     }
-    
 
-    handleKeyUp(event) {
-        this.setState({
-            keyword: event.target.value.toLowerCase()
-        })
-    }
+    dropdownItems(boardList) {
+        // Filter boardlist then render each board
 
-    toggleBoardList(event) {
-        console.log("clicked on boardlist");
-        $(event.target).closest('.board-list').toggleClass('active')
-        this.forceUpdate()
-    }
-
-    renderDropdownChildren(boardList) {
-
-        const boards = boardList.filter( 
-            ({description}) => 
-                description.toLowerCase().includes(this.state.keyword)
+        return boardList.filter( ({description}) => 
+            description.toLowerCase()
+                .includes(this.props.searchPhrase)
+        ).map( 
+            ({boardID, description}, index) => (
+                <div 
+                    key={uuid.v4()} 
+                    data-value={boardID} 
+                    data-index={index}
+                >{description}</div>
+            )
         )
-        return boards.map( ({boardID, description}, index) => (
-            <div key={uuid.v4()} data-value={boardID} data-index={index}>
-                {description}
-            </div>
-        ));
     }
 
+}
+
+BoardList.defaultProps = {
+    searchPhrase: '',
+    shouldPreload: true
 }
