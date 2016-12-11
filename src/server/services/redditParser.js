05eaab53110df84e7ceaaf370510bab79ee06f1f
Adam Salma <adam3salma@gmail.com>
2016-12-11 23:21:00 +0000

    Update reddit boardlist parser

--- src/server/services/redditParser.js
+++ src/server/services/redditParser.js
@@ -1,19 +1,35 @@
 import fs from 'fs'
+
 export function parseBoardList(boardList) {
     // console.log(JSON.stringify(boardList))
     try {
         boardList.data.children
     } catch (e) {
-        log.error(`Bad reddit data returned => ${e}`)
+        return log.error(`Bad reddit data returned: ${e}`)
     }
 
     const boards = boardList.data.children.map(board => board.data) // thanks, reddit!
 
-    return boards.map(({url, title}) => {
+    return boards.map( board => {
+        // Essentials
+        const { url, title, public_description } = board
         let boardID = url.split('/').slice(-2)[0]
+        
+        // Board info
+        const { subscribers, over18, icon_img } = board
+
         return {
+            url, 
             boardID,
-            description: `/${boardID}/ - ${title}`
+            title,
+            short_desc: `/${boardID}/ - ${title}`,
+            description: public_description,
+            info: {
+                subscribers, 
+                NSFW: over18, 
+                icon_img                
+            }
+
         }
     });
 }
