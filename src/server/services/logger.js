5f39572b6557196567ffb226a958c28aa7a40f82
Adam Salma <adam3salma@gmail.com>
2016-12-23 15:23:07 +0000

    Update server logger

--- src/server/services/logger.js
+++ src/server/services/logger.js
@@ -2,8 +2,8 @@
 var clc = require('cli-color');
 var moment = require('moment');
 
-var config = 'H:mm'
-var loggerWidth = 108
+var config = '[[]H:mm[]]'
+var loggerWidth = 130
 
 module.exports = {
     info: function(msg) {
@@ -28,16 +28,22 @@ function getTime(){
 }
 
 function getPath(){
-    let callee = (new Error).stack.split("\n")[4]
+    let callee = (new Error).stack.split("\n")[4]  // get file path of callee
     return callee.split('\\').pop().split('/').pop()  // handles \ or /
 }
 
 function format(string) {
-    let callee = getPath(), time = getTime()
-    let path = callee.split(':')
-    let spaces = loggerWidth - (string.length + callee.length + time.length)
-    let newString = time +' '+ string +
-                    Array(spaces > 0 ? spaces : 2).join(" ") +    // add spaces if possible
-                    clc.underline(path[0])+':'+clc.bold(path[1])  // color path and join
-    console.info(newString)
+    let callee = getPath(),
+        time = getTime(),
+        path_ = callee.split(':')
+
+    callee = clc.underline(path_[0])+':'+clc.bold(path_[1])  // color path and join
+    time = clc.bold(time)
+
+    let whitespace = loggerWidth - (string.length + callee.length + time.length)
+    whitespace = Array(whitespace > 0 ? whitespace : 2).join(" ")
+
+    let message = `${time} ${string} ${whitespace} ${callee}`
+
+    console.log(message)
 }
