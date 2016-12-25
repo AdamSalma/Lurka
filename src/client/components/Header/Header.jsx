557bc7e0fc15e7c8b53e9847be45e9149449d7d8
Adam Salma <adam3salma@gmail.com>
2016-12-25 00:42:31 +0000

    (Header) Remove old renders, Add scrollUp button + content filtering

--- src/client/components/Header/Header.jsx
+++ src/client/components/Header/Header.jsx
@@ -10,12 +10,7 @@ import SearchBox from "../SearchBox";
 export default class Header extends Component {
     constructor(props) {
         super(props);
-        // this.createBoardList = this.createBoardList.bind(this)
-        this.renderStatusText = this.renderStatusText.bind(this)
-        this.renderSearchbox = this.renderSearchbox.bind(this)
-        this.state = {
-            fadeOut: false
-        }
+        this.handleKeyUp = this.handleKeyUp.bind(this);
     }
 
     componentWillUpdate({ statusMessage }) {
@@ -26,48 +21,26 @@ export default class Header extends Component {
     }
 
     render() {
-        const { scrollPage, scrollHeader, statusMessage } = this.props;
+        const { scrollPage, scrollHeader, statusMessage, threadIsActive } = this.props;
+        const placeholder = `Search ${threadIsActive ? "thread" : "board"} ...`
+
         return (
             <div id="header" className="header">
+                <div className="header-background"/>
                 <div className="header-content">
                     <Logo />
-                    {this.renderStatusText()}
-                    {this.renderSearchbox()}
-                    {/*this.createBoardList()*/}
+                    <SearchBox placeholder={placeholder} onKeyUp={this.handleKeyUp}/>
+                    <input 
+                        type="button" value="Return" 
+                        onClick={scrollPage.bind(null, "content", false)} 
+                    />
                 </div>
             </div>
         )  // TODO: Add search/filter functionality
     }
 
-    // createBoardList() {
-    //     const {boardList, fetchBoardList, provider, fetchBoard} = this.props
-    //     return <BoardList 
-    //         provider={provider} 
-    //         boardList={boardList[provider]} 
-    //         fetchBoard={fetchBoard}
-    //         fetchBoardList={fetchBoardList}
-    //     />
-    // }
-
-    renderSearchbox() {
-        const {threadIsActive} = this.props
-        const placeholder = `Search ${threadIsActive ? "thread" : "board"} ...`
-        return <SearchBox placeholder={placeholder} />
-    }
-
-    renderStatusText() {
-        const { statusMessage } = this.props;
-        const statusClasses = classNames("status", {
-            "status-active": !!statusMessage,
-            "fade-out": this.state.fadeOut  // TODO: make status message fadeout
-        })
-
-        return <div id="status" className={statusClasses}>
-            {statusMessage 
-                ? <span className="status-content">{statusMessage}</span> 
-                : "" }
-        </div>
+    handleKeyUp(event) {
+        // TODO
+        this.props.filterBoard(event.target.value)
     }
 }
-
-
