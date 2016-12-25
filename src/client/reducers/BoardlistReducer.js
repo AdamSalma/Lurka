d29e4a0385f07ec73361e9a75c7e40e48a5dcd37
Adam Salma <adam3salma@gmail.com>
2016-12-25 00:47:18 +0000

    Add reducer for BoardList favourites + request invalidation

--- src/client/reducers/BoardlistReducer.js
+++ src/client/reducers/BoardlistReducer.js
@@ -2,18 +2,43 @@ import initialState from './initialState';
 import { 
     BOARD_LIST_LOADED, 
     BOARD_LIST_REQUESTED,
+    BOARD_LIST_INVALIDATED,
+    BOARD_LIST_ADD_FAVOURITE,
+    BOARD_LIST_REMOVE_FAVOURITE,
 } from '../constants'
-
-// ADD_FAVOURITE_BOARD
-// TODO: Add boardList invalidation + favourites
 
 export default function (state=initialState.boardList, action) {
     switch (action.type) {
 
+        case BOARD_LIST_REQUESTED:
+            return Object.assign({}, state, {
+                didInvalidate: false
+            })
         
+        case BOARD_LIST_INVALIDATED:
+            return Object.assign({}, state, {
+                didInvalidate: true
+            })
+
         case BOARD_LIST_LOADED:
             return Object.assign({}, state, {
                 [action.provider]: action.payload
+            })
+
+        case BOARD_LIST_ADD_FAVOURITE:
+            return Object.assign({}, state, {
+                favourites: [...state.favourites,
+                    action.payload
+                ]
+            })
+
+        case BOARD_LIST_REMOVE_FAVOURITE:
+            return Object.assign({}, state, {
+                favourites: state.favourites.filter(
+                    board =>
+                        board.boardID !== action.payload.boardID &&
+                        board.provider === action.payload.provider
+                )
             })
 
         default:
