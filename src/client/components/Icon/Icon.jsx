c07b623f5a9d34371c0974f3b7baf4f2e7fcbbc7
Adam Salma <adam3salma@gmail.com>
2016-12-27 23:23:58 +0000

    Change component Icon to autocomplete classname prefix of icon vendor

--- src/client/components/Icon/Icon.jsx
+++ src/client/components/Icon/Icon.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import classNames from 'classnames'
 
+const iconPack = "mdi"
+
 export default function ( props ) {
+    // add default prefix eg 'mdi mdi-icon'
+    const iconName = props.name ? `${iconPack} ${iconPack}-${props.name}` : ``
     const newProps = Object.assign({}, props, {
-        className: classNames('icon', props.className)
+        className: classNames('icon', props.className, iconName)
     })
 
     return <span {...newProps}/>
