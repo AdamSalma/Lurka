3688e995df2662eb260eb15c754eb85f33526fa0
Adam Salma <adam3salma@gmail.com>
2016-12-30 20:28:34 +0000

    Add Hierarchy and return animation to Header

--- src/client/components/Header/Header.jsx
+++ src/client/components/Header/Header.jsx
@@ -1,12 +1,10 @@
 import React, { Component } from "react";
-
-import Velocity from 'velocity-animate';  // TODO: Is this needed? Logo?
 import classNames from "classnames";
 
 import Logo from "../Logo";
-// import BoardList from "../BoardList";
-import SearchBox from "../SearchBox";
 import Icon from "../Icon";
+import SearchBox from "../SearchBox";
+import Hierarchy from "../Hierarchy";
 
 export default class Header extends Component {
     constructor(props) {
@@ -15,37 +13,45 @@ export default class Header extends Component {
         this.returnHome = this.returnHome.bind(this);
     }
 
-    componentWillUpdate({ statusMessage }) {
-        if (this.props.statusMessage && !statusMessage) {
-            // Status message will be cleared
-            this.setState({fadeOut: true})
-        }
-    }
-
     render() {
-        const { scrollPage, scrollHeader, statusMessage, threadIsActive, closeThread } = this.props;
+        // Actions
+        const { scrollPage, scrollHeader, closeThread } = this.props;
+        // State
+        const { threadIsActive, provider, boardID, threadID } = this.props;
+
         const placeholder = `Search ${threadIsActive ? "thread" : "board"} ...`
 
         return (
             <div id="header" className="header">
                 <div className="header-background"/>
-                <div className="header-content">
-                    <Logo />
-                    <SearchBox placeholder={placeholder} onKeyUp={this.handleKeyUp}/>
-                    <Icon name="arrow-up" onClick={this.returnHome} />
+                <div className="header-content-wrap">
+                    <div className="header-content">
+                        <Logo />
+                        <SearchBox placeholder={placeholder} onKeyUp={this.handleKeyUp}/>
+                        <Hierarchy provider={provider} boardID={boardID} threadID={threadID}/>
+                        <Icon name="arrow-up" onClick={this.returnHome} />
+                    </div>
                 </div>
             </div>
         )  // TODO: Add search/filter functionality
     }
 
     returnHome(){
-        this.props.closeThread(null,  // threadID
-            this.props.scrollPage.bind(null, "content", false) // page to scroll, scroll up
+        const {closeThread, scrollPage, destroyBoard, scrollHeader} = this.props
+
+        closeThread(null,  // threadID
+            () => {
+                scrollPage({
+                    page: "board", 
+                    direction: "down",
+                    callback: destroyBoard
+                })   
+                scrollHeader(false)  // hide header
+            }
         )
     }
 
     handleKeyUp(event) {
-        // TODO
-        this.props.filterBoard(event.target.value)
+        this.props.searchBoard(event.target.value)
     }
 }
