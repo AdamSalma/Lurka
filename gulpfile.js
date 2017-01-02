614c318414c4285795a1c5f665a734893b6b61fe
Adam Salma <adam3salma@gmail.com>
2017-01-02 23:46:32 +0000

    Update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,58 @@
 var gulp = require('gulp');
 var webpack = require('webpack');
+var runSequence = require('run-sequence');
+var del = require('del');
 
 var clientConfig = require('./config/webpack.prod.client.js')
 var serverConfig = require('./config/webpack.prod.server.js')
 
 
-gulp.task('bundle-client', function(done) {
-    webpack( clientConfig ).run(onBundle(done))
+/* App Bundle */
+gulp.task('bundle:client', function(done) {
+    webpack( clientConfig ).run(onWebpackBundle(done))
 });
 
-gulp.task('bundle-server', function(done) {
-    webpack( serverConfig ).run(onBundle(done))
+gulp.task('bundle:server', function(done) {
+    webpack( serverConfig ).run(onWebpackBundle(done))
 });
 
-gulp.task('move-assets', function() {
-	gulp.src('./src/assets/**/*', {base: './src'}).pipe(gulp.dest('./app/'))
+/* Copy */
+gulp.task('copy:assets', function() {
+	gulp.src('src/assets/**/*', {base: 'src'}).pipe(gulp.dest('app/'))
 });
 
+gulp.task('copy:electron', function() {
+    gulp.src('src/electron/**/*').pipe(gulp.dest('app/'))
+});
 
-gulp.task('bundle', ['bundle-client', 'bundle-server']);
-gulp.task('build', ['bundle', 'move-assets'])  // TODO - elctron build
+gulp.task('copy:fonts-to-assets', function() {
+    gulp.src('src/app/**/*.{ttf,svg,woff,eot}').pipe(gulp.dest('app/assets/'))
+});
+
+/* Clean */
+gulp.task('clear', function() {
+    return del([
+        'app/**/*',
+        '!app/package.json',
+        '!app/node_modules',
+        '!app/node_modules/**/*'
+    ])
+});
+
+gulp.task('copy', function(){
+    runSequence(['copy:assets', 'copy:electron'], 'copy:fonts-to-assets')
+})
 
 
-function onBundle(done) {
+/* Main tasks */
+gulp.task('bundle', ['bundle:client', 'bundle:server']);
+gulp.task('build', function(done){  // TODO - full elctron build
+    runSequence('clear', 'bundle', 'copy', done)
+})  
+gulp.task('default', ['build']);
+
+
+function onWebpackBundle(done) {
     return function(err, stats) {
         if (err) console.log('Error', err);
         else console.log(stats.toString({chunks: false, colors: true}));
