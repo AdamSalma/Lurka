bf4f71988f9604b3087be7607b741f3675dda450
Adam Salma <adam3salma@gmail.com>
2017-01-08 23:59:11 +0000

    Add AppContainer - for future tabs

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -5,7 +5,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
-import App from './containers/App';
+import {App, AppContainer} from './containers/App';
 import configureStore from './store';
 import { loadState, saveState } from './store/localStorage';
 
