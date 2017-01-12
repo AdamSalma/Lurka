b7e5d65ee533d3a6847125598537d928b14f34a5
Adam Salma <adam3salma@gmail.com>
2017-01-12 23:34:04 +0000

    Settings: add board/thread maximum age

--- src/client/constants/settings.js
+++ src/client/constants/settings.js
@@ -21,15 +21,13 @@ module.exports = [
         key: "NSFW",
     },
     {
-        disabled: true,
         type: 'Content',
-        title: "NSFW",
+        title: "Board Update",
         value: 15,
         desc: "Seconds to wait before updating the current board",
         key: "boardUpdateInterval",
     },
     {
-        disabled: true,
         type: 'Content',
         title: "Thread Update",
         value: 5,
@@ -43,5 +41,21 @@ module.exports = [
         value: 3,
         desc: "Limit seconds to wait between requests",
         key: "requestThrottle",
+    },
+    {
+        disabled: true,
+        type: 'Content',
+        title: "Board Age",
+        value: 900,  // 15 mins
+        desc: "Maximum age (seconds) to load a board from history rather than a new request",
+        key: "maxBoardAge",
+    },
+    {
+        disabled: true,
+        type: 'Content',
+        title: "Thread Age",
+        value: 900,  // 15 mins
+        desc: "Maximum age (seconds) to load a thread from history rather than a new request",
+        key: "maxThreadAge",
     }
 ]
