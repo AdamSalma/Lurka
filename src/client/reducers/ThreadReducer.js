443b0bf30740e620ed017265c0460cf8cdfdb0b3
Adam Salma <adam3salma@gmail.com>
2017-01-12 23:35:55 +0000

    Create reducers for board/thread history, update others

--- src/client/reducers/ThreadReducer.js
+++ src/client/reducers/ThreadReducer.js
@@ -3,7 +3,8 @@ import {
     THREAD_LOADED, 
     THREAD_REQUESTED, 
     THREAD_DESTROYED,
-    THREAD_INVALIDATED
+    THREAD_INVALIDATED,
+    THREAD_LOADED_FROM_HISTORY
 } from '../constants'
 
 export default function (state = initialState.thread, action) {
@@ -30,14 +31,14 @@ export default function (state = initialState.thread, action) {
             })
 
         case THREAD_DESTROYED:
-            const history = Object.assign({}, state.history, {
-                [action.payload]: state.posts
-            })
-
             return Object.assign({}, state, {
-                history: history,
                 posts: [],
                 isActive: false
+            })
+
+        case THREAD_LOADED_FROM_HISTORY:
+            return Object.assign({}, state, action.payload, {
+                isActive: true
             })
 
         default:
--- src/client/reducers/index.js
+++ src/client/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import ThreadReducer from "./ThreadReducer";
 import BoardReducer from "./BoardReducer";
+import BoardHistoryReducer from "./BoardHistoryReducer";
+import ThreadHistoryReducer from "./ThreadHistoryReducer";
 import BoardlistReducer from "./BoardlistReducer";
 import StatusReducer from "./StatusReducer";
 import SettingsReducer from "./SettingsReducer";
@@ -9,7 +11,9 @@ import SettingsReducer from "./SettingsReducer";
 const rootReducer = combineReducers({
     boardList: BoardlistReducer,
     board: BoardReducer,
+    boardHistory: BoardHistoryReducer,
     thread: ThreadReducer,
+    threadHistory: ThreadHistoryReducer,
     status: StatusReducer,
     settings: SettingsReducer
 });
