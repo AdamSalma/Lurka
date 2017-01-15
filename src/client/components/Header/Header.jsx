770e339d415909f134c6871c71dd69b9957751a7
Adam Salma <adam3salma@gmail.com>
2017-01-15 23:54:32 +0000

    Refactor header, add icons

--- src/client/components/Header/Header.jsx
+++ src/client/components/Header/Header.jsx
@@ -5,6 +5,9 @@ import Logo from "../Logo";
 import Icon from "../Icon";
 import SearchBox from "../SearchBox";
 import Hierarchy from "../Hierarchy";
+import HeaderItem from "../HeaderItem";
+
+import {version} from "../../../../package.json"
 
 export default class Header extends Component {
     constructor(props) {
@@ -15,25 +18,48 @@ export default class Header extends Component {
 
     render() {
         // Actions
-        const { scrollPage, scrollHeader, closeThread } = this.props;
+        const { scrollPage, scrollHeader, closeThread, toggleNavbar } = this.props;
         // State
         const { threadIsActive, provider, boardID, threadID } = this.props;
 
         const placeholder = `Search ${threadIsActive ? "thread" : "board"} ...`
 
+        const headerContent = classNames('header-content', {
+            'thread': threadIsActive, 
+            'board': !threadIsActive
+        })
+
         return (
             <div id="header" className="header">
                 <div className="header-background"/>
-                <div className="header-content-wrap">
-                    <div className="header-content">
-                        <Logo />
-                        <SearchBox placeholder={placeholder} onKeyUp={this.handleKeyUp}/>
+                <div className={headerContent}>
+                    <HeaderItem className="icon" onClick={this.toggleActive}>
+                        <Icon name="menu" onClick={toggleNavbar}/>
+                    </HeaderItem>
+                    <HeaderItem className="lurka">
+                        Lurka v{version}
+                    </HeaderItem>
+                    <HeaderItem className="breadcrumb">
                         <Hierarchy provider={provider} boardID={boardID} threadID={threadID}/>
-                        <Icon name="arrow-up" onClick={this.returnHome} />
-                    </div>
+                    </HeaderItem>
+                    <HeaderItem className="searchbox">
+                        <SearchBox placeholder={placeholder} onKeyUp={this.handleKeyUp}/>
+                    </HeaderItem>
+                    <HeaderItem className="icon shift-right" onClick={this.toggleActive}>
+                        <Icon name="eye" onClick={this.returnHome} />
+                    </HeaderItem>
+                    <HeaderItem className="icon" onClick={this.toggleActive}>
+                        <Icon name="archive" />
+                    </HeaderItem>
+                    <HeaderItem className="icon" onClick={this.toggleActive}>
+                        <Icon name="filter" />
+                    </HeaderItem>
+                    <HeaderItem className="icon" onClick={this.toggleActive}>
+                        <Icon name="sort" />
+                    </HeaderItem>
                 </div>
             </div>
-        )  // TODO: Add search/filter functionality
+        )  // TODO: Add filter functionality + buttons
     }
 
     returnHome(){
@@ -53,5 +79,10 @@ export default class Header extends Component {
 
     handleKeyUp(event) {
         this.props.searchBoard(event.target.value)
+    }
+
+    toggleActive(e) {
+        console.log('toggling active')
+        e.target.classList.toggle('active')
     }
 }
