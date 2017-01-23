28d35615fc7dd0e1b8721e898baf277e3f12cbc2
Adam Salma <adam3salma@gmail.com>
2017-01-23 23:59:52 +0000

    Add board parsing to reddit

--- src/server/parsers/redditParser.js
+++ src/server/parsers/redditParser.js
@@ -1,16 +1,8 @@
-import fs from 'fs'
+import proxify from '../services/proxyUrls';
 
-export function parseBoardList(boardList) {
-    // console.log(JSON.stringify(boardList))
-    try {
-        boardList.data.children
-    } catch (e) {
-        return log.error(`Bad reddit data returned: ${e}`)
-    }
 
-    const boards = boardList.data.children.map(board => board.data) // thanks, reddit!
-
-    return boards.map( board => {
+export function parseBoardList(boards) {
+    return boards.data.children.map(({data: board }) => {
         // Essentials
         const { url, title, public_description } = board
         let boardID = url.split('/').slice(-2)[0]
@@ -33,3 +25,31 @@ export function parseBoardList(boardList) {
         }
     });
 }
+
+
+export function parseBoard(board) {
+    return board.data.children.map(({data: post }) => {
+        return {
+            id: post.id,
+            title: post.title,
+            comment: post.selftext || '',
+            date: post.created || post.created_utc,
+            author: post.author,
+            replies: {
+                textCount: post.num_comments,
+                ups: post.ups,
+                downs: post.downs
+            },
+            NSFW: post.over_18,
+            media_embed: post.media_embed,
+            media: {
+                is_external: !post.domain.includes('self'),
+                thumbnail: proxify("/media", {url: post.media ? post.media.thumbnail_url : post.thumbnail, provider: "reddit"}), 
+            }
+
+
+        }
+    })
+}
+
+
