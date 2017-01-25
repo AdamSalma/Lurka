2473f3bcc84d94d46d7420316d5afc25149cb318
Adam Salma <adam3salma@gmail.com>
2017-01-25 23:48:59 +0000

    Rename exports, add reddit search + board

--- src/server/config/apiEndpoints.js
+++ src/server/config/apiEndpoints.js
@@ -1,9 +1,8 @@
 /**
  * External API endpoints for various resources
- * @return {Function -> Object}
  */
 
-export const chan = function(boardID, threadID) {
+export const fourchanAPI = function(boardID, threadID) {
     return {
         boardlist: `http://a.4cdn.org/boards.json`,
         board: `https://a.4cdn.org/${boardID}/catalog.json`,
@@ -14,11 +13,26 @@ export const chan = function(boardID, threadID) {
     }
 }
 
-export const reddit = function({type="default", limit=50}) {
-    // @param type  - default, popular, new, gold
-    // @param limit - the maximum number of items desired (default: 25, maximum: 100)
-    return {
-        subreddits: `http://reddit.com/subreddits/${type}/.json?limit=${limit}`
+export const redditAPI = {
+    subreddits: ({type="default", limit=100}) => {
+        // @param type  - default, popular, new, gold
+        // @param limit - the maximum number of items desired (default: 25, maximum: 100)
+        return `http://reddit.com/subreddits/${type}/.json?limit=${limit}`
+    },
+    search_subreddits: ({query}) => {
+        if (!query) {
+            throw new Error('No query supplied')
+        }
+
+        return `https://www.reddit.com/search/.json?q=${query}`
+    },
+    board: ({boardID, limit=100}) => {
+        if (!boardID) {
+            throw new Error('No boardID supplied to reddit')
+        }
+
+        return `https://www.reddit.com/r/${boardID}/.json?limit=${limit}`
+
     }
 }
 
