ccfd407e29e162bfd37befbc1db1430f2bab5d89
Adam Salma <adam3salma@gmail.com>
2017-01-26 19:36:56 +0000

    Refactor settings from list to object

--- src/client/constants/settings.js
+++ src/client/constants/settings.js
@@ -1,68 +1,61 @@
-module.exports = [
-    {
+module.exports = {
+    "download-location": {
         type: 'OS',
         title: "Download location", 
         value: './',
         desc: "The default folder to save content to",
-        key: "download-location",
     },
-    {
+    "stylesheet": {
         type: 'Interface',
         title: "Custom Styles",
         value: null,
         desc: "Insert a custom CSS stylesheet",
-        key: "stylesheet",
     },
-    {
+    "NSFW": {
         type: 'Content',
         title: "NSFW",
-        value: true,
+        value: false,
         desc: "Over 18",
-        key: "NSFW",
+        checkbox: true
     },
-    {
+    "boardUpdateInterval": {
         type: 'Resource Fetching',
         title: "Board Update",
         value: 15,
         desc: "Seconds to wait before updating the current board",
-        key: "boardUpdateInterval",
     },
-    {
+    "threadUpdateInterval": {
         type: 'Resource Fetching',
         title: "Thread Update",
         value: 5,
         desc: "Seconds to wait before updating the current thread",
-        key: "threadUpdateInterval",
     },
-    {
+    "requestThrottle": {
         disabled: true,
         type: 'Resource Fetching',
         title: "Limit Requests",
         value: 3,
         desc: "Limit seconds to wait between requests",
-        key: "requestThrottle",
     },
-    {
+    "maxBoardAge": {
         disabled: true,
         type: 'Resource Fetching',
         title: "Board Age",
         value: 900,  // 15 mins
         desc: "Maximum age (seconds) to load a board from history rather than a new request",
-        key: "maxBoardAge",
     },
-    {
+    "maxThreadAge": {
         disabled: true,
         type: 'Resource Fetching',
         title: "Thread Age",
         value: 900,  // 15 mins
         desc: "Maximum age (seconds) to load a thread from history rather than a new request",
-        key: "maxThreadAge",
     },
-    {
+    "muteWebM": {
         type: 'Content',
         title: "Auto-mute Videos",
         value: false,
         desc: "Mute WebM videos automatically when opened",
-        key: "maxBoardAge",
+        checkbox: true
     },
-]
+}
