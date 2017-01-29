2fce2d552dc7f371daf650fc0dc85b7fec91ac2f
Adam Salma <adam3salma@gmail.com>
2017-01-29 23:49:54 +0000

    Refactor reducers to drop reddit

--- src/client/reducers/SettingsReducer.js
+++ src/client/reducers/SettingsReducer.js
@@ -1,8 +1,20 @@
 import initialState from '../constants/initialState';
-// import {} from '../constants';
+import {
+    SETTING_CHANGED
+} from '../constants';
 
 export default function (state = initialState.settings, action) {
     switch (action.type) {
+        case SETTING_CHANGED:
+            const {name, value} = action.payload
+
+            // assign new value to setting while keeping other 
+            // properties intact
+            const newSetting = Object.assign({}, state[name], {value})
+            
+            return Object.assign({}, state, {
+                [name]: newSetting
+            })
 
         default:
             return state
--- src/client/reducers/ThreadHistoryReducer.js
+++ src/client/reducers/ThreadHistoryReducer.js
@@ -1,7 +1,8 @@
 import initialState from '../constants/initialState';
 import { 
     THREAD_LOADED_FROM_HISTORY,
-    THREAD_SAVED_TO_HISTORY
+    THREAD_SAVED_TO_HISTORY,
+    THREAD_HISTROY_CLEARED
 } from '../constants'
 
 export default function (state = initialState.threadHistory, action) {
@@ -9,22 +10,12 @@ export default function (state = initialState.threadHistory, action) {
     switch (action.type) {
 
         case THREAD_SAVED_TO_HISTORY:
-            history = Object.assign({}, state[action.provider], {
-                [action.threadID]: action.payload
-            })
-
             return Object.assign({}, state, {
-                [action.provider]: history
+                [action.provider]: action.payload
             })
 
-        case THREAD_LOADED_FROM_HISTORY:
-            history = Object.assign({}, state[action.provider], {
-                [action.threadID]: {}
-            })
-
-            return Object.assign({}, state, {
-                [action.provider]: history
-            })
+        case THREAD_HISTROY_CLEARED:
+            return {}
 
         default:
             return state
