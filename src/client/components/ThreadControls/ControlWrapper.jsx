2918c9e866bb7a000274cbf41494a41fd428da6b
Adam Salma <adam3salma@gmail.com>
2017-01-31 23:56:44 +0000

    Add styles for thread controls

---
+++ src/client/components/ThreadControls/ControlWrapper.jsx
@@ -0,0 +1,47 @@
+import React, {Component} from 'react'
+import classNames from 'classnames'
+
+export default class ControlWrapper extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isToggled: false
+        }
+
+        this.handleClick = this.handleClick.bind(this)
+    }
+
+    render() {
+        const controlClasses = classNames('control-wrapper', {
+            "selected": this.props.isActive
+        })
+
+        return (
+            <div className={controlClasses} onClick={this.handleClick}>
+                {this.renderChildren()}
+            </div>
+        )
+    }
+
+    renderChildren() {
+        const { children, toggleProps } = this.props
+
+        if (this.state.isToggled) {
+            console.log("RENDERING TOGGLED PROPS")
+            return React.Children.map(children, child => {
+                // Use custom props
+                return React.cloneElement(child, toggleProps)
+            })
+        } else {
+            return children
+        }
+    }
+
+    handleClick() {
+        this.setState(state => {
+            return {
+                isToggled: !state.isToggled
+            }
+        })
+    }
+}
