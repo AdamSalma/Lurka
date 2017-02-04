f779993b0dbfda47ed8a089a7bd51e75ed63d688
Adam Salma <adam3salma@gmail.com>
2017-02-04 03:08:29 +0000

    Restructure: combine actions in index.js

---
+++ src/client/actions/index.js
@@ -0,0 +1,33 @@
+export {
+    scrollPage,
+    scrollHeader,
+    toggleNavbar,
+} from './AnimationActions'
+
+export {
+    loadMorePosts,
+    fetchBoard,
+    destroyBoard,
+    searchBoard,
+} from './BoardActions'
+
+export {
+    fetchBoardList,
+    searchBoardlist,
+    addToFavourites,
+    removeFromFavourites
+} from './BoardListActions'
+
+export {
+    toggleSetting
+} from './SettingsActions'
+
+export {
+    changeProvider,
+    alertMessage
+} from './StatusActions'
+
+export {
+    fetchThread,
+    closeThread
+} from './ThreadActions'
