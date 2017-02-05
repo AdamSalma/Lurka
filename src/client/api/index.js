6988e7f9515ee41b67b7b370d2ce21b665cf2b0a
Adam Salma <adam3salma@gmail.com>
2017-02-05 23:52:57 +0000

    Initial client API

---
+++ src/client/api/index.js
@@ -0,0 +1,10 @@
+export default {
+    boardlist: ({boardID}) => 
+        `/api/4chan/boards`,
+    board: ({boardID}) => 
+        `/api/4chan/board/${boardID}`,
+    archive: ({boardID}) => 
+        `/api/4chan/${boardID}/archive`,
+    thread: ({boardID, threadID}) => 
+        `/api/4chan/board/${boardID}/thread/${threadID}`,
+}
