13ff344b81f5a723cd4b6b43f721ada2838bb952
Adam Salma <adam3salma@gmail.com>
2017-02-12 18:37:08 +0000

    server(route): Add If-Mmodified-Since to thread requests

--- src/server/routes/4chan/getThread.js
+++ src/server/routes/4chan/getThread.js
@@ -4,10 +4,18 @@ import { parseThread } from '../../parsers/4chanParser';
 import { chan as options } from '../../config/requestHeaders';
 import { fourchanAPI } from '../../config/apiEndpoints';
 
+import moment from 'moment'
+
+const timeFormat = 'ddd[,] M MMM YYYY hh:mm:ss [GMT]'
 
 export default function (req, res, next) {
-    const { boardID, threadID } = req.params;
+    const { boardID, threadID, requestedAt } = req.params;
     const url = fourchanAPI(boardID, threadID).thread
+
+    log.info("requestedAt:",requestedAt)
+    if (requestedAt) {
+        options['If-Modified-Since'] = moment(requestedAt).format(timeFormat)
+    }
 
     log.http(`Fetching Thread from ${url}`)
 
