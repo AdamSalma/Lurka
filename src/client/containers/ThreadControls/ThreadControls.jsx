9b483a854ea6f4e015793b494e63700f124d5fdd
Adam Salma <adam3salma@gmail.com>
2017-02-20 23:26:07 +0000

    refactor(ThreadControls): Add archive, update, comment controllers

--- src/client/containers/ThreadControls/ThreadControls.jsx
+++ src/client/containers/ThreadControls/ThreadControls.jsx
@@ -5,78 +5,39 @@ import uuid from "uuid";
 
 import { Icon, ButtonCircle } from '~/components'
 
-import {WatchController} from './controllers'
+import {
+    WatchController, 
+    ArchiveController, 
+    CommentController, 
+    UpdateController
+} from './controllers'
 
 
 export default class ThreadControls extends Component {
-    constructor(props) {
-        super(props);
-        this.subprops = {
-
-        }
-    }
-
     render() {
-        const {
-            thread:{isActive, posts, requestedAt, didInvalidate}, 
-            status: {threadID, boardID},
-            unmonitorThread,
-            monitorThread 
-        } = this.props;
+        const {thread:{ isActive, posts }} = this.props;
 
         const controlClasses = classNames('thread-controls', {
             "animate-in": isActive && posts.length,
             "animate-out": !isActive
         })
 
-        
-
         return (
             <div className={controlClasses}>
                 <div className="controls left-controls">
-                    
-                    {/*Save to archive
-                        <Icon name="delete" /> 
-                        bin icon -> to remove from archive
 
-                       OR use this instead of 'download':
-                        <Icon name="library-plus" /> 
-
-                       OR this. looks the best imo:
-                        <Icon name="package-down" />   
-                    */}
-                    <ButtonCircle toggleProps={{name:"package-down"}}>
-                        <Icon name="package-down" /> 
-                    </ButtonCircle>
-
+                    <ArchiveController {...this.props}/>
                     <WatchController {...this.props}/>
-                    
+
                 </div>
                 <div className="controls right-controls">
+
+                    <CommentController {...this.props}/>
+                    <UpdateController {...this.props}/>
 
                     {/*<ButtonCircle toggleProps={{name:"download"}}>
                         <Icon name="download" /> 
                     </ButtonCircle>*/}
-
-                    
-                    {/* OR
-                        right facing:
-                        <Icon name="message" />
-                        <Icon name="message-text" />
-                        
-                        left facing:
-                        <Icon name="message-reply-text" />  
-                        <Icon name="message-reply-text" />
-                        <Icon name="reply" />  addow facing left
-
-                    */}
-                    <ButtonCircle toggleProps={{name:"comment-text"}}>
-                        <Icon name="comment-text"/>
-                    </ButtonCircle>
-
-                    <ButtonCircle toggleProps={{name:"update"}}>
-                        <Icon name="update" />
-                    </ButtonCircle>
 
                     {/*<ButtonCircle toggleProps={{name:"close"}}>
                         <Icon name="close" />
@@ -84,14 +45,6 @@ export default class ThreadControls extends Component {
                 </div>
             </div>
         )
-    }
-
-    showControls() {
-
-    }
-
-    hideControls() {
-
     }
 }
 
