b72c5b325cca4bbadd112684b0e4d6765c6e962c
Adam Salma <adam3salma@gmail.com>
2017-02-22 23:51:35 +0000

    feat(Spinner): Spin enabled with no props

--- src/client/components/Spinner.jsx
+++ src/client/components/Spinner.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import classNames from 'classnames';
 
-export default ({ isSpinning }) => {
+export default ({ isSpinning=true }) => {
     const spinnerClasses = classNames("spinner", {
         "spinner-active": isSpinning
     })
