06e8d17d479aeeefedf0498db987b364b59d5563
Adam Salma <adam3salma@gmail.com>
2017-02-22 23:53:12 +0000

    refactor(BoardPost): decouple render, use new components

--- src/client/containers/BoardPost/BoardPost.jsx
+++ src/client/containers/BoardPost/BoardPost.jsx
@@ -1,9 +1,22 @@
 import React, {Component} from 'react';
 
-import Tooltip from '../../components/Tooltip';
-import Counter from "../../components/Counter"
+import {
+    Image,
+    Counter,
+    Tooltip,
+    Spinner,
+    Icon
+} from '~/components'
 
-import Velocity from "velocity-animate"
+
+function setHTML(html) {
+    return {
+        dangerouslySetInnerHTML: {
+            __html: html
+        }
+    }
+}
+
 
 export default class BoardPost extends Component {
 
@@ -11,8 +24,12 @@ export default class BoardPost extends Component {
         super(props);
         this.state = {
             maxPostHeight: 400,
-            isMovable: null
+            isMovable: null,
+            imageInvalidated: false
         }
+
+        this.renderImage = this.renderImage.bind(this)
+        this.renderComment = this.renderComment.bind(this)
 
         this.scrollPostUp = this.scrollPostUp.bind(this)
         this.scrollPostDown = this.scrollPostDown.bind(this)
@@ -20,45 +37,61 @@ export default class BoardPost extends Component {
     }
 
     render() {
-        const { onClick, onLoad, post: {
-            id, title, comment, date, media, replies
-        }} = this.props;
+        const { onClick, post } = this.props;
 
         return (
             <div 
-                id={"t"+id}
-                className="board-post"
+                id={"t" + post.id} className="board-post"
                 onClick={ onClick }
                 onMouseEnter={this.scrollPostUp}
                 onMouseLeave={this.scrollPostDown}
             >
-                <div className="image-wrap">   
-                    <img ref="image" src={media.thumbnail} onLoad={onLoad} onError={this.hideImage}/>
-                </div>   
-                <div className="comment-wrap clearfix" ref="comment">
-                    <div className="counters">
-                        <div className="float-right">
-                            <i className="mdi mdi-comment-text"/>
-                            <Counter value={replies.textCount}/>
-                        </div>
-                        <div className="float-right">
-                            <i className="mdi mdi-image-filter-hdr"/>
-                            <Counter value={replies.imgCount} />
-                        </div>
+                {this.renderImage()}
+                {this.renderComment()}
+            </div>
+        )
+    }
+
+    renderImage() {
+        const {post:{media}, onLoad} = this.props
+        return !this.state.imageInvalidated ? (
+            <div className="image-wrap">
+                <Image 
+                    src={media.thumbnail} 
+                    width={media.width} height={media.height}
+                    loader={<Spinner/>}
+                    onLoad={onLoad} onError={this.hideImage}/>
+            </div>
+        ) : false
+    }
+
+    renderComment() {
+        const {post:{ title, comment, date, media, replies }} = this.props
+
+        return (
+            <div className="comment-wrap clearfix" ref={el => this._comment = $(el)}>
+                <div className="counters">
+                    <div className="float-right">
+                        <Icon name="comment-text"/>
+                        <Counter value={replies.textCount}/>
                     </div>
-                    <div className="op">
-                        <b dangerouslySetInnerHTML={{__html: title}} className="title" />
-                        <div dangerouslySetInnerHTML={{__html: comment}} />
+                    <div className="float-right">
+                        <Icon name="image-filter-hdr"/>
+                        <Counter value={replies.imgCount} />
                     </div>
+                </div>
+                <div className="op">
+                    <b {...setHTML(title)} className="title" />
+                    <div {...setHTML(comment)} />
                 </div>
             </div>
         )
     }
 
     scrollPostUp(){
-        const { comment, image } = this.refs;
-        const imgHeight = image.clientHeight, 
-              commentHeight = comment.clientHeight,
+        // TODO: get height from props not ref
+        const imgHeight = this.props.post.media.height, 
+              commentHeight = this._comment.clientHeight,
               maxHeight = this.state.maxPostHeight
 
         if (imgHeight + commentHeight > maxHeight ) {
@@ -69,15 +102,17 @@ export default class BoardPost extends Component {
             const offScreenAmount = commentHeight - (maxHeight - imgHeight) + 5 // 5px for padding at bottom of comment
             const height = imgHeight/1.5 // 3/4 of image height
             const distance = offScreenAmount < height ? offScreenAmount : height
-            Velocity(comment, {top: `-${distance}px`}, 200, 'linear')
+            this._comment.velocity({top: `-${distance}px`}, 200, 'linear')
         }
     }
  
     scrollPostDown(){
-        Velocity(this.refs.comment, {top: 0}, 200)
+        this._comment.velocity({top: 0}, 200)
     }
 
     hideImage() {
-        this.refs.image.classList.add('hidden')
+        this.setState({
+            imageInvalidated: true
+        })
     }
 }
