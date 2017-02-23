cae8886c922c4429793f9012ec9dd73b2dd6fd7a
Adam Salma <adam3salma@gmail.com>
2017-02-23 00:19:45 +0000

    style(media): assume 4chan

--- src/server/routes/media.js
+++ src/server/routes/media.js
@@ -1,24 +1,18 @@
 import Express from "express";
 import request from "request";
+import Axios from "axios";
 import headers from '../config/requestHeaders'
 
 const router = Express.Router();
 var options;
 
 router.get('/', function(req, res, next){
-    const {url, provider} = req.query
+    const { url, provider } = req.query
     
-    if (provider === "4chan") {
-        options = headers.chan
-        options.headers["host"] = "i.4cdn.org"
-    }
-
-    if (provider === "reddit") {
-        options = headers.reddit
-        // options.headers["host"] = "i.4cdn.org"
-    }
-
+    options = headers.chan
+    options.headers["host"] = "i.4cdn.org"
     options.url = unescape(url)
+
     request.get(options).pipe(res).on('error', function(err){
         console.error("Caught media error!")
         console.log(err.message)
