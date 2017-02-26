b540719a40915c91b44e05c743b5d38d6990460d
Adam Salma <adam3salma@gmail.com>
2017-02-26 23:58:14 +0000

    feat(BoardPost): Add lazyloading, remove mouseover

--- src/client/containers/BoardPost/BoardPost.jsx
+++ src/client/containers/BoardPost/BoardPost.jsx
@@ -1,13 +1,16 @@
 import React, {Component} from 'react';
+import LazyLoad from 'react-lazyload';
 
 import {
-    Image,
     Counter,
     Tooltip,
     Spinner,
-    Icon
+    Icon,
+    Image,
+    Line
 } from '~/components'
 
+import PostHeader from './BoardPostHeader'
 
 function setHTML(html) {
     return {
@@ -23,91 +26,51 @@ export default class BoardPost extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            maxPostHeight: 400,
+            maxCommentHeght: 150,
             isMovable: null,
             imageInvalidated: false
         }
 
-        this.renderImage = this.renderImage.bind(this)
-        this.renderComment = this.renderComment.bind(this)
-
-        this.scrollPostUp = this.scrollPostUp.bind(this)
-        this.scrollPostDown = this.scrollPostDown.bind(this)
         this.hideImage = this.hideImage.bind(this)
     }
 
     render() {
-        const { onClick, post } = this.props;
+        const { onClick, onLoad, post } = this.props;
 
         return (
-            <div 
-                id={"t" + post.id} className="board-post"
-                onClick={ onClick }
-                onMouseEnter={this.scrollPostUp}
-                onMouseLeave={this.scrollPostDown}
-            >
-                {this.renderImage()}
-                {this.renderComment()}
+            <div id={"t" + post.id} className="board-post" onClick={ onClick }>
+                {this.renderImage(onLoad, post)}
+                {this.renderComment(post)}
             </div>
         )
     }
 
-    renderImage() {
-        const {post:{media}, onLoad} = this.props
-        return !this.state.imageInvalidated ? (
-            <div className="image-wrap">
+    renderImage(onLoad, {media}) {
+        return <div className="image-wrap">
+            <LazyLoad height={media.height} once>
                 <Image 
                     src={media.thumbnail} 
                     width={media.width} height={media.height}
-                    loader={<Spinner/>}
-                    onLoad={onLoad} onError={this.hideImage}/>
-            </div>
-        ) : false
+                    onLoad={onLoad} onError={this.hideImage}
+                    hideUntilLoaded loader={<Spinner/>}
+                />
+            </LazyLoad>
+        </div>
     }
 
-    renderComment() {
-        const {post:{ title, comment, date, media, replies }} = this.props
-
+    renderComment({ title, comment, date, replies }) {
         return (
-            <div className="comment-wrap clearfix" ref={el => this._comment = $(el)}>
-                <div className="counters">
-                    <div className="float-right">
-                        <Icon name="comment-text"/>
-                        <Counter value={replies.textCount}/>
+            <div className="comment-wrap">
+                <div className="comment-slider" ref={el => this._comment = $(el)}>
+                    <PostHeader replies={replies} date={date}/>
+                    <Line />
+                    <div className="op">
+                        { title && <b {...setHTML(title)} className="title" /> }
+                        <div {...setHTML(comment)} className="comment"/>
                     </div>
-                    <div className="float-right">
-                        <Icon name="image-filter-hdr"/>
-                        <Counter value={replies.imgCount} />
-                    </div>
-                </div>
-                <div className="op">
-                    <b {...setHTML(title)} className="title" />
-                    <div {...setHTML(comment)} />
                 </div>
             </div>
         )
-    }
-
-    scrollPostUp(){
-        // TODO: get height from props not ref
-        const imgHeight = this.props.post.media.height, 
-              commentHeight = this._comment.clientHeight,
-              maxHeight = this.state.maxPostHeight
-
-        if (imgHeight + commentHeight > maxHeight ) {
-            // comment is long and offscreen; make it more viewable by moving it up
-            
-            // move by difference, else if that would move comment outside 
-            // thread, use image height (scroll to top)
-            const offScreenAmount = commentHeight - (maxHeight - imgHeight) + 5 // 5px for padding at bottom of comment
-            const height = imgHeight/1.5 // 3/4 of image height
-            const distance = offScreenAmount < height ? offScreenAmount : height
-            this._comment.velocity({top: `-${distance}px`}, 200, 'linear')
-        }
-    }
- 
-    scrollPostDown(){
-        this._comment.velocity({top: 0}, 200)
     }
 
     hideImage() {
