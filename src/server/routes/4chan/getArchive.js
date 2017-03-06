24cc6925e25bbe7a10a99bd2bfac91cc8d63245f
Adam Salma <adam3salma@gmail.com>
2017-03-06 00:46:32 +0000

    server(4chan routes): improve flow, add error handling

--- src/server/routes/4chan/getArchive.js
+++ src/server/routes/4chan/getArchive.js
@@ -1,14 +1,18 @@
 import Axios from 'axios';
 
+import options from '../../config/requestHeaders';
 import { parseArchive } from '../../parsers/4chanParser'
-import { chan as options } from '../../config/requestHeaders';
 import { fourchanAPI } from '../../config/apiEndpoints';
-
+import { writeObjToRoot } from '../../services/inspector'
 export default function (req, res, next) {
     const { boardID } = req.params;
     const url = fourchanAPI(boardID).archive
     log.http(`Fetching archive from ${url}`)
     Axios(url, options)
         .then( board => res.send(board.data) )  // TODO: Parse 4chan archive
-        .catch( err => next(err));
+        .catch( err => {
+            log.error('Archive fetch error:', err)
+            writeObjToRoot('4chan_archive_error.json', err)
+            next(err)
+        });
 };
--- src/server/routes/4chan/getBoard.js
+++ src/server/routes/4chan/getBoard.js
@@ -1,14 +1,22 @@
 import Axios from 'axios';
 
+import options from '../../config/requestHeaders';
 import { parseBoard } from '../../parsers/4chanParser';
-import { chan as options } from '../../config/requestHeaders';
 import { fourchanAPI } from '../../config/apiEndpoints';
+import { writeObjToRoot } from '../../services/inspector'
+
 
 export default function (req, res, next) {
     const { boardID } = req.params;
     const url = fourchanAPI(boardID).board
-	log.http(`Fetching board from ${url}`)
+    log.http(`Fetching board from ${url}`)
     Axios(url, options)
-        .then( board => res.send(parseBoard(board.data, boardID)) )
-        .catch( err => next(err));
+        .then( r => r.data )
+        .then( data => parseBoard(data, boardID) )
+        .then( board => res.send(board) )
+        .catch( err => {
+            log.error('Board fetch error:', err)
+            writeObjToRoot('4chan_board_error.json', err)
+            next(err)
+        });
 }
