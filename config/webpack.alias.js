b1a5efd4feb2ec84edb8351fcc6780d1ad8c58cd
Adam Salma <Adam.Salma@kantarworldpanel.com>
2017-03-06 17:07:10 +0000

    (webpack): Add preact to aliases to replace react

--- config/webpack.alias.js
+++ config/webpack.alias.js
@@ -4,5 +4,7 @@ var rootPath = path.join(__dirname, '..')
 module.exports = { 
     '~': path.join(rootPath, 'src', 'client'),
     '~server': path.join(rootPath, 'src', 'server'),
-    '~root': rootPath
+    '~root': rootPath,
+    'react': 'preact-compat',
+	'react-dom': 'preact-compat'
 }
