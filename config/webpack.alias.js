7911d5b49fe8068c248ab8db98484bfca6e4ef2a
Adam Salma <adam3salma@gmail.com>
2017-03-12 23:46:56 +0000

    (config): Remove preact for now. Problems with event bubbling.

--- config/webpack.alias.js
+++ config/webpack.alias.js
@@ -4,7 +4,5 @@ var rootPath = path.join(__dirname, '..')
 module.exports = { 
     '~': path.join(rootPath, 'src', 'client'),
     '~server': path.join(rootPath, 'src', 'server'),
-    '~root': rootPath,
-    'react': 'preact-compat',
-	'react-dom': 'preact-compat'
+    '~root': rootPath
 }
