42fa9551d2b6c913cd4669362406d1452dbe4bda
Adam Salma <adam3salma@gmail.com>
2017-03-12 23:42:41 +0000

    (server): Change request headers to prefer JSON

--- src/server/config/requestHeaders.js
+++ src/server/config/requestHeaders.js
@@ -2,7 +2,7 @@ export default {
     headers: {
         "Host": "a.4cdn.org",
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:53.0) Gecko/20100101 Firefox/53.0",
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+        "Accept": "application/json,text/html,application/xml;q=0.9,*/*;q=0.8",
         "Accept-Language": "en-GB,en;q=0.5",
         "Accept-Encoding": "gzip, deflate",
         "DNT": "1",
