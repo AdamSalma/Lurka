e9f1dc042b4bbe3ad91d66d0da1156d3955e5b27
Adam Salma <adam3salma@gmail.com>
2017-03-12 23:22:25 +0000

    (Video): Switch to wrapper rather than custom controls

--- src/client/components/Video.jsx
+++ src/client/components/Video.jsx
@@ -1,23 +1,16 @@
-import React from 'react';
-import videoConnect from 'react-html5video';
- 
-const Video = ({ video, videoEl, children, ...restProps }) => (
-    <div className="video" onClick={ e => e.stopPropagation()}>
-        <video {...restProps}>
-            { children }
-        </video>
-        <p>
-            Here are the video properties for the above HTML5 video:
-            { JSON.stringify(video) }
-        </p>
-        <a href="#" onClick={(e) => {
-            e.preventDefault();
-            // You can do what you like with the HTMLMediaElement DOM element also. 
-            videoEl.pause();
-        }}>
-            Pause video
-        </a>
-    </div>
-);
- 
-export default videoConnect(Video)
+import { DefaultPlayer as Video } from 'react-html5video';
+import 'react-html5video/dist/styles.css';
+
+/**
+ * Wrapper to avoid messy imports.
+ */
+export default ({controls, poster, onCanPlayThrough, children, ...restProps}) => {
+    return (
+        <Video {...restProps}
+            controls={controls}
+            poster={poster}
+            onCanPlayThrough={onCanPlayThrough}>
+            {children}
+        </Video>
+    );
+}
