3813997bc9497fcd96d6493ff87066feca179563
Adam Salma <adam3salma@gmail.com>
2017-03-19 23:58:07 +0000

    feat(DualMedia): New component

---
+++ src/client/components/DualMedia.jsx
@@ -0,0 +1,52 @@
+import React, { Component, PropTypes } from 'react';
+import classes from 'classnames'
+import { bindMembersToClass } from '~/utils'
+
+
+export default class DualMedia extends Component {
+    static propTypes = {
+        thumbnail: PropTypes.element,
+    }
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            toggled: false
+        }
+
+        bindMembersToClass(this, 'toggle')
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        return nextState.toggled !== this.state.toggled
+    }
+
+    render() {
+        const {thumbnail, children, className, ...restProps} = this.props
+        const dualMediaClasses = classes("dual-media", className, {
+            "toggled": this.state.toggled 
+        })
+
+        console.warn(this.state.toggled)
+        return (
+            <div {...restProps} className={dualMediaClasses} onClick={this.toggle}>
+                <div className="dual-media-thumbnail">
+                    {thumbnail}
+                </div>
+                { this.state.toggled && 
+                    <div className="dual-media-children">
+                        {children}
+                    </div>
+                }
+            </div>
+        )
+    }
+
+    toggle() {
+        console.log("DualMedia.toggle()")
+        this.setState({
+            toggled: !this.state.toggled
+        })
+    }
+}
