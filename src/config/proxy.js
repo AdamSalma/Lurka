522220cdc6d9c82094f62cb2e5ae7425ea638e1a
Adam Salma <adam3salma@gmail.com>
2017-04-01 19:42:11 +0100

    feat(config): Combine server/root configs and move to src/

---
+++ src/config/proxy.js
@@ -0,0 +1,14 @@
+export default {
+    headers: {
+        "Host": "a.4cdn.org",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:53.0) Gecko/20100101 Firefox/53.0",
+        "Accept": 'application/json, text/plain, */*',
+        "Accept-Language": "en-GB,en;q=0.5",
+        "Accept-Encoding": "gzip, deflate",
+        "DNT": "1",
+        "Connection": "keep-alive",
+        "Upgrade-Insecure-Requests": "1",
+        "Pragma": "no-cache",
+        "Cache-Control": "no-cache"
+    }   
+}
