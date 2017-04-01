8e7f5aba0dbb5f6071c4c99562446d79a6814693
Adam Salma <adam3salma@gmail.com>
2017-04-01 19:43:43 +0100

    chore(webpack): Move files from config to own folder

---
+++ webpack/webpack.loaders.js
@@ -0,0 +1,37 @@
+module.exports = [
+	{
+		test: /\.jsx?$/,
+		exclude: /node_modules/,
+		loader: 'babel',
+	},
+    {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        loaders: ['style', 'css', 'postcss', 'sass']
+    },
+    {
+        test: /\.sass$/,
+        exclude: /node_modules/,
+        loaders: ['style', 'css', 'postcss', 'sass']
+    },
+	{
+		test: /\.css$/,
+		loaders: ['style', 'css']
+	},
+	{
+		test: /\.json$/,
+		loader: 'json'
+	},
+	{ 
+		test: /.(png|gif|woff(2)?|eot|ttf|svg)(\?[a-z0-9=\.]+)?$/, 
+		loader: 'url-loader?limit=100000&name=[name]-[hash].[ext]' 
+	},
+	{
+		test: /\.(md|ejs)$/, 
+		loader: 'ignore-loader' 
+	},
+	{
+		test: /\LICENSE$/, 
+		loader: 'ignore-loader' 
+	}
+];
---
+++ webpack/webpack.prod.server.js
@@ -0,0 +1,42 @@
+var webpack = require('webpack');
+var loaders = require('./webpack.loaders');
+var path = require('path');
+var fs = require('fs');
+ 
+var outpath = path.join(__dirname, "..", "app");
+var node_modules = path.join(outpath, "node_modules");
+ 
+// excludes some node_modules files from bundle to avoid errors (esp with Express)
+var modules = {};
+fs.readdirSync(node_modules).filter(function(x) {
+    return ['.bin'].indexOf(x) === -1;
+}).forEach(function(mod) {
+    modules[mod] = 'commonjs ' + mod;
+});
+ 
+module.exports = {
+    target: "node",
+    node: {
+        __dirname: false
+    },
+    externals: modules,
+    entry: `./src/server/`,
+    output: {
+        path: outpath,
+        filename: 'server.bundle.js'
+    },
+    resolve: {
+        extensions: ['', '.js', '.json'],
+        root: node_modules
+    },
+    module: {
+        loaders: loaders
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify('production')
+            }
+        })
+    ]
+};
