55658b2478dd8eb9495e7a1302422f854430e2b9
Adam Salma <adam3salma@gmail.com>
2017-04-02 22:46:06 +0100

    fix(electron): Remove config import due to not found error when compiled

--- src/electron/index.js
+++ src/electron/index.js
@@ -1,7 +1,6 @@
 const {app, BrowserWindow} = require('electron')
 
-const {server} = require('../config')
-const serverURL = server.schema +'://'+ server.host +':'+ server.port
+const serverURL = 'http://127.0.0.1:3000'
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
