aa49e9a2902b2b23f455fccf9e3be70f4f49ffce
Adam Salma <adam3salma@gmail.com>
2017-04-02 22:38:42 +0100

    refactor(Header): Use new sub components

--- src/modules/Header/Header.jsx
+++ src/modules/Header/Header.jsx
@@ -2,35 +2,21 @@ import './Header.styles'
 import React, { Component } from "react";
 import classes from "classnames";
 
-import {clearState} from '~/redux/store/localStorage'
-import {
-    LogoText,
-    Icon,
-    SearchBox,
-    Hierarchy,
-    HeaderItem
-} from "~/components"
+import {bindMembersToClass} from '~/utils'
 
-import {bindMembersToClass, findParentWithClass} from '~/utils'
+import MainNav from './MainNav'
+import SubNav from './SubNav'
+// import DropdownNav from './DropdownNav'
 
-const HeaderIcon = ({name, onClick, active, ...restProps}) => {
-    return (
-        <HeaderItem 
-        className={classes("header-icon", {"active": active})} 
-        onClick={onClick} {...restProps}>
-            <Icon name={name}/>
-        </HeaderItem>
-    )
-}
 
 export default class Header extends Component {
     constructor(props) {
         super(props);
-        bindMembersToClass(this, 
-            'handleKeyUp',
-            'toggleActive',
-            'handleIconClick'
-        )
+        // bindMembersToClass(this, 
+        //     'handleKeyUp',
+        //     'toggleActive',
+        //     'handleIconClick'
+        // )
     }
 
     render() {
@@ -49,57 +35,22 @@ export default class Header extends Component {
         const placeholder = `Search ${threadIsActive ? "thread" : "board"} ...`
 
         return (
-            <div id="header" className="header">
-                <div className="header-background"/>
-                <div className='header-content'>
-                    <HeaderIcon name="menu" 
-                        active={isNavbarOpen}
-                        onClick={toggleNavbar}
-                    />
-
-                    <HeaderItem className="version" onClick={clearState}>
-                        <LogoText />
-                    </HeaderItem>
-                    <HeaderItem className="breadcrumb">
-                        <Hierarchy provider={provider} boardID={boardID} threadID={threadID}/>
-                    </HeaderItem>
-                    <HeaderItem className="searchbox">
-                        <SearchBox placeholder={placeholder} onKeyUp={this.handleKeyUp}/>
-                    </HeaderItem>
-
-                    <HeaderIcon name="eye" 
-                        onClick={this.handleIconClick.bind(null, 'watch')}
-                        active={activePanel === 'watch'}
-                    />
-                    <HeaderIcon name="archive" 
-                        onClick={this.handleIconClick.bind(null, 'archive')}
-                        active={activePanel === 'archive'}
-                    />
-                    <HeaderIcon name="filter" 
-                        onClick={this.handleIconClick.bind(null, 'filter')}
-                        active={activePanel === 'filter'}
-                    />
-                    <HeaderIcon name="sort" 
-                        onClick={this.handleIconClick.bind(null, 'sort')}
-                        active={activePanel === 'sort'}
+            <div id="header" className="Header">
+                <div className="background"/>
+                <div className='content'>
+                    <MainNav 
+                        activePanel={activePanel}
+                        toggleHeaderPanel={toggleHeaderPanel}
+                        toggleContentNav={() => console.log("toggleContentNav")}
+                        cycleContentNav={({left, right}) => 
+                            console.log(`cycleContentNav. left: ${left}, right: ${right}`)}
+                        threadID={threadID}
+                        boardID={boardID}
+                        threadIsActive={threadIsActive}
                     />
                 </div>
             </div>
-        )  // TODO: Add filter functionality + buttons
-    }
-
-    handleKeyUp(event) {
-        this.props.searchBoard(event.target.value)
-    }
-
-    handleIconClick(panel) {
-        console.log('handleIconClick()')
-        this.props.toggleHeaderPanel({panel})
-    }
-
-    toggleActive(e) {
-        console.log('toggling active')
-        findParentWithClass(e.target, 'header-item').classList.toggle('active')
+        )
     }
 }
 
