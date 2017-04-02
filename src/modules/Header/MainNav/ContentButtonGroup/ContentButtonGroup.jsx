106f58341c5c87f8dd75ab550dfff9fd242ccc26
Adam Salma <adam3salma@gmail.com>
2017-04-02 22:37:53 +0100

    feat(Header): Initial MainNav subcomponent

---
+++ src/modules/Header/MainNav/ContentButtonGroup/ContentButtonGroup.jsx
@@ -0,0 +1,27 @@
+import './ContentButtonGroup.styles';
+import React, { PropTypes } from 'react';
+import { Icon } from '~/components';
+
+const ContentButtonGroup = ({ className, children, onArrowClick, onButtonClick }) => {
+    return (
+        <div className={[
+            "ContentButtonGroup", 
+            className
+        ].join(' ')}>
+            <div className="arrow arrow-left" onClick={() => onArrowClick({left:true})}>
+                <Icon name="arrow-left"/>
+            </div>
+            <button className="button" onClick={onButtonClick}>
+                {children}
+            </button>
+            <div className="arrow arrow-right" onClick={() => onArrowClick({right:true})}>
+                <Icon name="arrow-right"/>
+            </div>
+
+        </div>
+    );
+};
+
+ContentButtonGroup.displayName = 'ContentButtonGroup';
+
+export default ContentButtonGroup;
