2b0416d40fe3210e1765100e4ddafdd990a951f5
Adam Salma <adam3salma@gmail.com>
2017-04-03 08:11:29 +0100

    feat(Header): Add title to HeaderIcons

--- src/modules/Header/HeaderIconGroup/HeaderIconGroup.jsx
+++ src/modules/Header/HeaderIconGroup/HeaderIconGroup.jsx
@@ -21,37 +21,37 @@ class HeaderIconGroup extends Component {
                 {icons.map(iconName => {
                     switch (iconName) {
                         case 'watch':
-                            return <HeaderIcon name="eye" key="watch"
+                            return <HeaderIcon name="eye" key="watch" title="watch"
                                 onClick={() => onClick('watch')}
                                 active={activePanel === 'watch'}
                             />
                         case 'archive':
-                            return <HeaderIcon name="archive" key="archive"
+                            return <HeaderIcon name="archive" key="archive" title="archive"
                                 onClick={() => onClick('archive')}
                                 active={activePanel === 'archive'}
                             />
                         case 'account':
-                            return <HeaderIcon name="account" key="account"
+                            return <HeaderIcon name="account" key="account" title="account"
                                 onClick={() => onClick('account')}
                                 active={activePanel === 'account'}
                             />
                         case 'filter':
-                            return <HeaderIcon name="filter" key="filter"
+                            return <HeaderIcon name="filter" key="filter" title="filter"
                                 onClick={() => onClick('filter')}
                                 active={activePanel === 'filter'}
                             />
                         case 'sort':
-                            return <HeaderIcon name="sort" key="sort"
+                            return <HeaderIcon name="sort" key="sort" title="sort"
                                 onClick={() => onClick('sort')}
                                 active={activePanel === 'sort'}
                             />
                         case 'search':
-                            return <HeaderIcon name="magnify" key="search"
+                            return <HeaderIcon name="magnify" key="search" title="search"
                                 onClick={() => onClick('search')}
                                 active={activePanel === 'search'}
                             />
                         case 'layout':
-                            return <HeaderIcon name="view-dashboard" key="layout"
+                            return <HeaderIcon name="view-dashboard" key="layout" title="layout"
                                 onClick={() => onClick('layout')}
                                 active={activePanel === 'layout'}
                             />
