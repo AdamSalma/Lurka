37f61e8f19556d701e21d3908baaa9f56ab9a139
Adam Salma <adam3salma@gmail.com>
2017-04-09 11:01:06 +0100

    feat(Header): Initial module structure

---
+++ src/modules/Header/Header.jsx
@@ -0,0 +1,54 @@
+import React, { Component, PropTypes } from 'react';
+
+import Sidebar from './Sidebar'
+import SidePanels from './SidePanels'
+import { bindMembersToClass } from '~/utils'
+
+class Header extends Component {
+    static propTypes = {
+        className: PropTypes.string,
+    };
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            activePanel: null
+        }
+
+        bindMembersToClass(this, 'togglePanel')
+    }
+
+    render() {
+        return (
+            <div className="Header">
+                <Sidebar 
+                    togglePanel={this.togglePanel}
+                />
+                <SidePanels
+                    activePanel={this.state.activePanel}
+                />
+            </div>
+        );
+    }
+
+    togglePanel(panel) {
+        this.setState( state => {
+            return {
+                activePanel: state.panel === panel ? null : panel
+            }
+        })
+    }
+}
+
+
+function mapStateToProps({ status, board }) {
+    return {    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({
+
+    }, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
