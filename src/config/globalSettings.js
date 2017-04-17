c58ccc57d1332fda1fd8e8cf83c5dbbb02c5ac51
Adam Salma <adam3salma@gmail.com>
2017-04-17 23:49:48 +0100

    feat: Add global settings to window

---
+++ src/config/globalSettings.js
@@ -0,0 +1,8 @@
+/**
+ * Global settings that the user should not have access to through the store
+ */
+window.appSettings = {
+    headerHeight: 60,
+    drawerWidth: 250,
+    drawerAnimationDuration: 400
+}
