72c1480808d99e54a07861f17928fb767cbd8e4d
Adam Salma <adam3salma@gmail.com>
2017-04-18 09:01:48 +0100

    feat(Board): Make layout configurable and more performant

--- src/modules/Board/layout.js
+++ src/modules/Board/layout.js
@@ -1,38 +1,61 @@
-var colCount, colWidth, outerMargin, $items, windowWidth
 
-var margin = 25
-var blocks = []
-var gutter = 60
 
-export default function setupBlocks() {
-	$items = $('.BoardPost')
-	if (!$items.length) return // sometimes called before posts are rendered
+export default function createLayout(options) {
+    var {
+        margin = 25,
+        gutterLeft = 60,
+        gutterRight = 60,
+        targetSelector,
+        containerSelector,
+    } = options;
 
-	blocks = []
-	windowWidth = $(window).width() - gutter
-	colWidth = $items.outerWidth()
-	colCount = Math.floor(windowWidth / (colWidth+margin) )
-	outerMargin = windowWidth - colCount * (colWidth+margin) + gutter
+    var colCount,
+        colWidth,
+        outerMargin,
+        $items,
+        containerWidth,
+        blocks
 
-	for (let i=0; i < colCount; i++) {
-		blocks.push(margin)
-	}
+    return (reuseElements=false) => {
+        discoverItems(reuseElements)
+        positionItems()
+    }
 
-	positionBlocks()
+    function discoverItems(reuseElements) {
+        if (!reuseElements) {
+            $items = $(targetSelector)
+        }
+
+        // sometimes called before items are rendered
+        if (!$items || !$items.length)
+            return
+
+        blocks = []
+        containerWidth = $(containerSelector).width() - gutterRight - gutterLeft
+        colWidth = $items.outerWidth() + margin
+        colCount = Math.floor(containerWidth / colWidth)
+        outerMargin = containerWidth - colCount * colWidth
+
+        for (let i=0; i < colCount; i++) {
+            blocks.push(margin)
+        }
+    }
+
+    function positionItems() {
+        $items && $items.each(function(){
+            var min = Array.min(blocks)
+            var index = $.inArray(min, blocks)
+            var leftPos = gutterLeft + outerMargin/2 + margin/2 + (index * colWidth)
+            $(this).css({
+                'top': min+'px',
+                'left': leftPos+'px'
+            })
+
+            blocks[index] = min + $(this).outerHeight() + margin
+        })
+    }
 }
 
-function positionBlocks() {
-	$items.each(function(){
-		var min = Array.min(blocks)
-		var index = $.inArray(min, blocks)
-		var leftPos = outerMargin/2 + margin/2 + (index * (colWidth+margin))
-		$(this).css({
-			'top': min+'px',
-			'left': leftPos+'px'
-		})
-		blocks[index] = min+$(this).outerHeight()+margin
-	})	
-}
 
 // Function to get the Min value in Array
 Array.min = function(array) {
