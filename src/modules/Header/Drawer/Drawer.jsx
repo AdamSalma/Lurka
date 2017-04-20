f335e6ad15074327a003f3b5b4ff24c735af3ec4
Adam Salma <adam3salma@gmail.com>
2017-04-20 02:20:04 +0100

    feat(Header): Add Searchbar and styles to drawer

--- src/modules/Header/Drawer/Drawer.jsx
+++ src/modules/Header/Drawer/Drawer.jsx
@@ -1,17 +1,62 @@
 import './Drawer.styles';
 import React, { Component, PropTypes } from 'react';
+import cx from 'classnames'
+
+import {
+    SearchBar, LogoText
+} from '~/components'
+
+import {
+    invokeAfterUninterruptedDelay
+} from '~/utils'
 
 class Drawer extends Component {
     constructor(props) {
         super(props);
+        this.throttleSearch = invokeAfterUninterruptedDelay(200, this.handleSearch)
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        const { isDrawerOpen } = this.props
+
+        if (isDrawerOpen && prevProps.isDrawerOpen !== isDrawerOpen) {
+            console.warn("Focusing drawer seabox")
+            this.SearchBarRef.focus()
+        }
     }
 
     render() {
+        const { isThreadOpen, isDrawerOpen } = this.props
+        const searchPlaceholder = `Search ${isThreadOpen ? 'thread': 'board'}`
+
+        const drawerClass = cx('Drawer', {
+            'is-open': isDrawerOpen,
+            'is-closed': !isDrawerOpen
+        })
+
         return (
-            <div className="Drawer">
-                Drawer
+            <div id="drawer" className={drawerClass}>
+                <div className="header">
+                    <SearchBar showIcons
+                        ref={el => this.SearchBarRef = el}
+                        placeholder={searchPlaceholder}
+                        onChange={this.throttleSearch}
+                    />
+                </div>
+                <div className="container">
+                    <div className="content">
+                        Content
+                    </div>
+                    <div className="footer">
+                        <LogoText />
+                    </div>
+                </div>
             </div>
         );
+    }
+
+    handleSearch(e, search) {
+        console.log('handleSearch:', arguments)
     }
 }
 
