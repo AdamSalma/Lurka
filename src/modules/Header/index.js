d6cca364081e4a517a8d36ed2e7322a2d8552122
Adam Salma <adam3salma@gmail.com>
2017-04-20 02:28:12 +0100

    feat(Header): Connect to redux store

--- src/modules/Header/index.js
+++ src/modules/Header/index.js
@@ -1,1 +1,46 @@
-export {default} from './Header'
+import Header from './Header'
+
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+
+import {
+    fetchBoardList,
+    fetchBoard,
+    closeThread,
+    destroyBoard,
+    toggleDrawer,
+    fetchThread,
+    updateMonitoredThread,
+    monitorThread,
+    unmonitorThread
+} from '~/redux/actions';
+
+function mapStateToProps({ status, display, boardList, threadMonitor, settings }) {
+    return {
+        status,
+        activePanel: display.activeHeaderPanel,
+        isThreadOpen: display.isThreadOpen,
+        isDrawerOpen: display.isDrawerOpen,
+        boardID: status.boardID,
+        threadID: status.threadID,
+        threadMonitor,
+        boardList,
+        settings,
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({
+        fetchBoardList,
+        fetchBoard,
+        closeThread,
+        destroyBoard,
+        toggleDrawer,
+        fetchThread,
+        updateMonitoredThread,
+        monitorThread,
+        unmonitorThread
+    }, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header)
