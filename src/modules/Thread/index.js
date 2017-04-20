e48790216e4fd7174b421e1bdcc1e1879a90f523
Adam Salma <adam3salma@gmail.com>
2017-04-20 02:29:37 +0100

    feat(Thread): Connect to redux store

--- src/modules/Thread/index.js
+++ src/modules/Thread/index.js
@@ -1,4 +1,35 @@
 import Thread from './Thread'
-import threadConnect from './ThreadHOC'
 
-export default threadConnect(Thread)
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+
+import {
+    closeThread,
+    monitorThread,
+    unmonitorThread,
+    scrollHeader,
+    toggleHeaderPanel
+} from '~/redux/actions';
+
+function mapStateToProps({ status, thread, display }) {
+    return {
+        isThreadOpen: display.isThreadOpen,
+        isCommentPanelOpen: display.isCommentPanelOpen,
+        isDrawerOpen: display.isDrawerOpen,
+        boardID: status.boardID,
+        threadID: status.threadID,
+        ...thread
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({
+        closeThread,
+        monitorThread,
+        unmonitorThread,
+        scrollHeader,
+        toggleHeaderPanel
+    }, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Thread)
