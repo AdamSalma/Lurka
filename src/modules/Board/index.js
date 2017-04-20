15f305ea357d749890a6ac7b51562b4ffc5f2cea
Adam Salma <adam3salma@gmail.com>
2017-04-20 02:30:32 +0100

    feat(Board): Connect to redux store

--- src/modules/Board/index.js
+++ src/modules/Board/index.js
@@ -1,1 +1,32 @@
-export {default} from './Board'
+import Board from './Board'
+
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+
+import {
+    fetchBoard,
+    loadMorePosts,
+    scrollHeader,
+    fetchThread
+} from '~/redux/actions';
+
+function mapStateToProps({ status, display, board, thread }) {
+    return {
+        status,
+        board,
+        isAppReady:   display.isAppReady,
+        isDrawerOpen: display.isDrawerOpen,
+        isThreadOpen: display.isThreadOpen
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({
+        fetchBoard,
+        fetchThread,
+        loadMorePosts,
+        scrollHeader
+    }, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Board)
