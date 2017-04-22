530e278a94c1dbbf74b896de0efa6a2817b2c796
Adam Salma <adam3salma@gmail.com>
2017-04-22 23:55:44 +0100

    feat(reducer): Add cache reducing for board/thread

---
+++ src/redux/reducers/cache.js
@@ -0,0 +1,37 @@
+import initialState from '../initialState';
+import {
+    BOARD_CACHE_LOADED,
+    BOARD_CACHE_SAVED,
+    BOARD_CACHE_CLEARED,
+
+    THREAD_CACHE_LOADED,
+    THREAD_CACHE_SAVED,
+    THREAD_CACHE_CLEARED,
+} from '../types'
+
+export default function (state = initialState.cache, action) {
+    switch (action.type) {
+        case BOARD_CACHE_SAVED:
+            return Object.assign({}, state, Object.assign({}, state.board, {
+                [action.boardID]: action.payload
+            }))
+
+        case THREAD_CACHE_SAVED:
+            return Object.assign({}, state, Object.assign({}, state.thread, {
+                [action.threadID]: action.payload
+            }))
+
+        case BOARD_CACHE_CLEARED:
+            return Object.assign({}, state, {
+                board: {}
+            })
+
+        case THREAD_CACHE_CLEARED:
+            return Object.assign({}, state, {
+                thread: {}
+            })
+
+        default:
+            return state
+    }
+}
