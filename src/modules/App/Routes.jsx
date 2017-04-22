17ffea8e3d9c10a3997f491d1143d66c0e629d09
Adam Salma <adam3salma@gmail.com>
2017-04-23 00:09:32 +0100

    feat: Add routes incase future switch

--- src/modules/App/Routes.jsx
+++ src/modules/App/Routes.jsx
@@ -1,4 +1,26 @@
 import React, { Component } from "react";
 
+import {
+    Router,
+    Route,
+    IndexRoute,
+    hashHistory
+} from "react-router";
+
+import HomeView   from '~/views/HomeView';
+import ContentView from '~/views/ContentView';
+import SettingsView from '~/views/SettingsView';
+
+const Routes = () => {
+    return (
+        <Provider store={store}>
+            <Router history={hashHistory}>
+                <Route exact path="/" component={HomeView} />
+                <Route path='content' component={ContentView}/>
+                <Route path='settings' component={SettingsView}></Route>
+            </Router>
+        </Provider>
+    )
+}
 
 export default Routes
