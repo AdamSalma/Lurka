945daf2e41e88fffc6ef07282da9f70539f13448
Adam Salma <adam3salma@gmail.com>
2017-04-23 00:17:12 +0100

    feat(appSettings): Add icons + enable testing

--- src/config/globalSettings.js
+++ src/config/globalSettings.js
@@ -1,8 +1,25 @@
 /**
- * Global settings that the user should not have access to through the store
+ * Global settings that do not change in a production environment
  */
+
+if (typeof window === 'undefined') {
+    // For tests
+    global.window = {}
+}
+
 window.appSettings = {
     headerHeight: 60,
     drawerWidth: 250,
-    drawerAnimationDuration: 400
+    drawerAnimationDuration:500,
+
+    icons: {
+        navbarArchive: 'ios-box',
+        navbarEye:     'ios-eye',
+        navbarChevron: 'ios-arrow-right',
+        navbarSearch:  'ios-search-strong',
+        navbarCompose: 'compose',
+        navbarRefresh: 'ios-reload',
+
+        threadPostMenu: 'more',
+    }
 }
