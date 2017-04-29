e0c685d096bac50e01946d43ab1c7c034ae68d98
Adam Salma <adam3salma@gmail.com>
2017-04-29 23:57:39 +0100

    feat(appSettings): Add view ID's and board layout options

--- src/config/globalSettings.js
+++ src/config/globalSettings.js
@@ -8,16 +8,25 @@ if (typeof window === 'undefined') {
 }
 
 window.appSettings = {
+    boardOuterMargin: 30,
+    boardPostMargin: 25,
+
+    // The following settings can't be changed explcitly.
+    // Needs to be kept in sync with src/styles/base/variables
     headerHeight: 60,
     drawerWidth: 250,
-    drawerAnimationDuration:500,
+    drawerAnimationDuration: 500,
+
+    homeViewID: 'HomeView',
+    contentViewID: 'ContentView',
+    settingsViewID: 'SettingsView',
 
     icons: {
         navbarArchive: 'ios-box',
         navbarEye:     'ios-eye',
         navbarChevron: 'ios-arrow-right',
         navbarSearch:  'ios-search-strong',
-        navbarCompose: 'compose',
+        navbarCompose: 'ios-plus-outline',
         navbarRefresh: 'ios-reload',
 
         threadPostMenu: 'more',
