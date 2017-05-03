b77d85ad867a8ecea02aa0dffbafcb101433fa57
Adam Salma <adam3salma@gmail.com>
2017-05-03 01:16:18 +0100

    test(setup): Import client settings

--- test/setup.js
+++ test/setup.js
@@ -1,12 +1,9 @@
 import expect from 'expect';
 import expectJSX from 'expect-jsx';
 import {shallow} from './helpers';
+import '../config/client.settings'
 
 expect.extend(expectJSX);
 
 global.expect = expect;
 global.shallow = shallow;
-
-global.window = {
-    appSettings: {}
-}
