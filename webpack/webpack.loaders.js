c2552ec8349097875fae9778462c188c00a38ffc
Adam Salma <adam3salma@gmail.com>
2017-05-09 23:58:57 +0100

    chore(webpack): Indent with spaces

--- webpack/webpack.loaders.js
+++ webpack/webpack.loaders.js
@@ -1,32 +1,32 @@
 module.exports = [
-	{
-		test: /\.jsx?$/,
-		exclude: /node_modules/,
-		loader: 'babel',
-	},
+    {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+    },
     {
         test: /\.sass|scss$/,
         exclude: /node_modules/,
         loaders: ['style', 'css?sourceMap', 'postcss?sourceMap', 'sass?sourceMap']
     },
-	{
-		test: /\.css$/,
-		loaders: ['style', 'css?sourceMap']
-	},
-	{
-		test: /\.json$/,
-		loader: 'json'
-	},
-	{
-		test: /.(png|gif|woff(2)?|eot|ttf|svg)(\?[a-z0-9=\.]+)?$/,
-		loader: 'url-loader?limit=100000&name=[name].[ext]'
-	},
-	{
-		test: /\.(md|ejs)$/,
-		loader: 'ignore-loader'
-	},
-	{
-		test: /\LICENSE$/,
-		loader: 'ignore-loader'
-	}
+    {
+        test: /\.css$/,
+        loaders: ['style', 'css?sourceMap']
+    },
+    {
+        test: /\.json$/,
+        loader: 'json'
+    },
+    {
+        test: /.(png|gif|woff(2)?|eot|ttf|svg)(\?[a-z0-9=\.]+)?$/,
+        loader: 'url-loader?limit=100000&name=[name].[ext]'
+    },
+    {
+        test: /\.(md|ejs)$/,
+        loader: 'ignore-loader'
+    },
+    {
+        test: /\LICENSE$/,
+        loader: 'ignore-loader'
+    }
 ];
--- webpack/webpack.prod.client.js
+++ webpack/webpack.prod.client.js
@@ -9,44 +9,44 @@ var app = path.join(__dirname, "..", "app")
 var node_modules = path.join(app, "node_modules")
 
 module.exports = {
-	target: "electron",
-	entry: {
+    target: "electron",
+    entry: {
         app: path.join(src, 'index.jsx')
-	},
-	output: {
-		path: app,
-		filename: '[name].bundle.js',
-		publicPath: require('../config').server.url,
-		sourceMapFilename: '[name].bundle.map'
-	},
-	resolve: {
-		extensions: ['', '.js', '.jsx', '.css', '.scss', '.sass', '.json'],
-		root: node_modules,
-		alias: require("./webpack.alias")
-	},
-	module: {
-		loaders: loaders
-	},
-	plugins: [
+    },
+    output: {
+        path: app,
+        filename: '[name].bundle.js',
+        publicPath: require('../config').server.url,
+        sourceMapFilename: '[name].bundle.map'
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx', '.css', '.scss', '.sass', '.json'],
+        root: node_modules,
+        alias: require("./webpack.alias")
+    },
+    module: {
+        loaders: loaders
+    },
+    plugins: [
         new webpack.optimize.UglifyJsPlugin({
-        	sourceMap: false,
-    		warnings: false,
+            sourceMap: false,
+            warnings: false,
         }),
-		new webpack.ProvidePlugin({
-			$: "jquery",
-			jQuery: "jquery",
-			"window.jQuery": "jquery"
-		}),
-		new webpack.DefinePlugin({
-			'process.env': {
-				'NODE_ENV': JSON.stringify('production')
-			}
-		}),
-		new HtmlWebpackPlugin({
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery",
+            "window.jQuery": "jquery"
+        }),
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify('production')
+            }
+        }),
+        new HtmlWebpackPlugin({
             template: path.join(src, 'index.html')
         })
-	],
-	postcss: function () {
+    ],
+    postcss: function () {
         return [autoprefixer, require('postcss-nested')];
     }
 };
