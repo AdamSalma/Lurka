21343a1c2828e69679759e601c2ec51b1936e3f5
Adam Salma <adam3salma@gmail.com>
2017-05-09 23:47:55 +0100

    feat(utils): Move isElementInViewport fn from Thread

--- src/utils/dom.js
+++ src/utils/dom.js
@@ -1,17 +1,34 @@
 /**
- * Traverses up the DOM until an element is found that contains the passed in 
+ * Traverses up the DOM until an element is found that contains the passed in
  * class
- * 
+ *
  * @param  {Object} el   - The initial DOM element reference
  * @param  {String} clss - Name of the class to check for
  * @return {Object}      - The first DOM element that has the specified class
  */
-export function findParentWithClass(el, clss) {
+export const findParentWithClass = (el, clss) => {
     while (true) {
         if (el.classList.contains(clss)) {
-            break
+            break;
         }
-        el = el.parentNode
+        el = el.parentNode;
     }
-    return el
+    return el;
+}
+
+
+// http://stackoverflow.com/a/7557433
+export const isElementInViewport = (el) => {
+    if (typeof jQuery === "function" && el instanceof jQuery) {
+        el = el[0];
+    }
+
+    var rect = el.getBoundingClientRect();
+
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= $(window).height() &&
+        rect.right <= $(window).width()
+    );
 }
