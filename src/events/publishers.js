1f9cf330084937d997887d2ad43ccf8c0e355087
Adam Salma <adam3salma@gmail.com>
2017-05-11 00:00:09 +0100

    feat(events): Bind dispatches early for performance

--- src/events/publishers.js
+++ src/events/publishers.js
@@ -1,6 +1,7 @@
 import { dispatch } from "pub-sub-es6";
 import * as types from "./types"
 
-export const emitDrawerToggle = () => {
-    dispatch(types.DRAWER_TOGGLE);
-}
+export const emitAppReady = dispatch.bind(null, types.APP_READY);
+
+export const emitDrawerToggle = dispatch.bind(null, types.TOGGLE_DRAWER);
+export const emitContentViewToggle = dispatch.bind(null, types.TOGGLE_CONTENT_VIEW);
