2e951d9e8357e88e8574c74fb688140bdf123669
Adam Salma <adam3salma@gmail.com>
2017-05-12 23:30:48 +0100

    feat(events): Add thread and media reel

--- src/events/publishers.js
+++ src/events/publishers.js
@@ -5,3 +5,7 @@ export const emitAppReady = dispatch.bind(null, types.APP_READY);
 
 export const emitDrawerToggle = dispatch.bind(null, types.TOGGLE_DRAWER);
 export const emitContentViewToggle = dispatch.bind(null, types.TOGGLE_CONTENT_VIEW);
+export const emitMediaReelOpen = dispatch.bind(null, types.OPEN_MEDIA_REEL);
+
+export const emitThreadOpen = dispatch.bind(null, types.THREAD_OPEN);
+export const emitThreadClose = dispatch.bind(null, types.THREAD_CLOSE);
--- src/events/subscribers.js
+++ src/events/subscribers.js
@@ -2,22 +2,34 @@ import { on } from 'pub-sub-es6';
 import * as types from './types';
 
 /**
- * Event subscribers used as component decorators
+ * Event subscribers used as component method decorators
  *
  * @onAppReady
- * onAppReady() { ... }
+ * onAppReady(eventArgs) { ... }
  */
 
 // Note: Subscribers must use `function` not `=>` because of scope binding
 
-export const onAppReady = function() {
+export const onAppReady = function () {
     on(types.APP_READY).apply(null, arguments);
 }
 
-export const onContentViewToggle = function() {
+export const onContentViewToggle = function () {
     on(types.TOGGLE_CONTENT_VIEW).apply(null, arguments);
 }
 
-export const onDrawerToggle = function() {
+export const onDrawerToggle = function () {
     on(types.TOGGLE_DRAWER).apply(null, arguments);
+}
+
+export const onMediaReelOpen = function () {
+    on(types.OPEN_MEDIA_REEL).apply(null, arguments);
+}
+
+export const onThreadOpen = function () {
+    on(types.THREAD_OPEN).apply(null, arguments);
+}
+
+export const onThreadClose = function () {
+    on(types.THREAD_CLOSE).apply(null, arguments);
 }
