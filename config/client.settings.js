fdb6fbb4d3ad48863fe6a63837d538e4cd5b6eed
Adam Salma <adam3salma@gmail.com>
2017-05-13 20:44:09 +0100

    feat(appSettings): Add icons and comments

--- config/client.settings.js
+++ config/client.settings.js
@@ -1,5 +1,5 @@
 /**
- * Global settings that do not change in a production environment.
+ * Immutable global app settings used to configure how Lurka behaves.
  *
  * To see the effects you must either rebuild the project or run in dev mode.
  * See package.json -> scripts
@@ -10,12 +10,20 @@ if (typeof window === 'undefined') {
 }
 
 window.appSettings = {
+    // The spacing between board posts
     boardOuterMargin: 30,
     boardPostMargin: 25,
+
+    // when you click on a reference in a thread
+    threadpostScrollDuration: 600,
+
+    // how long to keep a thread highlighted on scroll
+    threadpostScrollHighlightDuration: 2000,
 
     // The following settings can't be changed explcitly.
     // Needs to be kept in sync with src/styles/base/variables
     headerHeight: 60,
+    threadWidth: 981,
     drawerWidth: 250,
     drawerAnimationDuration: 500,
 
@@ -28,9 +36,17 @@ window.appSettings = {
         navbarEye:     'ios-eye',
         navbarChevron: 'ios-arrow-right',
         navbarSearch:  'ios-search-strong',
-        navbarCompose: 'ios-plus-outline',
-        navbarRefresh: 'ios-reload',
+        navbarCompose: 'plus',
+        navbarRefresh: 'refresh',
+
+        threadPostImageFullscreen: 'arrow-expand',
 
         threadPostMenu: 'more',
+        threadPostControlsReport: "flag",
+        threadPostControlsDownload: "arrow-down-a",
+        threadPostControlsReply: "chatbox-working",
+        threadPostControlsImageSearch: "ios-search-strong",
+        threadPostControlsHide: "minus-circled"
+
     }
 }
