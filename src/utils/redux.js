d4eaad30493b2f92cf9c057933e9e3c3941f267f
Adam Salma <Adam.Salma@kantarworldpanel.com>
2017-05-17 14:59:12 +0100

    feat(utils): Add mergeState for reducers

--- src/utils/redux.js
+++ src/utils/redux.js
@@ -1,19 +1,21 @@
 /**
- * Uses a dictionary hash table rather than a switch statement to execute 
- * reducers, which slightly increases performance on reducers with 
- * multiple handlers 
+ * Uses a dictionary hash table rather than a switch statement to execute
+ * reducers, which slightly increases performance on reducers with
+ * multiple handlers
  */
-export const createReducer = (initialState, handlers) => 
+export const createReducer = (initialState, handlers) =>
     return (state=initialState, action) =>
-        handlers.hasOwnProperty(action.type) 
-            ? handlers[action.type](state, action) 
+        handlers.hasOwnProperty(action.type)
+            ? handlers[action.type](state, action)
             : state;
 
 /**
- * Creates a new state object without mutating it.
- * @param  {...Object} - any number of objects to merge into the new state
+ * Extend a state object without mutating it.
+ *
+ * @param  {Object} oldState - The old store state
+ * @param  {Object} newState - The new store state
  * @return {Object}
  */
-export const newState = function() {
-    return Object.assign.apply(null, [{}, arguments])
+export const mergeState = function(oldState, newState) {
+    return Object.assign({}, oldState, newState);
 }
