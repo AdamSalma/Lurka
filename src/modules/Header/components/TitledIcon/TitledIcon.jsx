01c06e0904f1d88331a5fe3e253e4aa04c841b48
Adam Salma <adam3salma@gmail.com>
2017-05-22 01:49:23 +0100

    feat(SubHeader): Style TitledIcon

--- src/modules/Header/components/TitledIcon/TitledIcon.jsx
+++ src/modules/Header/components/TitledIcon/TitledIcon.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import './TitledIcon.styles';
 import Icon from '~/components/Icon';
 
 export default function TitledIcon ({ name, title, className }) {
-    return <div className={className}>
-        <Icon name={name}/>
+    return <div className={className)}>
+        <Icon name={name} className="TitledIcon"/>
         <span>{title}</span>
     </div>
 }
