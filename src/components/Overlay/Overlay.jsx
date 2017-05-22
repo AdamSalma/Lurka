1411ee9dcd529bb3a4095540409cb47d0174e955
Adam Salma <adam3salma@gmail.com>
2017-05-22 01:58:57 +0100

    refactor(Overlay): Add state to animate in out using ref

--- src/components/Overlay/Overlay.jsx
+++ src/components/Overlay/Overlay.jsx
@@ -1,17 +1,42 @@
 import './Overlay.styles'
-import React from 'react';
+import React, { Component, PropTypes } from 'react';
 import cx from 'classnames';
 
-const Overlay = ({ isVisible=true, className, children, ...restProps }) => {
-    return <div className={cx("Overlay", className, {
-        "Overlay-active": isVisible
-    })} {...restProps}>
-        {children}
-    </div>
-}
+class Overlay extends Component {
+    static propTypes = {
+        className: PropTypes.string,
+    };
 
-Overlay.defaultProps = {
-    isVisible: true
+    constructor(props) {
+        super(props);
+        this.state = {
+            fade: props.isVisible
+        }
+    }
+
+    render() {
+        const { isVisible, className, children, ...restProps } = this.props;
+        return <div className={cx("Overlay", className, {
+            "Overlay-active": isVisible,
+            "fade": this.state.fade
+        })} {...restProps}>
+            {children}
+        </div>
+    }
+
+    show() {
+        console.warn("Overlay.show()")
+        this.setState({
+            fade: true
+        });
+    }
+
+    hide() {
+        console.warn("Overlay.hide()")
+        this.setState({
+            fade: false
+        });
+    }
 }
 
 export default Overlay
