1f868b0a8ace8a4d85d791d07b96d91efd3af0dd
Adam Salma <adam3salma@gmail.com>
2017-05-23 00:21:24 +0100

    fix(Header): Remove extra bracket

--- src/modules/Header/components/TitledIcon/TitledIcon.jsx
+++ src/modules/Header/components/TitledIcon/TitledIcon.jsx
@@ -3,7 +3,7 @@ import './TitledIcon.styles';
 import Icon from '~/components/Icon';
 
 export default function TitledIcon ({ name, title, className }) {
-    return <div className={className)}>
+    return <div className={className}>
         <Icon name={name} className="TitledIcon"/>
         <span>{title}</span>
     </div>
