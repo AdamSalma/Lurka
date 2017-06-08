9f13d3d904a00a5917115f32ba1e18a0d8913337
Adam Salma <adam3salma@gmail.com>
2017-06-08 03:13:00 +0100

    feat(HomeView): Use Container

--- src/views/HomeView.jsx
+++ src/views/HomeView.jsx
@@ -2,12 +2,17 @@ import React, { PropTypes } from 'react';
 import cx from 'classnames'
 // import BoardSelection from '~/modules/BoardSelection';
 import Home from '~/modules/Home';
+import { Container } from '~/components';
+import './HomeView.styles';
 
 const HomeView = ({ className, children }) => {
     return (
         <section className={cx('View HomeView', className)}>
             <Home.containers.BoardSearch/>
-            <Home.containers.BoardList/>
+            <Container className="HomeView__Container">
+                <Home.containers.BoardList/>
+                <Home.containers.HomeBoard/>
+            </Container>
             {/*<BoardSelection />*/}
         </section>
     );
