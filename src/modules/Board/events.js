18decc0076faf09d5778cab48843bf7a8ea1047c
Adam Salma <Adam.Salma@kantarworldpanel.com>
2017-06-12 15:51:43 +0100

    remove: Board/events - unused

--- src/modules/Board/events.js
+++
@@ -1,9 +0,0 @@
-// TODO: Sort this out
-export function catchTooltip(board) {
-	$(board).on("hover", ".BoardPost", function( event ) {
-		console.info('BoardPost mouseover')
-        event.stopPropagation();
-        $(this).find('.Tooltip')
-        	.toggleClass('Tooltip-active')
-    });
-}
