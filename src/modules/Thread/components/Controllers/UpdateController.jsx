a20d7dc4b94c4faa4fb387a91b5c25de7438d54c
Adam Salma <Adam.Salma@kantarworldpanel.com>
2017-06-12 15:36:52 +0100

    feat(Thread): Enable old thread controls

--- src/modules/Thread/components/Controllers/UpdateController.jsx
+++ src/modules/Thread/components/Controllers/UpdateController.jsx
@@ -5,26 +5,26 @@ export default function(props) {
     // const { } = props;
 
     const toggledProps = {
-        name: "update"
+        name: "refresh-1"
     }
 
     const defaultProps = {
-        name: "update"
+        name: "refresh-1"
     }
 
     {/* OR
         right facing:
         <Icon name="message" />
         <Icon name="message-text" />
-        
+
         left facing:
-        <Icon name="message-reply-text" />  
+        <Icon name="message-reply-text" />
         <Icon name="message-reply-text" />
         <Icon name="reply" />  addow facing left
 
     */}
 
     return <ButtonCircle toggleProps={toggledProps}>
-        <Icon {...defaultProps} /> 
+        <Icon {...defaultProps} />
     </ButtonCircle>
 }
