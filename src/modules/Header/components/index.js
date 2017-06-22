6612a02424c142b5192440de6030ff61c43b3ba4
Adam Salma <Adam.Salma@kantarworldpanel.com>
2017-06-22 14:35:42 +0100

    feat(Header): Add component FullLogo

--- src/modules/Header/components/index.js
+++ src/modules/Header/components/index.js
@@ -1,5 +1,6 @@
 export {default as BoardSpecs} from './BoardSpecs';
 export {default as ContentButtonGroup} from './ContentButtonGroup';
+export {default as FullLogo} from './FullLogo';
 export {default as HeaderItem} from './HeaderItem';
 export {default as IconGroup} from './IconGroup';
 export {default as Title} from './Title';
