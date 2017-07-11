971dcb19a33cdc1bdcebe3bd2ec7614148b1918f
Adam Salma <Adam.Salma@kantarworldpanel.com>
2017-07-11 11:43:25 +0100

    thread(config): Change bezier-curves

--- src/UI/containers/Thread/config.js
+++ src/UI/containers/Thread/config.js
@@ -4,9 +4,17 @@ export const animationStyles = {
     in: {
         translateY: [headerHeight, "100vh"],
         translateZ: 0, // Force hardware acceleration by animating a 3D property
+        scale: [1,1],
+        opacity: [1, 1],
     },
+    // out: {
+    //     translateY: [window.innerHeight, headerHeight],
+    //     translateZ: 0,
+    // }
     out: {
-        translateY: [window.innerHeight, headerHeight],
+        // scale: 0,
+        opacity: 0,
+        // translateY: [window.innerHeight, headerHeight],
         translateZ: 0,
     }
 }
@@ -14,10 +22,13 @@ export const animationStyles = {
 export const animationOptions = {
     in: {
         duration: 800,  // this too
-        easing: [0.165, 0.84, 0.44, 1],
+        easing: [0.19, 1, 0.22, 1],       // ease out expo
+        // easing: [0.23, 1, 0.32, 1],       // ease out quint
     },
     out: {
         duration: 400,
-        easing: [0.25, 0.8, 0.25, 1],
+        // easing: [0.25, 0.8, 0.25, 1],
+        // easing: [0.23, 1, 0.32, 1],       // ease out quint
+        easing: [0.19, 1, 0.22, 1],       // ease out expo
     }
 }
