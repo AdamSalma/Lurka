564806c99fddcf9fb6f4672774c9c4f1113f91ba
Adam Salma <adam3salma@gmail.com>
2017-07-16 11:58:22 +0100

    tweak(server): use aliased import path on webpack middleware

--- src/Server/middleware/webpackMiddleware.js
+++ src/Server/middleware/webpackMiddleware.js
@@ -1,16 +1,16 @@
 import webpack from 'webpack';
 import webpackDevMiddleware from 'webpack-dev-middleware';
 import webpackHotMiddleware from 'webpack-hot-middleware';
-import webpackConfig from '../../../webpack/webpack.dev.js';
+import config from '-/webpack/webpack.dev.js';
 
-const webpackCompiler = webpack(webpackConfig);
+const webpackCompiler = webpack(config);
 
 // enable webpack middleware for hot-reloads in development
 export default function useWebpackMiddleware(app) {
 
     log.app("Bundling client code...")
     app.use(webpackDevMiddleware(webpackCompiler, {
-        publicPath: webpackConfig.output.publicPath,
+        publicPath: config.output.publicPath,
         stats: {
             colors: true,
             chunks: false,
