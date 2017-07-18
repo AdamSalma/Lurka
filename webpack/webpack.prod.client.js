12e40a187750b967c57e51860c59bb259f84c102
Adam Salma <adam3salma@gmail.com>
2017-07-18 20:30:35 +0100

    build: update webpack minifiyjs args

--- webpack/webpack.prod.client.js
+++ webpack/webpack.prod.client.js
@@ -26,8 +26,23 @@ module.exports = {
     module: { loaders: loaders },
     plugins: [
         new webpack.optimize.UglifyJsPlugin({
-            warnings: false
-        }),
+            warnings: false,
+            sourceMap: true,
+            compress: {
+              screw_ie8: true, // React doesn't support IE8
+              warnings: isVerbose,
+              unused: true,
+              dead_code: true,
+            },
+            mangle: {
+              screw_ie8: true,
+            },
+            output: {
+              comments: false,
+              screw_ie8: true,
+            },
+          }),
+        ]),
         new webpack.ProvidePlugin({
             $: "jquery",
             jQuery: "jquery",
