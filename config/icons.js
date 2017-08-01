1e2ba20ba68e2e6d49b7929083caa2599b304493
Adam Salma <adam3salma@gmail.com>
2017-08-01 23:48:03 +0100

    config: Move icons to new file

---
+++ config/icons.js
@@ -0,0 +1,67 @@
+module.exports = {
+
+    // navbarMenu: "navicon",
+    navbarForwards: "arrow-big-right",
+    navbarBackwards: "arrow-big-left",
+
+    navbarInfo: 'information',
+    navbarRefresh: 'refresh',
+
+    navbarAccount:  'person-add',
+    navbarBookmark: 'bookmark-outline',
+    navbarDB:       'data',
+    navbarEye:      'ios-clock',
+    navbarSettings: 'cog',
+    navbarTheme: 'paint-brush-2',
+    navbarMenu: 'menu',
+
+    subNavbarOpen: 'chevron-down',
+    subNavbarClose: 'chevron-up',
+
+    searchMagnify: "search-1",
+    searchClose: "times",
+
+    videoFullscreen: "expand-2",
+    videoDownload: "download-1",
+    videoPlay: "playback-play",
+    videoPause: "playback-pause",
+    videoError: "warning-alt",
+    videoVolumeOff: "volume-disabled",
+    videoVolumeMute: "volume-mute",
+    videoVolumeLow: "volume-low",
+    videoVolumeMedium: "volume-med",
+    videoVolumeHigh: "volume-high",
+
+    boardPostReplyCount: "mesage-1",
+    boardPostImageCount: "image",
+
+    // threadPostImageFullscreen: 'arrow-expand',
+
+    threadPostMenu: 'dot-3',
+    threadPostControlsReport: "flag",
+    threadPostControlsBookmark: "bookmark",
+    threadPostControlsReply: "quote",
+    threadPostControlsImageSearch: "search-2",
+    threadPostControlsHide: "minus-circle",
+
+    watchPanelClose: "times",
+    // watcchPanelUpdate: "update",
+
+    boardToolbarSort: "sort-az",
+    boardToolbarFilter: "filter-full",
+    boardToolbarLayout: "layout",
+    boardToolbarInfo: "info",
+    boardToolbarOpen: "chevron-up",
+    boardToolbarClose: "chevron-down",
+
+    subheaderToolbarSort: "sort-az",
+    subheaderToolbarFilter: "filter-full",
+    subheaderToolbarChangeLayout: "layout",
+    subheaderToolbarNewThread: "plus-1",
+    subheaderToolbarRefresh: "refresh-1",
+    subheaderToolbarArchive: "archive",
+    subheaderToolbarSettings: "settings",
+    subheaderToolbarRedo: "forwards",
+    subheaderToolbarUndo: "backwards"
+
+}
