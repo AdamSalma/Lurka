2a71f2f318df12f224581dcf3822196645e18df2
Adam Salma <adam3salma@gmail.com>
2017-08-08 23:49:17 +0100

    feat(components): Create SearchBarWithIcons, simplify SearchBar

--- src/app/components/elements/SearchBar/index.js
+++ src/app/components/elements/SearchBar/index.js
@@ -1,1 +1,52 @@
-export {default} from './Searchbar'
+import React, { PureComponent } from 'react';
+import cx from 'classnames'
+import './styles'
+
+import { Icon } from '~/components'
+import { bindMembersToClass } from '~/utils/react'
+
+const i = window.appSettings.icons;
+
+class Searchbar extends PureComponent {
+    setRef = (ref) => this._searchbar = ref
+
+    render() {
+        const {
+            className,
+            ...restProps
+        } = this.props;
+
+        console.error("Searchbar.Render()")
+        const classNames = cx('Searchbar', className)
+
+        return (
+            <input className={classNames}
+                maxLength="100"
+                autoCapitalize="none"
+                autoComplete="off"
+                autoCorrect="off"
+                {...restProps}
+                type="text"
+                ref={this.setRef}
+            />
+        )
+    }
+
+    get value () {
+        return this._searchbar.value
+    }
+
+    focus = () => {
+        console.log(this._searchbar)
+        this._searchbar && this._searchbar.focus()
+    }
+
+    clear = () => {
+        this._searchbar.value = ''
+        this.setState({
+            'searchValue': ''
+        })
+    }
+}
+
+export default Searchbar;
--- src/app/components/index.js
+++ src/app/components/index.js
@@ -1,5 +1,6 @@
 export * from './compound'
 export * from './elements'
 export * from './functional'
+export * from './grids'
 export * from './hoc'
 export * from './layout'
