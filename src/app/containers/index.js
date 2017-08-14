35064a1195cacbe5527497d6b66c782e877fb532
Adam Salma <Adam.Salma@kantarworldpanel.com>
2017-08-14 14:54:50 +0100

    fix(containers): add Alerts + Watcher imports in index

--- src/app/containers/index.js
+++ src/app/containers/index.js
@@ -1,3 +1,4 @@
+export {default as Alerts} from './Alerts';
 export {default as App} from './App';
 export {default as Board} from './Board';
 export {default as Dashboard} from './Dashboard';
@@ -6,3 +7,4 @@ export {default as MediaViewer} from './MediaViewer';
 export {default as Panels} from './Panels';
 export {default as Settings} from './Settings';
 export {default as Thread} from './Thread';
+export {default as Watcher} from './Watcher';
