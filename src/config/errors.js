124d8d48bbdba28cf1cbdc8bf5c58b180062f357
Adam Salma <Adam.Salma@kantarworldpanel.com>
2017-08-29 09:47:17 +0100

    config: Flatten error obj messages

--- src/config/errors.js
+++ src/config/errors.js
@@ -1,6 +1,4 @@
-export default {
-    api: {
-        noResponse: "No response from 4chan. (Check your internet connection)",
-        internalError: "Internal error occured while preparing to make request. Contact developers if error persists."
-    }
+module.exports = module.exports.default = {
+    apiNoResponse: "No response from 4chan. (Are you connected to the internet?)",
+    apiInternalError: "Internal error occured while preparing to make request. Contact developers if error persists."
 }
