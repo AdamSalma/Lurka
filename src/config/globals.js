dac1729ea21bcf8e5f2ca41845c956640e3065d6
Adam Salma <adam3salma@gmail.com>
2017-09-28 20:12:59 +0100

    feat(globals): Put app settings under global Lurka namespace

---
+++ src/config/globals.js
@@ -0,0 +1,66 @@
+/**
+ * Immutable global app settings used to configure how Lurka behaves.
+ */
+
+if (typeof window === 'undefined') {
+    global.window = {}  // For tests
+}
+
+window.Lurka = {};
+Lurka.defaultTheme = "dark-red"
+
+if (process.env.NODE_ENV !== "production") {
+    // IIFE allows the `require()` to only happen once.
+    Lurka.performanceTest = ((testDuration=5000) => {
+        const Perf =  require('react-addons-perf');
+        return () => {
+            Perf.start()
+            setTimeout(() => {
+                Perf.stop();
+                Perf.printDOM();
+                Perf.printWasted();
+            }, testDuration);
+        }
+    })();
+
+    // JQuery is only global inside webpack
+    Lurka.$ = $
+}
+
+Lurka.iconPackName = 'lurka';
+Lurka.icons = require('./icons');
+
+// Duration, type and message for alerts
+Lurka.alerts = require('./alerts');
+
+// Notification messages etc. Reusable.
+Lurka.errors = require('./errors');
+
+Lurka.settings = {
+    // The spacing between board posts
+    boardOuterMargin: 30,
+    boardPostMargin: 25,
+
+    // when you click on a threadpost quotelink
+    threadpostScrollDuration: 600,
+
+    // how long to keep a post highlighted on scroll
+    threadpostScrollHighlightDuration: 2000,
+
+    // The following settings can't be changed explcitly.
+    // Needs to be kept in sync with src/app/sass/partials/_config.sass
+    headerHeightExpanded: 112,
+    headerHeight: 64,
+    subheaderHeight: 48,
+    threadWidth: 850,
+    settingsWidth: 320,
+
+    // Seconds to backoff when making a request
+    apiBackoff: [10, 15, 20, 30, 60, 90, 120, 180, 240, 300],
+
+    // Which side of the screen alerts pop up from
+    alertPosition: "top left"
+}
+
+// Makes object immutable.
+require('deep-freeze-node')(Lurka);
