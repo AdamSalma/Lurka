bc1026f346dd039727c93edab913c17fb47f84e1
Adam Salma <Adam.Salma@kantarworldpanel.com>
2017-10-11 11:18:32 +0100

    refactor(sass): Archive unused folders

--- tools/webpack/index.js
+++ tools/webpack/index.js
@@ -1,21 +1,32 @@
 require('babel-register');
 
 module.exports = module.exports.default = function getConfig(env) {
-    // App config - dev/prod
-    if (env.dev) {
-        return require('./configs/dev');
+    // Note: All configs run in production, except for dev
+
+    switch (env) {
+        /**
+         * App Configs
+         */
+        case "dev":
+        case "development":
+            return require('./configs/dev');
+
+        case "preview":
+            return require('./configs/preview');
+
+        case "prod":
+        case "production":
+            return require('./configs/prod');
+
+        /**
+         * Shell configs
+         */
+        case "electron":
+            return require('./configs/electron');
+
+        case "chrome":
+            return require('./configs/chrome');
     }
 
-    if (env.prod) {
-        return require('./configs/prod');
-    }
-
-    // Shell configs - always in production
-    if (env.electron) {
-        return require('./configs/electron');
-    }
-
-    if (env.chrome) {
-        return require('./configs/chrome');
-    }
+    throw new Error(`No webpack config found for env '${env}'`);
 }
