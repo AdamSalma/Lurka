b39f1b0c557614709afdc1a5be622922c2c362f1
Adam Salma <adam3salma@gmail.com>
2017-10-21 21:53:15 +0100

    feat(Board): Add BoardHeader styles for different states

--- src/app/containers/Board/assemblies/BoardHeader/index.jsx
+++ src/app/containers/Board/assemblies/BoardHeader/index.jsx
@@ -2,8 +2,14 @@ import React, { Component } from 'react';
 import cx from 'classnames';
 
 import './styles';
-import {ActionButton, BoardSearch} from '../../components';
-import {Icon} from '~/components'
+import {BoardSearch} from '../../components';
+import {
+    Icon,
+    ActionButton,
+    Title,
+    CircleSpinner,
+    ButtonWithPopout
+} from '~/components'
 
 const i = Lurka.icons;
 
@@ -13,37 +19,68 @@ class BoardHeader extends Component {
     }
 
     render() {
-        const { className, onSearch } = this.props;
+        const { className, onSearch, boardID, boardTitle, isDisabled, isSpinnerActive, ErrorMessage, isActive } = this.props;
+        const classes = cx(
+            'BoardHeader',
+            isDisabled && 'BoardHeader--disabled',
+            isActive && 'BoardHeader--animate',
+            ErrorMessage && 'BoardHeader--error',
+            className
+        );
+
+        const props = this.props;
 
         return (
-            <div className={cx('BoardHeader', className)}>
-                <div className="BoardHeader__search">
-                    <BoardSearch
-                        onChange={onSearch}
-                        placeholder="Search"
-                    />
+            <div className={classes}>
+                <div className="BoardHeader__title">
+                    <Title size={1} weight="light" align="center">
+                        {boardTitle && `/${boardID}/ - ${boardTitle}`}
+                    </Title>
                 </div>
-                <div className="BoardHeader__actions">
-                    <ActionButton onClick={this.handleCreateThread}>
-                        <Icon name={i.navbarNewThread}/>
-                        <div className="title">Create</div>
-                    </ActionButton>
-                    <ActionButton onClick={this.handleViewArchive}>
-                        <Icon name={i.navbarArchive}/>
-                        <div className="title">Archive</div>
-                    </ActionButton>
-                    <ActionButton onClick={this.handleRefreshBoard}>
-                        <Icon name={i.navbarRefresh}/>
-                        <div className="title">Refresh</div>
-                    </ActionButton>
-                    <ActionButton onClick={this.handleSort}>
-                        <Icon name={i.navbarSort}/>
-                        <div className="title">Sort by</div>
-                    </ActionButton>
-                    <ActionButton onClick={this.handleFilter}>
-                        <Icon name={i.navbarFilter}/>
-                        <div className="title">Filter</div>
-                    </ActionButton>
+                <div className="BoardHeader__metacontent">
+                    {isSpinnerActive &&
+                        <div className="spinner-wrap">
+                            <CircleSpinner className="spinner" />
+                            <Title size={6} className="spinner-text">Loading </Title>
+                        </div>
+                    }
+                    {ErrorMessage &&
+                        <div className="error-wrapper">
+                            <Title size={4}>{ErrorMessage}</Title>
+                            <div className="error-reload"><Icon name={i.boardHeaderRefresh}/>Try Reloading</div>
+                            <div className="error-report"><Icon name={i.boardHeaderReportError}/>Report error</div>
+                        </div>
+                    }
+                </div>
+                <div className="BoardHeader__subcontents">
+                    <div className="BoardHeader__search">
+                        <BoardSearch
+                            onChange={onSearch}
+                            placeholder="Search"
+                        />
+                    </div>
+                    <div className="BoardHeader__btns">
+                        <ButtonWithPopout className="BoardHeader__btn"
+                          onClick={this.handleSort}
+                          popout={<Icon name={i.navbarSort}/>}>
+                            <div className="title">Sort by</div>
+                        </ButtonWithPopout>
+                        <ButtonWithPopout className="BoardHeader__btn"
+                          onClick={this.handleFilter}
+                          popout={<Icon name={i.navbarFilter}/>}>
+                            <div className="title">Filter</div>
+                        </ButtonWithPopout>
+                        <ButtonWithPopout className="BoardHeader__btn push-left"
+                          onClick={this.handleCreateThread}
+                          popout={<Icon name={i.navbarNewThread}/>}>
+                            <div className="title">Create</div>
+                        </ButtonWithPopout>
+                        <ButtonWithPopout className="BoardHeader__btn"
+                          onClick={this.handleRefreshBoard}
+                          popout={<Icon name={i.navbarRefresh}/>}>
+                            <div className="title">Refresh</div>
+                        </ButtonWithPopout>
+                    </div>
                 </div>
             </div>
         );
@@ -51,7 +88,7 @@ class BoardHeader extends Component {
 
     handleSearch = () => {}
     handleCreateThread = () => {}
-    handleViewArchive = () => {}
+    // handleViewArchive = () => {}
     handleRefreshBoard = () => {}
     handleSort = () => {}
     handleFilter = () => {}
