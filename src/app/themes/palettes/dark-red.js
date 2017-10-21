c387fa1ab48dbfcb8c37187c6a8c8d0bca74cf60
Adam Salma <adam3salma@gmail.com>
2017-10-21 20:48:53 +0100

    refactor(themes): Use programatic approach to themes

--- src/app/themes/palettes/dark-red.js
+++ src/app/themes/palettes/dark-red.js
@@ -1,41 +1,67 @@
-const darkRed = {
-    primaryLightest: '#eca8a7',
-    primaryLight:    '#e77a79',
+/**
+ * Dark Red theme
+ * By Lurka
+ */
+
+import c from 'color'
+import themeFreeze from '../themeFreeze';
+import { warmTint, coldTint } from '../utils';
+
+export const darkRed = {
+    // *** Primary ***
     primary:         '#e44a49',
-    primaryDark:     '#e01b1a',
-    primaryDarkest:  '#b71110',
+    get primaryDark () { return c(this.primary).alpha(0.5) },
+    get primaryDarkest () { return c(this.primary).alpha(0.22) },
+    get primaryLight () { return c(this.primary).lighten(0.1).saturate(0.1) },
+    get primaryLightest () { return c(this.primary).lighten(0.3).saturate(0.3) },
+    // primaryLightest: '#eca8a7',
+    // primaryLight:    '#e77a79',
+    // primaryDark:     '#e01b1a',
+    // primaryDarkest:  '#b71110',
 
-    greyLightest: "#45403e",
-    greyLight:    "#35302e",
-    grey:         "#2e2927",
-    greyDark:     "#26211f",
-    greyDarkest:  "#1f1a18",
+    // *** Greys ***
+    greyLightest: "hsl(0, 0%, 19%)",
+    greyLight:    "hsl(0, 0%, 15%)",
+    grey:         "hsl(0, 0%, 13%)",
+    greyDark:     "hsl(0, 0%, 11%)",
+    greyDarkest:  "hsl(0, 0%, 7%)",
 
-    textPrimary:   'rgba(255,255,255, 1)',
-    textSecondary: 'rgba(255,255,255, 0.7)',
-    textDisabled:  'rgba(255,255,255, 0.5)',
-    divider:       'rgba(255,255,255, 0.12)',
+    // *** Text ***
+    textPrimary:   c('white').alpha(1),
+    textSecondary: c('white').alpha(0.7),
+    textDisabled:  c('white').alpha(0.5),
+    divider:       c('white').alpha(0.12),
 
+    // *** Other ***
     greenText: '#a4c73b',
 
-    board: '#2e2927',  // hsl(0, 0%, 16%) warm-tint
+    get textSelectionBackground(){ return this.primary },
+    get textSelectionColor() { return c(this.primary).lighten(0.6) },
+
+    highlight: '#402c2a',  // mix($primary, grey(7), 10%)
+    get primaryOverlay() { return c(this.primary).alpha(0.22) },
+    get interfaceColor () { return this.greyDark }, // hsl(0, 0%, 16%) warm-tint
+    get contentColor () { return this.grey },
+
+    // *** Layout ***
+    get board () { return this.grey },  // hsl(0, 0%, 16%) warm-tint
+    get headerShrunk () { return this.greyDark },
+    get headerExpanded () { return this.greyDark },
+
     // boardSearchbar: 'rgba(228, 74, 73, 0.11)',
     boardSearchbar: 'rgba(255, 255, 255, 0.02)',
-    boardPostBackground: '#26211f',  // hsl(0, 0%, 13%) warm-tint
-    boardPostShadow: '#312d2b',  // HOOK
+    get boardPostBackground () { return this.greyLight },  // hsl(0, 0%, 13%) warm-tint
+    boardPostShadow: c('#312d2b'),  // HOOK
 
-    threadPostBackground: '#2e2927', // hsl(0, 0%, 16%) warm-tint
+    get threadPostBackground () { return this.grey }, // hsl(0, 0%, 16%) warm-tint
+    get contentBorder () { return this.grey },
 
-    postHighlight: '#402c2a',  // mix($primary, grey(7), 10%)
 
-    textSelectionBackground: '#e44a49',  // $primary
-    textSelectionColor: '#f4b6b6',  // lighten($primary, 60%)
-
-    contentBorder: '#2e2927', // hsl(0, 0%, 16%) warm-tint
+    /* Tint functions from sass color scheme */
+    // warm-tint: adjust-color($color, $red: +5, $blue: -2);
+    // cold-tint: adjust-color($color, $red: -2, $blue: +5);
 }
 
+// Stringify then parse to turn getters into normal props
 
-// warm-tint: adjust-color($color, $red: +5, $blue: -2);
-// cold-tint: adjust-color($color, $red: -2, $blue: +5);
-
-export default darkRed;
+export default themeFreeze(darkRed);
