bc6afae425c3503d460724062155ff53ace1da0e
Adam Salma <adam3salma@gmail.com>
2017-10-21 21:04:18 +0100

    feat(components): Bind reference to ExpandedImage functional component

--- src/app/components/media/ExpandedImage/ExpandedImage.jsx
+++ src/app/components/media/ExpandedImage/ExpandedImage.jsx
@@ -1,24 +1,20 @@
 import React from 'react';
 import cx from 'classnames';
-import { Image } from '~/components';
 import './ExpandedImage.styles';
 
 
-const ExpandedImage = ({ className, srcThumbnail, srcExpanded, ...restProps }) => {
-
+const ExpandedImage = ({ className, srcThumbnail, srcExpanded, setReference, ...restProps }) => {
     const thumbnailStyles = {
         backgroundImage: `url(${srcThumbnail})`
     }
 
-    // Else is loading fullsize image
     return (
-        <div className={cx('ExpandedImage', className)} {...restProps}>
+        <div className={cx('ExpandedImage', className)} ref={setReference} {...restProps}>
             <div className="ExpandedImage__blurred-thumbnail" style={thumbnailStyles}/>
             <img className="ExpandedImage__expanded" src={srcExpanded}/>
         </div>
     );
 };
-            // <Image className="ExpandedImage__expanded" src={srcExpanded}/>
 
 ExpandedImage.displayName = 'ExpandedImage';
 
