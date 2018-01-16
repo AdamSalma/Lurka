df1081807f1cb832aa27056d82cd01eb35ad2e18
Adam Salma <Adam.Salma@kantarworldpanel.com>
2018-01-16 15:44:00 +0000

    tools(gulp): Upgrade to v5 and add packaging tasks

--- tools/gulp/gulpfile.js
+++ tools/gulp/gulpfile.js
@@ -2,33 +2,45 @@ import gulp from 'gulp';
 import runSequence from 'run-sequence';
 import * as tasks from './tasks';
 
-/* Build */
-gulp.task('build', done => runSequence('clear', 'build-all', done));
-gulp.task('build-all', ['build:app', 'build:electron' /*'build:chrome'*/])
-gulp.task('build:app', tasks.buildApp);
-gulp.task('build:electron', tasks.buildElectron);
-// gulp.task('bundle:chrome', tasks.bundleChrome);
 
-
-/* Package */
-gulp.task('prepackage', ['ensureAppIsBuilt', 'mkdir-dist']);
-gulp.task('package', done => runSequence('prepackage', ['package:electron', 'package:chrome'], done));
-gulp.task('package:electron', done => runSequence('prepackage', tasks.packageElectron, done));
-gulp.task('package:chrome', done => runSequence('prepackage', tasks.packageChrome, done));
-
-/* Electron package */
-// architectures: ia32, x64, armv7l
-// platforms:  linux, win32, darwin, mas
+/**
+ * Build plan:
+ *
+ * Parse argv to get build targets
+ * Clean the output directory
+ * Run
+ *
+ *
+ * Build app/electron/both
+ * Package them up into dist
+ */
 
 
 /* Helpers */
 gulp.task('clear', tasks.clear);
 gulp.task('ensureAppIsBuilt', tasks.ensureAppIsBuilt);
 gulp.task('mkdir-dist', tasks.mkdirDist);
+
+/* Build */
+gulp.task('build:electron', tasks.buildElectron);
+gulp.task('build:app', tasks.buildApp);
+gulp.task('build', gulp.series('clear', gulp.parallel('build:app', 'build:electron')));
+// gulp.task('bundle:chrome', tasks.bundleChrome);
+
+
+/* Package */
+gulp.task('pre-package', gulp.parallel('ensureAppIsBuilt', 'mkdir-dist'));
+gulp.task('package', gulp.series('pre-package', tasks.packageBuild));
+
+/* Electron package */
+// architectures: ia32, x64, armv7l
+// platforms:  linux, win32, darwin, mas
+
+
 // gulp.task('copy:assets', tasks.copyAssets);
 // gulp.task('copy:fonts', tasks.copyFonts);
 // gulp.task('copy', () => runSequence(['copy:assets'], 'copy:fonts'));
 
 
 
-gulp.task('default', ['build']);
+// gulp.task('default', );
