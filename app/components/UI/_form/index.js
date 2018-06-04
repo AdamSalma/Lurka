fb20666bdf50814e63fb1ea5aaffdab8169ffa1f
Adam Salma <adam3salma@gmail.com>
2018-06-04 23:01:14 +0100

    Remove old UI folders

--- app/components/UI/_form/index.js
+++
@@ -1,7 +0,0 @@
-export {default as Checkbox} from './Checkbox'
-export {default as FileInput} from './FileInput'
-export {default as RadioGroup} from './RadioGroup'
-export {default as SearchBar} from './SearchBar'
-export {default as SearchBarWithIcons} from './SearchBarWithIcons'
-export {default as TextArea} from './TextArea'
-export {default as TextField} from './TextField'
