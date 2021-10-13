8a215aba776d2631359bd84a22528e254af01c18
Adam Salma <adam3salma@gmail.com>
2021-10-13 23:54:25 +0100

    Add install.sh script for linux development (WSL)

---
+++ install.sh
@@ -0,0 +1,6 @@
+sudo apt update
+sudo apt-get install libgconf-2-4 libnss3-dev libgdk-pixbuf2.0-dev libgtk-3-dev libxss-dev;
+npm install;
+cd build && npm install;
+
+echo '\nApp dependencies installed'
